be62c2e6513ab5c85055b1df92a941e2cf7797da
Nouha <bnouha1@yahoo.com>
2016-02-10 14:22:51 -0500

    clear stacktrace

--- app/src/com/constellio/app/services/extensions/plugins/ConstellioPluginConfigurationManager.java
+++ app/src/com/constellio/app/services/extensions/plugins/ConstellioPluginConfigurationManager.java
@@ -78,7 +78,7 @@ public class ConstellioPluginConfigurationManager {
 			throw new RuntimeException("Unsupported status " + status);
 		}
 		setPluginAttributeValue(pluginId, STATUS_ATTRIBUTE, ENABLED.toString());
-		setPluginAttributeValue(pluginId, STATUS_ATTRIBUTE, "");
+		setPluginAttributeValue(pluginId, STACK_TRACE, "");
 	}
 
 	private ConstellioPluginStatus prValidateModule(String pluginId)
