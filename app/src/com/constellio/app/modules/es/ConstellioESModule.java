070a7896eaba044c05b6486d9ac67a932f1eeeba
francisbaril <baril.francis@gmail.com>
2016-03-02 12:45:41 -0500

    fixing ess module

--- app/src/com/constellio/app/modules/es/ConstellioESModule.java
+++ app/src/com/constellio/app/modules/es/ConstellioESModule.java
@@ -33,7 +33,6 @@ import com.constellio.app.modules.es.services.ConnectorManager;
 import com.constellio.app.modules.es.services.ESSchemasRecordsServices;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.model.entities.configs.SystemConfiguration;
-import com.constellio.model.entities.schemas.MetadataSchemaType;
 import com.constellio.model.extensions.ModelLayerCollectionExtensions;
 import com.constellio.model.services.factories.ModelLayerFactory;
 import com.constellio.model.services.records.cache.RecordsCache;
@@ -103,7 +102,6 @@ public class ConstellioESModule implements InstallableModule {
 		setupModelLayerExtensions(collection, appLayerFactory);
 		setupAppLayerExtensions(collection, appLayerFactory);
 
-		registerPublicTypes(collection, appLayerFactory);
 
 	}
 
@@ -152,20 +150,6 @@ public class ConstellioESModule implements InstallableModule {
 		recordsCache.configureCache(permanentCache(es.connectorInstance.schemaType()));
 
 		extensions.recordExtensions.add(new ESRecordExtension(es));
-	}
-
-	private void registerPublicTypes(String collection, AppLayerFactory appLayerFactory) {
-
-		List<MetadataSchemaType> schemaTypes = appLayerFactory.getModelLayerFactory().getMetadataSchemasManager()
-				.getSchemaTypes(collection).getSchemaTypes();
-
-		for (MetadataSchemaType metadataSchemaType : schemaTypes) {
-			if (metadataSchemaType.getCode().startsWith("connector") && (metadataSchemaType.getCode().contains("Document")
-					|| metadataSchemaType.getCode().contains("Folder"))) {
-				appLayerFactory.getModelLayerFactory().getSecurityTokenManager()
-						.registerPublicType(metadataSchemaType.getCode());
-			}
-		}
 	}
 
 }
