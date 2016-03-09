d554f23e00a90cb5236590350c269fa86d4759ef
Tommaso Donnarumma <tommaso.donnarumma@doculibre.com>
2016-03-09 16:07:39 -0500

    Retention rules
    * Create Component for document retention copy selection
    * Plug the component in the document form
    * Refactoring

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo6_2.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo6_2.java
@@ -7,6 +7,8 @@ import com.constellio.app.modules.rm.model.calculators.folder.FolderMainCopyRule
 import com.constellio.app.modules.rm.wrappers.Document;
 import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.app.services.schemasDisplay.SchemaTypesDisplayTransactionBuilder;
+import com.constellio.app.services.schemasDisplay.SchemasDisplayManager;
 import com.constellio.model.entities.schemas.MetadataValueType;
 import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
@@ -21,6 +23,17 @@ public class RMMigrationTo6_2 implements MigrationScript {
 	public void migrate(String collection, MigrationResourcesProvider migrationResourcesProvider, AppLayerFactory appLayerFactory)
 			throws Exception {
 		new SchemaAlterationsFor6_2(collection, migrationResourcesProvider, appLayerFactory).migrate();
+		setupDisplayConfigs(collection, appLayerFactory);
+	}
+
+	private void setupDisplayConfigs(String collection, AppLayerFactory factory) {
+		SchemasDisplayManager schemaDisplayManager = factory.getMetadataSchemasDisplayManager();
+
+		SchemaTypesDisplayTransactionBuilder transaction = schemaDisplayManager.newTransactionBuilderFor(collection);
+
+		transaction.in(Document.SCHEMA_TYPE).addToForm(Document.MAIN_COPY_RULE_ID_ENTERED).afterMetadata(Document.TITLE);
+
+		schemaDisplayManager.execute(transaction.build());
 	}
 
 	public static class SchemaAlterationsFor6_2 extends MetadataSchemasAlterationHelper {
