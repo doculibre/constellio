433e2355b87396d059deec47525e2d657c3479bb
francisbaril <baril.francis@gmail.com>
2016-03-28 19:10:13 -0400

    optimized code for faster import

--- model/src/com/constellio/model/utils/MaskUtils.java
+++ model/src/com/constellio/model/utils/MaskUtils.java
@@ -1,6 +1,10 @@
 package com.constellio.model.utils;
 
 import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.text.MaskFormatter;
 
@@ -8,30 +12,32 @@ import com.constellio.model.utils.MaskUtilsException.MaskUtilsException_InvalidV
 
 public class MaskUtils {
 
-//	public static String format(String mask, String rawValue)
-//			throws MaskUtilsException {
-//
-//		if (rawValue == null || mask == null || isValid(mask, rawValue)) {
-//			return rawValue;
-//		}
-//
-//		try {
-//
-//			MaskFormatter maskFormatter = newMaskFormatter(mask);
-//			maskFormatter.setValueContainsLiteralCharacters(false);
-//			String formattedValue = maskFormatter.valueToString(rawValue);
-//
-//			if (!isValid(mask, formattedValue)) {
-//				throw new MaskUtilsException_InvalidValue(mask, rawValue);
-//			}
-//
-//			return formattedValue;
-//
-//		} catch (ParseException e) {
-//			throw new MaskUtilsException_InvalidValue(mask, rawValue, e);
-//		}
-//
-//	}
+	//	public static String format(String mask, String rawValue)
+	//			throws MaskUtilsException {
+	//
+	//		if (rawValue == null || mask == null || isValid(mask, rawValue)) {
+	//			return rawValue;
+	//		}
+	//
+	//		try {
+	//
+	//			MaskFormatter maskFormatter = newMaskFormatter(mask);
+	//			maskFormatter.setValueContainsLiteralCharacters(false);
+	//			String formattedValue = maskFormatter.valueToString(rawValue);
+	//
+	//			if (!isValid(mask, formattedValue)) {
+	//				throw new MaskUtilsException_InvalidValue(mask, rawValue);
+	//			}
+	//
+	//			return formattedValue;
+	//
+	//		} catch (ParseException e) {
+	//			throw new MaskUtilsException_InvalidValue(mask, rawValue, e);
+	//		}
+	//
+	//	}
+
+	private static Map<String, Pattern> maskRegexPatterns = new HashMap<>();
 
 	public static boolean isValid(String mask, String value) {
 		try {
@@ -42,18 +48,27 @@ public class MaskUtils {
 		}
 	}
 
-	private static String buildRegex(String mask) {
-		return mask.replace("\\9", "__ZE_NEUF__").replace("\\A", "__ZE_FIRST_LETTER__").replace("\\*", "__ZE_FLOCON__")
-				.replace("9", "\\d").replace("A", "[A-Za-z]").replace("*", "[A-Za-z0-9]").replace("(", "\\(").replace(")", "\\)")
-				.replace("__ZE_NEUF__", "9").replace("__ZE_FIRST_LETTER__", "A").replace("__ZE_FLOCON__", "\\*");
+	private static Pattern buildRegex(String mask) {
+		Pattern maskRegexPattern = maskRegexPatterns.get(mask);
+		if (maskRegexPattern == null) {
+			String regex = mask.replace("\\9", "__ZE_NEUF__").replace("\\A", "__ZE_FIRST_LETTER__")
+					.replace("\\*", "__ZE_FLOCON__")
+					.replace("9", "\\d").replace("A", "[A-Za-z]").replace("*", "[A-Za-z0-9]").replace("(", "\\(")
+					.replace(")", "\\)")
+					.replace("__ZE_NEUF__", "9").replace("__ZE_FIRST_LETTER__", "A").replace("__ZE_FLOCON__", "\\*");
+			maskRegexPattern = Pattern.compile(regex);
+			maskRegexPatterns.put(mask, maskRegexPattern);
+		}
+		return maskRegexPattern;
 	}
 
 	public static void validate(String mask, String formattedValue)
 			throws MaskUtilsException {
 
-		String regex = buildRegex(mask);
+		Pattern pattern = buildRegex(mask);
+		Matcher matcher = pattern.matcher(formattedValue);
 
-		boolean valid = formattedValue.matches(regex);
+		boolean valid = matcher.matches();
 		if (!valid) {
 			throw new MaskUtilsException_InvalidValue(mask, formattedValue);
 		}
