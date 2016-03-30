bee23e82e42bf8118fe1549c343e65b20779bdc9
Nouha <bnouha1@yahoo.com>
2016-03-30 13:37:57 -0400

    containers button ok

--- app/src/com/constellio/app/ui/framework/buttons/ContainersButton.java
+++ app/src/com/constellio/app/ui/framework/buttons/ContainersButton.java
@@ -4,66 +4,95 @@ import static com.constellio.app.ui.i18n.i18n.$;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
-import com.constellio.app.modules.rm.model.labelTemplate.LabelTemplate;
-import com.constellio.app.modules.rm.reports.factories.labels.LabelsReportFactory;
-import com.constellio.app.ui.entities.LabelParametersVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.constellio.app.modules.rm.wrappers.ContainerRecord;
 import com.constellio.app.ui.framework.buttons.LabelsButton.RecordSelector;
 import com.constellio.app.ui.framework.components.BaseForm;
-import com.constellio.app.ui.framework.components.ReportViewer;
-import com.constellio.model.frameworks.validation.ValidationException;
+import com.constellio.app.ui.framework.components.fields.lookup.LookupRecordField;
 import com.vaadin.data.fieldgroup.PropertyId;
-import com.vaadin.ui.AbstractSelect.ItemCaptionMode;
+import com.vaadin.server.Page;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 public class ContainersButton extends WindowButton {
-	private final RecordSelector selector;
-	private final Map<String, String> containersLabelsMappedById;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ContainersButton.class);
+	private final ContainersAssigner selector;
 
-	@PropertyId("startPosition") private ComboBox startPosition;
+	private LookupRecordField containerLookup;
 
-	public ContainersButton(String caption, String windowCaption, RecordSelector selector,
-			Map<String, String> containersLabelsMappedById) {
-		super(caption, windowCaption, WindowConfiguration.modalDialog("75%", "75%"));
+	public ContainersButton(ContainersAssigner selector) {
+		this($("ContainersButton.container"), $("ContainersButton.containerAssigner"), selector);
+	}
+
+	public ContainersButton(String caption, String windowCaption, ContainersAssigner selector) {
+		super(caption, windowCaption, WindowConfiguration.modalDialog("65%", "25%"));
 		this.selector = selector;
-		this.containersLabelsMappedById = containersLabelsMappedById;
 	}
 
 	@Override
 	protected Component buildWindowContent() {
-		startPosition = new ComboBox($("LabelsButton.startPosition"), this.containersLabelsMappedById.values());
-		startPosition.setNullSelectionAllowed(false);
+		VerticalLayout formLayout = new VerticalLayout();
+		formLayout.setSpacing(true);
+		containerLookup = new LookupRecordField(ContainerRecord.SCHEMA_TYPE);
+		containerLookup.setCaption($("ContainersButton.containerTitle"));
+		HorizontalLayout containerLayout = new HorizontalLayout();
+		containerLayout.addComponent(containerLookup);
+		containerLayout.setComponentAlignment(containerLookup, Alignment.MIDDLE_CENTER);
+		formLayout.addComponent(containerLayout);
 
-		/*List<LabelTemplate> configurations = templatesFactory.get();
-		format = new ComboBox($("LabelsButton.labelFormat"));
-		for (LabelTemplate configuration : configurations) {
-			format.addItem(configuration);
-			format.setItemCaption(configuration, $(configuration.getName()));
-		}
-		if(configurations.size() > 0) {
-			format.select(configurations.get(0));
-		}*/
+		HorizontalLayout buttonsLayout = new HorizontalLayout();
+		buttonsLayout.addStyleName(BaseForm.BUTTONS_LAYOUT);
+		buttonsLayout.setSpacing(true);
 
-		return new BaseForm<LabelParametersVO>(
-				new LabelParametersVO(new LabelTemplate()), this, startPosition) {
+		Button saveButton = new Button($("ContainersButton.putInContent"));
+		saveButton.addStyleName(BaseForm.SAVE_BUTTON);
+		saveButton.addStyleName(ValoTheme.BUTTON_PRIMARY);
+		saveButton.addClickListener(new ClickListener() {
 			@Override
-			protected void saveButtonClick(LabelParametersVO parameters)
-					throws ValidationException {
-				/*LabelTemplate labelTemplate = format.getValue() != null ? (LabelTemplate) format.getValue() : new LabelTemplate();
-				LabelsReportFactory factory = new LabelsReportFactory(
-						selector.getSelectedRecordIds(), labelTemplate,
-						parameters.getStartPosition(), parameters.getNumberOfCopies());
-				getWindow().setContent(new ReportViewer(factory));*/
+			public void buttonClick(ClickEvent event) {
+				try{
+					selector.putRecordsInContainer(selector.getSelectedRecordIds(), containerLookup.getValue());
+					getWindow().close();
+				}catch(Throwable e){
+					showErrorMessage(e);
+				}
+
 			}
+		});
 
+		Button cancelButton = new Button($("cancel"));
+		cancelButton.addStyleName(BaseForm.CANCEL_BUTTON);
+		cancelButton.addClickListener(new ClickListener() {
 			@Override
-			protected void cancelButtonClick(LabelParametersVO parameters) {
+			public void buttonClick(ClickEvent event) {
 				getWindow().close();
 			}
-		};
+		});
+		formLayout.addComponent(buttonsLayout);
+		buttonsLayout.addComponents(saveButton, cancelButton);
+		return formLayout;
+	}
+
+	private void showErrorMessage(Throwable e) {
+		Notification notification = new Notification(e.getMessage() + "<br/><br/>" + $("clickToClose"), Type.WARNING_MESSAGE);
+		notification.setHtmlContentAllowed(true);
+		notification.show(Page.getCurrent());
+		LOGGER.warn(e.getMessage(), e);
+	}
+
+	public static interface ContainersAssigner extends Serializable, RecordSelector {
+		List<String> getSelectedRecordIds();
+		void putRecordsInContainer(List<String> recordsIds, String containerId);
 	}
 
 }
