cb247e6e9619f5370ab9e224187ba0c736bacdc1
limathiago <thiago.lima@doculibre.com>
2016-04-15 14:32:14 -0400

    Hierarchy des unitées administratives dans display folder

---
+++ app/src/com/constellio/app/modules/rm/ui/components/retentionRule/AdministrativeUnitReferenceDisplay.java
@@ -0,0 +1,46 @@
+package com.constellio.app.modules.rm.ui.components.retentionRule;
+
+import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
+import com.constellio.app.ui.application.ConstellioUI;
+import com.constellio.app.ui.entities.RecordVO;
+import com.constellio.app.ui.framework.components.converters.RecordIdToCaptionConverter;
+import com.constellio.app.ui.framework.components.display.ReferenceDisplay;
+import com.constellio.model.entities.records.Record;
+import com.constellio.model.services.factories.ModelLayerFactory;
+import com.constellio.model.services.records.RecordServices;
+
+public class AdministrativeUnitReferenceDisplay extends ReferenceDisplay {
+
+	private static final String SEPARATOR = " > ";
+
+	public AdministrativeUnitReferenceDisplay(RecordVO recordVO) {
+		super(recordVO);
+	}
+
+	public AdministrativeUnitReferenceDisplay(String recordId) {
+		super(recordId);
+	}
+
+	@Override
+	public void setCaption(String caption) {
+
+		RecordIdToCaptionConverter recordIdToCaptionConverter = new RecordIdToCaptionConverter();
+		ConstellioUI ui = ConstellioUI.getCurrent();
+		String collection = ui.getSessionContext().getCurrentCollection();
+		ModelLayerFactory modelLayerFactory = ui.getConstellioFactories().getModelLayerFactory();
+		RecordServices recordServices = modelLayerFactory.newRecordServices();
+		RMSchemasRecordsServices rmSchemasRecordsServices = new RMSchemasRecordsServices(collection, ui);
+
+		Record record = recordServices.getDocumentById(getRecordId());
+		String id = record.get(rmSchemasRecordsServices.administrativeUnitParent());
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(caption);
+		while (id != null) {
+			record = recordServices.getDocumentById(id);
+			String value = recordIdToCaptionConverter.convertToPresentation(id, String.class, getLocale()) + SEPARATOR;
+			stringBuilder.insert(0, value);
+			id = record.get(rmSchemasRecordsServices.administrativeUnitParent());
+		}
+		super.setCaption(stringBuilder.toString());
+	}
+}
