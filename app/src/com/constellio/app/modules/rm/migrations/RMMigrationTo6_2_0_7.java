a231b728a198e524329687aa5a30a8fedd723748
francisbaril <baril.francis@gmail.com>
2016-04-27 11:00:18 -0400

    fixed other metadatas

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo6_2_0_7.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo6_2_0_7.java
@@ -7,26 +7,30 @@ import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
 
-public class RMMigrationTo6_2_0_6 implements MigrationScript {
+public class RMMigrationTo6_2_0_7 implements MigrationScript {
 	@Override
 	public String getVersion() {
-		return "6.2.0.6";
+		return "6.2.0.7";
 	}
 
 	@Override
 	public void migrate(String collection, MigrationResourcesProvider migrationResourcesProvider, AppLayerFactory appLayerFactory)
 			throws Exception {
-		new SchemaAlterationsFor6_2_0_6(collection, migrationResourcesProvider, appLayerFactory).migrate();
+		new SchemaAlterationsFor6_2_0_7(collection, migrationResourcesProvider, appLayerFactory).migrate();
 	}
 
-	public static class SchemaAlterationsFor6_2_0_6 extends MetadataSchemasAlterationHelper {
-		protected SchemaAlterationsFor6_2_0_6(String collection, MigrationResourcesProvider provider, AppLayerFactory factory) {
+	public static class SchemaAlterationsFor6_2_0_7 extends MetadataSchemasAlterationHelper {
+		protected SchemaAlterationsFor6_2_0_7(String collection, MigrationResourcesProvider provider, AppLayerFactory factory) {
 			super(collection, provider, factory);
 		}
 
 		@Override
 		protected void migrate(MetadataSchemaTypesBuilder typesBuilder) {
 			typesBuilder.getSchema(Folder.DEFAULT_SCHEMA).get(Folder.ADMINISTRATIVE_UNIT_ENTERED).setDefaultRequirement(false);
+			typesBuilder.getSchema(Folder.DEFAULT_SCHEMA).get(Folder.CATEGORY_ENTERED).setDefaultRequirement(false);
+			typesBuilder.getSchema(Folder.DEFAULT_SCHEMA).get(Folder.UNIFORM_SUBDIVISION_ENTERED).setDefaultRequirement(false);
+			typesBuilder.getSchema(Folder.DEFAULT_SCHEMA).get(Folder.COPY_STATUS_ENTERED).setDefaultRequirement(false);
+			typesBuilder.getSchema(Folder.DEFAULT_SCHEMA).get(Folder.RETENTION_RULE_ENTERED).setDefaultRequirement(false);
 		}
 
 	}
