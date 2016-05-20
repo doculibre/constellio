17de6516c7c12049a5f14c024dd1983726891825
francisbaril <baril.francis@gmail.com>
2016-05-20 11:31:36 -0400

    exception message when get types with invalid collection

--- model/src/com/constellio/model/services/schemas/MetadataSchemasManager.java
+++ model/src/com/constellio/model/services/schemas/MetadataSchemasManager.java
@@ -30,6 +30,7 @@ import com.constellio.model.services.collections.CollectionsListManager;
 import com.constellio.model.services.extensions.ConstellioModulesManager;
 import com.constellio.model.services.factories.ModelLayerFactory;
 import com.constellio.model.services.schemas.MetadataSchemasManagerException.OptimisticLocking;
+import com.constellio.model.services.schemas.MetadataSchemasManagerRuntimeException.MetadataSchemasManagerRuntimeException_NoSuchCollection;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
 import com.constellio.model.services.schemas.impacts.SchemaTypesAlterationImpact;
 import com.constellio.model.services.schemas.impacts.SchemaTypesAlterationImpactsCalculator;
@@ -147,7 +148,11 @@ public class MetadataSchemasManager implements StatefulService, OneXMLConfigPerC
 	}
 
 	public MetadataSchemaTypes getSchemaTypes(String collection) {
-		return oneXmlConfigPerCollectionManager().get(collection);
+		MetadataSchemaTypes types = oneXmlConfigPerCollectionManager().get(collection);
+		if (types == null) {
+			throw new MetadataSchemasManagerRuntimeException_NoSuchCollection(collection);
+		}
+		return types;
 	}
 
 	private OneXMLConfigPerCollectionManager<MetadataSchemaTypes> oneXmlConfigPerCollectionManager() {
