461689e8cd210d4c66770ac283cad45653a94746
bnouha1 <bnouha1@yahoo.com>
2016-06-14 15:51:56 -0400

    record delete with new option test

--- sdk/src/com/constellio/model/services/records/RecordDeleteServicesAcceptanceTest.java
+++ sdk/src/com/constellio/model/services/records/RecordDeleteServicesAcceptanceTest.java
@@ -1,11 +1,28 @@
 package com.constellio.model.services.records;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import com.constellio.app.modules.rm.RMTestRecords;
 import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
+import com.constellio.app.modules.rm.wrappers.Category;
+import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.modules.tasks.model.wrappers.Task;
+import com.constellio.app.modules.tasks.services.TasksSchemasRecordsServices;
+import com.constellio.data.dao.services.records.RecordDao;
+import com.constellio.data.utils.TimeProvider;
 import com.constellio.model.entities.records.wrappers.User;
+import com.constellio.model.entities.schemas.Metadata;
+import com.constellio.model.entities.schemas.MetadataSchema;
+import com.constellio.model.entities.schemas.MetadataValueType;
+import com.constellio.model.services.schemas.MetadataSchemaTypesAlteration;
+import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypeBuilder;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
 import com.constellio.model.services.search.SearchServices;
 import com.constellio.model.services.trash.TrashServices;
 import com.constellio.sdk.tests.ConstellioTest;
@@ -13,24 +30,31 @@ import com.constellio.sdk.tests.setups.Users;
 
 public class RecordDeleteServicesAcceptanceTest extends ConstellioTest {
 
+	private static final String CUSTOM_TASK_LOCAL_CODE = "zTask";
+	private static final String CUSTOM_TASK_NEW_METADATA = "zMeta";
 	TrashServices trashServices;
 	Users users = new Users();
 	RMTestRecords records = new RMTestRecords(zeCollection);
 
 	private RecordServices recordServices;
-	private String folderDeletedLogicallyId, documentDeletedLogicallyId, categoryDeletedLogicallyId,
-			documentInADeletedFolder;
 	User admin;
 	private SearchServices searchServices;
 	private RMSchemasRecordsServices rm;
+	private TasksSchemasRecordsServices tasks;
+	private RecordDeleteServices deleteService;
+
+	Category category;
+	Folder parentFolderInCategory_A, subFolder_B;
+	Task taskReferencesFolderB;
+	private MetadataSchema customTaskSchema;
+	private Metadata zMeta;
 
 	@Before
 	public void setUp()
 			throws Exception {
 		prepareSystem(
 				withZeCollection().withConstellioRMModule().withAllTestUsers().withRMTest(records)
-						.withFoldersAndContainersOfEveryStatus().withDocumentsHavingContent(),
-				withCollection(businessCollection).withTasksModule().withAllTestUsers()
+						.withFoldersAndContainersOfEveryStatus().withDocumentsHavingContent()
 		);
 
 		recordServices = getModelLayerFactory().newRecordServices();
@@ -39,6 +63,59 @@ public class RecordDeleteServicesAcceptanceTest extends ConstellioTest {
 		admin = users.adminIn(zeCollection);
 		searchServices = getModelLayerFactory().newSearchServices();
 		rm = new RMSchemasRecordsServices(zeCollection, getAppLayerFactory());
+		tasks = new TasksSchemasRecordsServices(zeCollection, getAppLayerFactory());
+		RecordDao recordDao = getDataLayerFactory().newRecordDao();
+		deleteService = new RecordDeleteServices(recordDao, getModelLayerFactory());
 
+		createCustomTaskSchema();
+		initTests();
+
+	}
+
+	private void createCustomTaskSchema() {
+		getModelLayerFactory()
+				.getMetadataSchemasManager().modify(zeCollection, new MetadataSchemaTypesAlteration() {
+					@Override
+					public void alter(MetadataSchemaTypesBuilder types) {
+						MetadataSchemaTypeBuilder tasksSchemaType = types.getSchemaType(Task.SCHEMA_TYPE);
+						MetadataSchemaBuilder tasksSchema = tasksSchemaType.createCustomSchema(CUSTOM_TASK_LOCAL_CODE);
+						MetadataSchemaBuilder folderSchemaBuilder = types.getDefaultSchema(Folder.SCHEMA_TYPE);
+						tasksSchema.create(CUSTOM_TASK_NEW_METADATA).setType(MetadataValueType.REFERENCE).defineReferencesTo(folderSchemaBuilder);
+					}
+				});
+		customTaskSchema = getModelLayerFactory().getMetadataSchemasManager().getSchemaTypes(zeCollection).getSchemaType(Task.SCHEMA_TYPE).getSchema(CUSTOM_TASK_LOCAL_CODE);
+		zMeta = customTaskSchema.getMetadata(CUSTOM_TASK_NEW_METADATA);
+	}
+
+	private void initTests()
+			throws RecordServicesException {
+		parentFolderInCategory_A = records.getFolder_A01();
+		category = rm.getCategory(parentFolderInCategory_A.getCategory());
+		subFolder_B = rm.newFolder().setParentFolder(parentFolderInCategory_A).setTitle("subFolder_B").setOpenDate(TimeProvider.getLocalDate());
+		recordServices.add(subFolder_B);
+		taskReferencesFolderB = tasks.wrapTask(tasks.create(customTaskSchema));
+		taskReferencesFolderB.set(zMeta.getLocalCode(), subFolder_B);
+	}
+
+	@Test
+	public void givenRecordRefereedByOtherRecordsWhenPhysicallyDeleteFromTrashAndGetNonBreakableLinksThenOk()
+			throws Exception {
+		deleteService.logicallyDelete(parentFolderInCategory_A.getWrappedRecord(), null);
+		Set<String> relatedRecords = deleteService
+				.physicallyDeleteFromTrashAndGetNonBreakableLinks(parentFolderInCategory_A.getWrappedRecord(), null);
+		assertThat(relatedRecords).contains(taskReferencesFolderB.getId());
+		assertThat(relatedRecords).doesNotContain(subFolder_B.getId(), category.getId());
+	}
+
+	@Test
+	public void givenRecordRefereedByOtherRecordsWhenPhysicallyDeleteFromTrashAndGetNonBreakableLinksThenOk2()
+			throws Exception {
+
+		deleteService.logicallyDelete(category.getWrappedRecord(), null);
+		Set<String> relatedRecords = deleteService
+				.physicallyDeleteFromTrashAndGetNonBreakableLinks(category.getWrappedRecord(), null);
+		//pas sure?!
+		assertThat(relatedRecords).contains(parentFolderInCategory_A.getId(), subFolder_B.getId());
+		assertThat(relatedRecords).doesNotContain(taskReferencesFolderB.getId());
 	}
 }
