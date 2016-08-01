24e9f0c5b9c09562b292b8902534c045502ddd7a
bnouha1 <bnouha1@yahoo.com>
2016-08-01 12:29:55 -0400

    migration script some tests

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo6_5_1.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo6_5_1.java
@@ -16,10 +16,20 @@ import com.constellio.app.modules.rm.wrappers.Document;
 import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.model.entities.schemas.Metadata;
+import com.constellio.model.entities.schemas.MetadataSchemaTypes;
+import com.constellio.model.entities.schemas.MetadataSchemasRuntimeException;
 import com.constellio.model.services.schemas.MetadataSchemaTypesAlteration;
+import com.constellio.model.services.schemas.MetadataSchemasManager;
 import com.constellio.model.services.schemas.builders.MetadataBuilder;
 import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
+import com.constellio.model.services.schemas.builders.MetadataSchemaBuilderRuntimeException;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilderRuntimeException;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilderRuntimeException.NoSuchSchemaType;
+import com.constellio.model.services.search.SearchServices;
+import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
+import com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators;
+import com.constellio.model.services.search.query.logical.condition.LogicalSearchCondition;
 
 public class RMMigrationTo6_5_1 implements MigrationScript {
 	@Override
@@ -36,37 +46,33 @@ public class RMMigrationTo6_5_1 implements MigrationScript {
 		makeArchivisticDatesBasedOnNumbers(collection, factory);
 	}
 
-	private void makeArchivisticDatesBasedOnNumbers(String collection, AppLayerFactory factory) {
-		deleteIfPossibleFolderCalendarYearMetadata();
-		deleteIfPossibleFolderCalendarYearEntredMetadata();
-		deleteIfPossibleDocumentCalendarYearMetadata();
-		deleteIfPossibleDocuemntCalendarYearEntredMetadata();
+	private void makeArchivisticDatesBasedOnNumbers(String collection, AppLayerFactory appLayerFactory) {
+		deleteIfPossibleOrDisableFolderCalendarYearMetadata(collection, appLayerFactory);
+		deleteIfPossibleOrDisableFolderCalendarYearEntredMetadata(collection, appLayerFactory);
+		deleteIfPossibleOrDisableDocumentCalendarYearMetadata(collection, appLayerFactory);
+		deleteIfPossibleOrDisableDocuemntCalendarYearEntredMetadata(collection, appLayerFactory);
 
-		//change calculators
+		//TODO change archivistic calculators
 	}
 
-	private void deleteIfPossibleDocuemntCalendarYearEntredMetadata() {
-
+	private void deleteIfPossibleOrDisableDocuemntCalendarYearEntredMetadata(String collection, AppLayerFactory appLayerFactory) {
+		Migration6_5_1_Helper
+				.deleteIfPossibleOrDisableMetadata("calendarYearEntered", Document.SCHEMA_TYPE, collection, appLayerFactory);
 	}
 
-	private void deleteIfPossibleDocumentCalendarYearMetadata() {
+	private void deleteIfPossibleOrDisableDocumentCalendarYearMetadata(String collection, AppLayerFactory appLayerFactory) {
+		Migration6_5_1_Helper
+				.deleteIfPossibleOrDisableMetadata("calendarYear", Document.SCHEMA_TYPE, collection, appLayerFactory);
 	}
 
-	private void deleteIfPossibleFolderCalendarYearEntredMetadata() {
-
+	private void deleteIfPossibleOrDisableFolderCalendarYearEntredMetadata(String collection, AppLayerFactory appLayerFactory) {
+		Migration6_5_1_Helper
+				.deleteIfPossibleOrDisableMetadata("calendarYearEntered", Folder.SCHEMA_TYPE, collection, appLayerFactory);
 	}
 
-	private void deleteIfPossibleFolderCalendarYearMetadata() {
-		//TODO
-		Metadata metadata = null;
-		if(!isUsed(metadata)){
-			//folderDefaultSchema.deleteMetadataWithoutValidation(metadata.getOriginalMetadata());
-		}
-
-	}
-
-	private boolean isUsed(Metadata metadata) {
-		return false;
+	private void deleteIfPossibleOrDisableFolderCalendarYearMetadata(String collection, AppLayerFactory appLayerFactory) {
+		Migration6_5_1_Helper
+				.deleteIfPossibleOrDisableMetadata("calendarYear", Folder.SCHEMA_TYPE, collection, appLayerFactory);
 	}
 
 	private void modifyArchivisticCalculators(String collection, AppLayerFactory factory) {
@@ -96,7 +102,6 @@ public class RMMigrationTo6_5_1 implements MigrationScript {
 		@Override
 		protected void migrate(MetadataSchemaTypesBuilder typesBuilder) {
 			MetadataSchemaBuilder folderDefaultSchema = types().getDefaultSchema(Folder.SCHEMA_TYPE);
-			MetadataSchemaBuilder documentDefaultSchema = types().getDefaultSchema(Document.SCHEMA_TYPE);
 			createArchivisticManualMetadata(folderDefaultSchema);
 			createFolderTimeRangeMetadata(folderDefaultSchema);
 		}
@@ -106,7 +111,6 @@ public class RMMigrationTo6_5_1 implements MigrationScript {
 					.setInputMask("9999-9999").setEnabled(false);
 		}
 
-
 		private void createArchivisticManualMetadata(MetadataSchemaBuilder folderDefaultSchema) {
 			folderDefaultSchema.createUndeletable(Folder.MANUAL_ARCHIVISTIC_STATUS).setType(ENUM).defineAsEnum(FolderStatus.class)
 					.setEnabled(false);
@@ -115,4 +119,73 @@ public class RMMigrationTo6_5_1 implements MigrationScript {
 			folderDefaultSchema.createUndeletable(Folder.MANUAL_EXPECTED_TRANSFER_DATE).setType(DATE).setEnabled(false);
 		}
 	}
+
+	public static class Migration6_5_1_Helper {
+		public static void deleteIfPossibleOrDisableMetadata(String metadataLocalCode, String schemaTypeCode, String collection,
+				AppLayerFactory appLayerFactory) {
+
+			if (isUsed(metadataLocalCode, schemaTypeCode, collection, appLayerFactory)) {
+				disableMetadta(metadataLocalCode, schemaTypeCode, collection, appLayerFactory);
+			} else {
+				deleteMetadata(metadataLocalCode, schemaTypeCode, collection, appLayerFactory);
+			}
+
+		}
+
+		private static void deleteMetadata(final String metadataLocalCode, final String schemaTypeCode, String collection,
+				AppLayerFactory appLayerFactory) {
+			MetadataSchemasManager schemaManager = appLayerFactory.getModelLayerFactory()
+					.getMetadataSchemasManager();
+			try {
+				final Metadata metadata = schemaManager.getSchemaTypes(collection).getDefaultSchema(schemaTypeCode)
+						.getMetadata(metadataLocalCode);
+				schemaManager.modify(collection, new MetadataSchemaTypesAlteration() {
+							@Override
+							public void alter(MetadataSchemaTypesBuilder types) {
+								MetadataSchemaBuilder schemaBuilder = types.getDefaultSchema(schemaTypeCode);
+								schemaBuilder.deleteMetadataWithoutValidation(metadata);
+							}
+						}
+				);
+			} catch (MetadataSchemasRuntimeException.NoSuchMetadata | MetadataSchemasRuntimeException.NoSuchSchemaType e) {
+				//OK metadat does not exist any more
+			}
+		}
+
+		private static void disableMetadta(final String metadataLocalCode, final String schemaTypeCode, String collection,
+				AppLayerFactory appLayerFactory) {
+			MetadataSchemasManager schemaManager = appLayerFactory.getModelLayerFactory()
+					.getMetadataSchemasManager();
+			schemaManager.modify(collection, new MetadataSchemaTypesAlteration() {
+				@Override
+				public void alter(MetadataSchemaTypesBuilder types) {
+					try {
+						types.getDefaultSchema(schemaTypeCode).getMetadata(metadataLocalCode).setEnabled(false);
+					} catch (MetadataSchemaBuilderRuntimeException.NoSuchMetadata | MetadataSchemaTypesBuilderRuntimeException.NoSuchSchemaType e) {
+						//OK metadat does not exist any more
+					}
+				}
+			});
+		}
+
+		public static boolean isUsed(String metadataLocalCode, String schemaTypeCode, String collection,
+				AppLayerFactory appLayerFactory) {
+			MetadataSchemasManager schemaManager = appLayerFactory.getModelLayerFactory()
+					.getMetadataSchemasManager();
+			MetadataSchemaTypes types = schemaManager.getSchemaTypes(collection);
+			try {
+				Metadata metadata = types.getDefaultSchema(schemaTypeCode)
+						.getMetadata(metadataLocalCode);
+				LogicalSearchCondition query = LogicalSearchQueryOperators.from(types.getSchemaType(schemaTypeCode))
+						.where(metadata).isNotNull();
+				SearchServices searchServices = appLayerFactory.getModelLayerFactory().newSearchServices();
+				return searchServices.hasResults(new LogicalSearchQuery(query));
+			} catch (MetadataSchemasRuntimeException.NoSuchSchemaType | MetadataSchemasRuntimeException.NoSuchMetadata e) {
+				//ok
+				return false;
+			}
+		}
+	}
+
+
 }
---
+++ sdk/src/com/constellio/app/modules/rm/migrations/RMMigrationTo6_5_1AcceptanceTest.java
@@ -0,0 +1,8 @@
+package com.constellio.app.modules.rm.migrations;
+
+import com.constellio.sdk.tests.ConstellioTest;
+
+public class RMMigrationTo6_5_1AcceptanceTest extends ConstellioTest {
+	//TODO tests
+
+}
