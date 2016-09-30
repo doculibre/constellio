c9002f67777a2835c3b578346b8afee12457ecf3
francisbaril <baril.francis@gmail.com>
2016-09-30 11:16:56 -0400

    integrated indexes in tests

--- sdk/src/com/constellio/app/ui/StartConstellioWithoutCollectionsAcceptTest.java
+++ sdk/src/com/constellio/app/ui/StartConstellioWithoutCollectionsAcceptTest.java
@@ -5,7 +5,9 @@ import static org.mockito.Mockito.doCallRealMethod;
 import org.junit.Test;
 
 import com.constellio.model.conf.ModelLayerConfiguration;
+import com.constellio.model.conf.PropertiesModelLayerConfiguration.InMemoryModelLayerConfiguration;
 import com.constellio.sdk.tests.ConstellioTest;
+import com.constellio.sdk.tests.ModelLayerConfigurationAlteration;
 import com.constellio.sdk.tests.annotations.MainTest;
 import com.constellio.sdk.tests.annotations.MainTestDefaultStart;
 import com.constellio.sdk.tests.annotations.UiTest;
@@ -19,7 +21,8 @@ public class StartConstellioWithoutCollectionsAcceptTest extends ConstellioTest 
 	public void startOnHomePage()
 			throws Exception {
 		givenTransactionLogIsEnabled();
-		
+
+
 		ModelLayerConfiguration config = getModelLayerFactory().getConfiguration();
 		doCallRealMethod().when(config).getMainDataLanguage();
 
