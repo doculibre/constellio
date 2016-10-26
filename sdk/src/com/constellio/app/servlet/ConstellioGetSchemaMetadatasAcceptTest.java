b679f5212b74d2942909d2f860ab4cf829706e03
Gabriel <gabriel.lefrancois@constellio.com>
2016-10-26 11:51:22 -0400

    Tests are cleaned

--- sdk/src/com/constellio/app/servlet/ConstellioGetSchemaMetadatasAcceptTest.java
+++ sdk/src/com/constellio/app/servlet/ConstellioGetSchemaMetadatasAcceptTest.java
@@ -1,6 +1,7 @@
 package com.constellio.app.servlet;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 import java.io.StringReader;
 import java.util.List;
@@ -9,7 +10,9 @@ import com.constellio.sdk.tests.annotations.InDevelopmentTest;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
@@ -32,15 +35,11 @@ public class ConstellioGetSchemaMetadatasAcceptTest extends ConstellioTest {
 			throws Exception {
 		prepareSystem(withZeCollection().withConstellioRMModule().withAllTest(users));
 		startApplication();
-
 	}
 
-	@Test
-	@InDevelopmentTest
-	public void browserTest()
-			throws Exception {
-		newWebDriver();
-		waitUntilICloseTheBrowsers();
+	@After
+	public void stopApplicationAfterTest() {
+		stopApplication();
 	}
 
 	@Test
@@ -79,26 +78,24 @@ public class ConstellioGetSchemaMetadatasAcceptTest extends ConstellioTest {
 		assertThat(document.getRootElement().getAttributeValue("code")).isEqualTo(zeCollection);
 
 		Element schema = document.getRootElement().getChildren("schema").get(0);
-		assertThat(schema.getAttributeValue("language")).isEqualTo("fr");
-		assertThat(schema.getAttributeValue("code")).isEqualTo("folder_default");
-		assertThat(schema.getAttributeValue("collection")).isEqualTo("zeCollection");
-		assertThat(schema.getAttributeValue("search-field")).isEqualTo("search_txt_fr");
+		assertThat(schema.getAttributes()).extracting("name", "value").contains(
+				tuple("language", "fr"), tuple("code", "folder_default"),
+				tuple("collection", "zeCollection"), tuple("search-field", "search_txt_fr")
+		);
 
 		List<Element> metadatas = schema.getChildren("metadata");
 		Element keywordMetadata = find("keywords", metadatas);
-		assertThat(keywordMetadata.getAttributeValue("code")).isEqualTo("keywords");
-		assertThat(keywordMetadata.getAttributeValue("title")).isEqualTo("Mots-clés");
-		assertThat(keywordMetadata.getAttributeValue("type")).isEqualTo("STRING");
-		assertThat(keywordMetadata.getAttributeValue("multivalue")).isEqualTo("true");
-		assertThat(keywordMetadata.getAttributeValue("solr-field")).isEqualTo("keywords_ss");
-		assertThat(keywordMetadata.getAttributeValue("solr-analyzed-field")).isEqualTo("keywords_txt_fr");
+		assertThat(keywordMetadata.getAttributes()).extracting("name", "value").contains(
+				tuple("code", "keywords"), tuple("title", "Mots-clés"), tuple("type", "STRING"),
+				tuple("multivalue", "true"), tuple("solr-field", "keywords_ss"),
+				tuple("solr-analyzed-field", "keywords_txt_fr")
+		);
 
 		Element typeMetadata = find("type", metadatas);
-		assertThat(typeMetadata.getAttributeValue("code")).isEqualTo("type");
-		assertThat(typeMetadata.getAttributeValue("title")).isEqualTo("Type");
-		assertThat(typeMetadata.getAttributeValue("type")).isEqualTo("REFERENCE");
-		assertThat(typeMetadata.getAttributeValue("multivalue")).isEqualTo("false");
-		assertThat(typeMetadata.getAttributeValue("solr-field")).isEqualTo("typeId_s");
+		assertThat(typeMetadata.getAttributes()).extracting("name", "value").contains(
+				tuple("code", "type"), tuple("title", "Type"), tuple("type", "REFERENCE"),
+				tuple("multivalue", "false"), tuple("solr-field", "typeId_s")
+		);
 	}
 
 	@Test
@@ -111,32 +108,87 @@ public class ConstellioGetSchemaMetadatasAcceptTest extends ConstellioTest {
 		List<Element> schemas = document.getRootElement().getChildren("schema");
 		Element schemaWithLabelDocument = find("document_default", schemas);
 		Element schemaWithLabelCourriel = find("document_email", schemas);
-		assertThat(schemaWithLabelDocument.getAttributeValue("language")).isEqualTo("fr");
-		assertThat(schemaWithLabelDocument.getAttributeValue("code")).isEqualTo("document_default");
-		assertThat(schemaWithLabelDocument.getAttributeValue("collection")).isEqualTo("zeCollection");
-		assertThat(schemaWithLabelDocument.getAttributeValue("search-field")).isEqualTo("search_txt_fr");
-		assertThat(schemaWithLabelCourriel.getAttributeValue("language")).isEqualTo("fr");
-		assertThat(schemaWithLabelCourriel.getAttributeValue("code")).isEqualTo("document_email");
-		assertThat(schemaWithLabelCourriel.getAttributeValue("collection")).isEqualTo("zeCollection");
-		assertThat(schemaWithLabelCourriel.getAttributeValue("search-field")).isEqualTo("search_txt_fr");
+		assertThat(schemaWithLabelDocument.getAttributes()).extracting("name", "value").contains(
+				tuple("language", "fr"), tuple("code", "document_default"),
+				tuple("collection", "zeCollection"), tuple("search-field", "search_txt_fr")
+		);
+		assertThat(schemaWithLabelCourriel.getAttributes()).extracting("name", "value").contains(
+				tuple("language", "fr"), tuple("code", "document_email"),
+				tuple("collection", "zeCollection"), tuple("search-field", "search_txt_fr")
+		);
 
-		List<Element> metadatasDocument = schemaWithLabelDocument.getChildren("metadata");
-		Element keywordMetadataDocument = find("keywords", metadatasDocument);
-		assertThat(keywordMetadataDocument.getAttributeValue("code")).isEqualTo("keywords");
-		assertThat(keywordMetadataDocument.getAttributeValue("title")).isEqualTo("Mots-clés");
-		assertThat(keywordMetadataDocument.getAttributeValue("type")).isEqualTo("STRING");
-		assertThat(keywordMetadataDocument.getAttributeValue("multivalue")).isEqualTo("true");
-		assertThat(keywordMetadataDocument.getAttributeValue("solr-field")).isEqualTo("keywords_ss");
-		assertThat(keywordMetadataDocument.getAttributeValue("solr-analyzed-field")).isEqualTo("keywords_txt_fr");
+		List<Element> documentMetadatas = schemaWithLabelDocument.getChildren("metadata");
+		Element documentKeywordMetadata = find("keywords", documentMetadatas);
+		assertThat(documentKeywordMetadata.getAttributes()).extracting("name", "value").contains(
+				tuple("code", "keywords"), tuple("title", "Mots-clés"), tuple("type", "STRING"),
+				tuple("multivalue", "true"), tuple("solr-field", "keywords_ss"),
+				tuple("solr-analyzed-field", "keywords_txt_fr")
+		);
 
-		List<Element> metadatasCourriel = schemaWithLabelDocument.getChildren("metadata");
-		Element keywordMetadataCourriel = find("keywords", metadatasCourriel);
-		assertThat(keywordMetadataCourriel.getAttributeValue("code")).isEqualTo("keywords");
-		assertThat(keywordMetadataCourriel.getAttributeValue("title")).isEqualTo("Mots-clés");
-		assertThat(keywordMetadataCourriel.getAttributeValue("type")).isEqualTo("STRING");
-		assertThat(keywordMetadataCourriel.getAttributeValue("multivalue")).isEqualTo("true");
-		assertThat(keywordMetadataCourriel.getAttributeValue("solr-field")).isEqualTo("keywords_ss");
-		assertThat(keywordMetadataCourriel.getAttributeValue("solr-analyzed-field")).isEqualTo("keywords_txt_fr");
+		List<Element> courrielMetadatas = schemaWithLabelCourriel.getChildren("metadata");
+		Element courrielKeywordMetadata = find("keywords", courrielMetadatas);
+		assertThat(courrielKeywordMetadata.getAttributes()).extracting("name", "value").contains(
+				tuple("code", "keywords"), tuple("title", "Mots-clés"), tuple("type", "STRING"),
+				tuple("multivalue", "true"), tuple("solr-field", "keywords_ss"),
+				tuple("solr-analyzed-field", "keywords_txt_fr")
+		);
+	}
+
+	@Test
+	public void whenGetDdvDocumentTypeFromTypeReThenValidReturnedDocument()
+			throws Exception {
+
+		Document document = callGetSchemaMetadatasWithType(zeCollection, "ddv", "Types de documents");
+		assertThat(document.getRootElement().getAttributeValue("code")).isEqualTo(zeCollection);
+
+		Element schema = document.getRootElement().getChildren("schema").get(0);
+
+		assertThat(schema.getAttributes()).extracting("name", "value").contains(
+				tuple("language", "fr"), tuple("code", "ddvDocumentType_default"),
+				tuple("collection", "zeCollection"), tuple("search-field", "search_txt_fr")
+		);
+
+		List<Element> metadatas = schema.getChildren("metadata");
+		Element allauthorizationsMetadata = find("allauthorizations", metadatas);
+		assertThat(allauthorizationsMetadata.getAttributes()).extracting("name", "value").contains(
+				tuple("code", "allauthorizations"), tuple("title", "Toutes les autorisations"), tuple("type", "STRING"),
+				tuple("multivalue", "true"), tuple("solr-field", "allauthorizations_ss")
+		);
+	}
+
+	@Test
+	public void whenGetAllDdvFromTypeThenValidReturnedDocument()
+			throws Exception {
+
+		Document document = callGetSchemaMetadatasWithType(zeCollection, "ddv");
+		assertThat(document.getRootElement().getAttributeValue("code")).isEqualTo(zeCollection);
+
+		List<Element> schemas = document.getRootElement().getChildren("schema");
+		Element schemaWithLabelDocument = find("ddvDocumentType_default", schemas);
+		Element schemaWithLabelContainer = find("ddvContainerRecordType_default", schemas);
+
+		assertThat(schemaWithLabelDocument.getAttributes()).extracting("name", "value").contains(
+				tuple("language", "fr"), tuple("code", "ddvDocumentType_default"),
+				tuple("collection", "zeCollection"), tuple("search-field", "search_txt_fr")
+		);
+		assertThat(schemaWithLabelContainer.getAttributes()).extracting("name", "value").contains(
+				tuple("language", "fr"), tuple("code", "ddvContainerRecordType_default"),
+				tuple("collection", "zeCollection"), tuple("search-field", "search_txt_fr")
+		);
+
+		List<Element> documentMetadatas = schemaWithLabelDocument.getChildren("metadata");
+		Element documentAllAuthorizationsMetadata = find("allauthorizations", documentMetadatas);
+		assertThat(documentAllAuthorizationsMetadata.getAttributes()).extracting("name", "value").contains(
+				tuple("code", "allauthorizations"), tuple("title", "Toutes les autorisations"), tuple("type", "STRING"),
+				tuple("multivalue", "true"), tuple("solr-field", "allauthorizations_ss")
+		);
+
+		List<Element> containerMetadatas = schemaWithLabelContainer.getChildren("metadata");
+		Element containerAllAuthorizationsMetadata = find("allauthorizations", containerMetadatas);
+		assertThat(containerAllAuthorizationsMetadata.getAttributes()).extracting("name", "value").contains(
+				tuple("code", "allauthorizations"), tuple("title", "Toutes les autorisations"), tuple("type", "STRING"),
+				tuple("multivalue", "true"), tuple("solr-field", "allauthorizations_ss")
+		);
 	}
 
 	private Element find(String code, List<Element> metadatas) {
