1a15dcfc411fac2980dec15a7e66b6a9b4acb0f8
francisbaril <baril.francis@gmail.com>
2016-10-27 10:57:40 -0400

    disabled admin only restriction + new utility method to log in

---
+++ sdk/src/com/constellio/app/api/cmis/accept/CmisAllowableActionsAcceptanceTest.java
@@ -0,0 +1,162 @@
+package com.constellio.app.api.cmis.accept;
+
+import static com.constellio.model.entities.security.Role.DELETE;
+import static com.constellio.model.entities.security.Role.READ;
+import static com.constellio.model.entities.security.Role.WRITE;
+import static com.constellio.model.entities.security.global.UserCredentialStatus.ACTIVE;
+import static com.constellio.sdk.tests.TestUtils.asSet;
+import static java.util.Arrays.asList;
+import static org.apache.chemistry.opencmis.commons.enums.AclPropagation.REPOSITORYDETERMINED;
+import static org.apache.chemistry.opencmis.commons.enums.Action.CAN_GET_CHILDREN;
+import static org.apache.chemistry.opencmis.commons.enums.Action.CAN_GET_PROPERTIES;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.commons.PropertyIds;
+import org.apache.chemistry.opencmis.commons.data.Ace;
+import org.apache.chemistry.opencmis.commons.enums.Action;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisRuntimeException;
+import org.assertj.core.api.ListAssert;
+import org.assertj.core.groups.Tuple;
+import org.joda.time.Duration;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.constellio.app.api.cmis.accept.CmisAcceptanceTestSetup.Records;
+import com.constellio.model.entities.records.Record;
+import com.constellio.model.entities.records.wrappers.User;
+import com.constellio.model.entities.security.Authorization;
+import com.constellio.model.entities.security.global.AuthorizationBuilder;
+import com.constellio.model.entities.security.global.UserCredential;
+import com.constellio.model.services.migrations.ConstellioEIMConfigs;
+import com.constellio.model.services.records.RecordServices;
+import com.constellio.model.services.schemas.MetadataSchemaTypesAlteration;
+import com.constellio.model.services.schemas.MetadataSchemasManager;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
+import com.constellio.model.services.security.AuthorizationsServices;
+import com.constellio.model.services.taxonomies.TaxonomiesManager;
+import com.constellio.model.services.taxonomies.TaxonomiesSearchServices;
+import com.constellio.model.services.users.UserServices;
+import com.constellio.sdk.tests.ConstellioTest;
+import com.constellio.sdk.tests.annotations.DriverTest;
+import com.constellio.sdk.tests.annotations.SlowTest;
+import com.constellio.sdk.tests.setups.Users;
+
+@DriverTest
+public class CmisAllowableActionsAcceptanceTest extends ConstellioTest {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CmisAllowableActionsAcceptanceTest.class);
+
+	UserServices userServices;
+	TaxonomiesManager taxonomiesManager;
+	MetadataSchemasManager metadataSchemasManager;
+	RecordServices recordServices;
+	Users users = new Users();
+	CmisAcceptanceTestSetup zeCollectionSchemas = new CmisAcceptanceTestSetup(zeCollection);
+	Records zeCollectionRecords;
+	TaxonomiesSearchServices taxonomiesSearchServices;
+
+	List<String> R = asList("cmis:read");
+	List<String> RW = asList("cmis:read", "cmis:write");
+	List<String> RWD = asList("cmis:read", "cmis:write", "cmis:delete");
+	Set<String> constellio_R = asSet(READ);
+	Set<String> constellio_RW = asSet(READ, WRITE);
+	Set<String> constellio_RWD = asSet(READ, WRITE, DELETE);
+
+	//	Session cmisSession;
+	Session session;
+
+	AuthorizationsServices authorizationsServices;
+
+	String aliceId, bobId, charlesId, dakotaId, edouardId, chuckId, gandalfId, robinId, heroesId;
+
+	@Before
+	public void setUp()
+			throws Exception {
+
+		userServices = getModelLayerFactory().newUserServices();
+		taxonomiesManager = getModelLayerFactory().getTaxonomiesManager();
+		metadataSchemasManager = getModelLayerFactory().getMetadataSchemasManager();
+		recordServices = getModelLayerFactory().newRecordServices();
+
+		taxonomiesSearchServices = getModelLayerFactory().newTaxonomiesSearchService();
+
+		users.setUp(userServices);
+
+		defineSchemasManager().using(zeCollectionSchemas);
+		taxonomiesManager.addTaxonomy(zeCollectionSchemas.getTaxonomy2(), metadataSchemasManager);
+		taxonomiesManager.setPrincipalTaxonomy(zeCollectionSchemas.getTaxonomy2(), metadataSchemasManager);
+		getModelLayerFactory().getMetadataSchemasManager().modify(zeCollection, new MetadataSchemaTypesAlteration() {
+			@Override
+			public void alter(MetadataSchemaTypesBuilder types) {
+				types.getSchemaType(zeCollectionSchemas.administrativeUnit.type().getCode()).setSecurity(true);
+				types.getSchemaType(zeCollectionSchemas.classificationStation.type().getCode()).setSecurity(true);
+				types.getSchemaType(zeCollectionSchemas.documentFond.type().getCode()).setSecurity(false);
+				types.getSchemaType(zeCollectionSchemas.category.type().getCode()).setSecurity(false);
+				types.getSchemaType(zeCollectionSchemas.folderSchema.type().getCode()).setSecurity(true);
+				types.getSchemaType(zeCollectionSchemas.documentSchema.type().getCode()).setSecurity(true);
+			}
+		});
+		zeCollectionRecords = zeCollectionSchemas.givenRecords(recordServices);
+
+		userServices.addUserToCollection(users.alice(), zeCollection);
+		userServices.addUserToCollection(users.bob(), zeCollection);
+		userServices.addUserToCollection(users.charles(), zeCollection);
+		userServices.addUserToCollection(users.dakotaLIndien(), zeCollection);
+		userServices.addUserToCollection(users.edouardLechat(), zeCollection);
+		userServices.addUserToCollection(users.gandalfLeblanc(), zeCollection);
+		userServices.addUserToCollection(users.chuckNorris(), zeCollection);
+		userServices.addUserToCollection(users.sasquatch(), zeCollection);
+		userServices.addUserToCollection(users.robin(), zeCollection);
+
+		userServices.addUserToCollection(users.admin(), zeCollection);
+		userServices.addUserToCollection(users.chuckNorris(), zeCollection);
+
+		recordServices.update(users.adminIn(zeCollection).setCollectionReadAccess(true).setCollectionWriteAccess(true));
+		recordServices.update(users.chuckNorrisIn(zeCollection).setCollectionReadAccess(true));
+
+		authorizationsServices = getModelLayerFactory().newAuthorizationsServices();
+
+		aliceId = users.aliceIn(zeCollection).getId();
+		bobId = users.bobIn(zeCollection).getId();
+		charlesId = users.charlesIn(zeCollection).getId();
+		dakotaId = users.dakotaIn(zeCollection).getId();
+		edouardId = users.edouardIn(zeCollection).getId();
+		gandalfId = users.gandalfIn(zeCollection).getId();
+		chuckId = users.chuckNorrisIn(zeCollection).getId();
+		heroesId = users.heroesIn(zeCollection).getId();
+		robinId = users.robinIn(zeCollection).getId();
+
+		givenConfig(ConstellioEIMConfigs.CMIS_NEVER_RETURN_ACL, false);
+	}
+
+	@Test
+	public void whenGetAllowableActionsOfRootThenOK()
+			throws Exception {
+
+		session = newCMISSessionAsUserInZeCollection(admin);
+		assertThat(session.getRootFolder().getAllowableActions().getAllowableActions()).containsOnly(
+				CAN_GET_PROPERTIES, CAN_GET_CHILDREN);
+
+		session = newCMISSessionAsUserInZeCollection(aliceWonderland);
+		assertThat(session.getRootFolder().getAllowableActions().getAllowableActions()).containsOnly(
+				CAN_GET_PROPERTIES, CAN_GET_CHILDREN);
+	}
+
+	private Folder cmisFolder(Record record) {
+		return (Folder) session.getObject(record.getId());
+	}
+
+}
