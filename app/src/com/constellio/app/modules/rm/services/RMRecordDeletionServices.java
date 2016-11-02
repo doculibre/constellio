6065aa0b0170b236289a886737683a9e25c75a50
Gabriel <gabriel.lefrancois@constellio.com>
2016-11-02 09:16:44 -0400

    Add tasks and cleaned code

--- app/src/com/constellio/app/modules/rm/services/RMRecordDeletionServices.java
+++ app/src/com/constellio/app/modules/rm/services/RMRecordDeletionServices.java
@@ -2,10 +2,14 @@ package com.constellio.app.modules.rm.services;
 
 import com.constellio.app.modules.rm.wrappers.AdministrativeUnit;
 import com.constellio.app.modules.rm.wrappers.DecommissioningList;
+import com.constellio.app.modules.rm.wrappers.RMTask;
+import com.constellio.app.modules.tasks.model.wrappers.Task;
+import com.constellio.app.modules.tasks.services.TasksSchemasRecordsServices;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.data.dao.services.bigVault.SearchResponseIterator;
 import com.constellio.model.entities.records.Record;
 import com.constellio.model.entities.records.wrappers.User;
+import com.constellio.model.entities.schemas.MetadataSchema;
 import com.constellio.model.entities.schemas.Schemas;
 import com.constellio.model.services.records.RecordPhysicalDeleteOptions;
 import com.constellio.model.services.records.RecordServices;
@@ -20,9 +24,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.ALL;
-import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.from;
-import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.where;
+import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.*;
 import static java.util.Arrays.asList;
 
 /**
@@ -41,6 +43,7 @@ public class RMRecordDeletionServices {
 
     static public void cleanAdministrativeUnit(String collection, AdministrativeUnit administrativeUnit,
                                                 AppLayerFactory appLayerFactory) {
+        cleanTaskInAdministrativeUnitRecursively(collection, administrativeUnit, appLayerFactory);
         cleanFoldersInAdministrativeUnitRecursively(collection, administrativeUnit, appLayerFactory);
         cleanContainersInAdministrativeUnitRecursively(collection, administrativeUnit, appLayerFactory);
     }
@@ -52,8 +55,7 @@ public class RMRecordDeletionServices {
         AdministrativeUnit administrativeUnit = rm.wrapAdministrativeUnit(searchServices.
                 searchSingleResult(from(rm.administrativeUnit.schema()).where(Schemas.IDENTIFIER)
                         .isEqualTo(administrativeUnitID)));
-        cleanFoldersInAdministrativeUnitRecursively(collection, administrativeUnit, appLayerFactory);
-        cleanContainersInAdministrativeUnitRecursively(collection, administrativeUnit, appLayerFactory);
+        cleanAdministrativeUnit(collection, administrativeUnit, appLayerFactory);
     }
 
     static private void cleanFoldersInAdministrativeUnitRecursively(String collection,
@@ -63,32 +65,21 @@ public class RMRecordDeletionServices {
         RMSchemasRecordsServices rm = new RMSchemasRecordsServices(collection, appLayerFactory);
         SearchServices searchServices = appLayerFactory.getModelLayerFactory().newSearchServices();
         RecordServices recordServices = appLayerFactory.getModelLayerFactory().newRecordServices();
+        TasksSchemasRecordsServices taskSchemas = new TasksSchemasRecordsServices(collection, appLayerFactory);
 
-        LogicalSearchQuery queryDocument = new LogicalSearchQuery().setCondition(from(rm.document.schema())
-                .where(Schemas.PRINCIPAL_PATH).isContainingText(administrativeUnit.getId())).sortDesc(Schemas.PRINCIPAL_PATH);
-        LogicalSearchQuery queryFolder = new LogicalSearchQuery().setCondition(from(rm.folder.schema())
-                .where(Schemas.PRINCIPAL_PATH).isContainingText(administrativeUnit.getId())).sortDesc(Schemas.PRINCIPAL_PATH);
-
-
-
-        SearchResponseIterator<Record> documentIterator = searchServices.recordsIterator(queryDocument);
-        SearchResponseIterator<Record> folderIterator = searchServices.recordsIterator(queryFolder);
+        SearchResponseIterator<Record> documentIterator = searchServices.recordsIterator(new LogicalSearchQuery().setCondition(from(rm.document.schema())
+                .where(Schemas.PRINCIPAL_PATH).isContainingText(administrativeUnit.getId())).sortDesc(Schemas.PRINCIPAL_PATH));
+        SearchResponseIterator<Record> folderIterator = searchServices.recordsIterator(new LogicalSearchQuery().setCondition(from(rm.folder.schema())
+                .where(Schemas.PRINCIPAL_PATH).isContainingText(administrativeUnit.getId())).sortDesc(Schemas.PRINCIPAL_PATH));
+        List<Record> taskList = searchServices.search(new LogicalSearchQuery().setCondition(from(taskSchemas.userTask.schema())
+                .where(Schemas.PRINCIPAL_PATH).isNot(containingText(administrativeUnit.getId()))).sortDesc(Schemas.PRINCIPAL_PATH));
 
         Set<String> recordIDs = new HashSet<>();
         while (documentIterator.hasNext()) {
             Record document = documentIterator.next();
-            List<DecommissioningList> decommissioningLists = rm.searchDecommissioningLists(
-                    where(rm.decommissioningList.documents()).isContaining(asList(document.getId())));
-            for(DecommissioningList decommissioningList: decommissioningLists) {
-                ArrayList<String> decommissioningListDocuments = new ArrayList<>(decommissioningList.getDocuments());
-                decommissioningListDocuments.remove(document.getId());
-                decommissioningList.setDocuments(decommissioningListDocuments);
-                try {
-                    recordServices.update(decommissioningList.getWrappedRecord());
-                } catch (RecordServicesException e) {
-                    LOGGER.info("Could not unlink document from DecommissioningList");
-                }
-            }
+            unlinkDocumentFromDecommissioningLists(document, collection, appLayerFactory);
+            unlinkDocumentFromTasks(document, taskList, collection, appLayerFactory);
+
             if(recordIDs.add(document.getId())) {
                 try {
                     recordServices.physicallyDeleteNoMatterTheStatus(document, User.GOD, new RecordPhysicalDeleteOptions());
@@ -99,17 +90,9 @@ public class RMRecordDeletionServices {
         }
         while (folderIterator.hasNext()) {
             Record folder = folderIterator.next();
-            List<DecommissioningList> decommissioningLists = rm.searchDecommissioningLists(
-                    where(rm.decommissioningList.folders()).isContaining(asList(folder.getId())));
-            for(DecommissioningList decommissioningList: decommissioningLists) {
-                decommissioningList.removeFolderDetail(folder.getId());
+            unlinkFolderFromDecommissioningLists(folder, collection, appLayerFactory);
+            unlinkFolderFromTasks(folder, taskList, collection, appLayerFactory);
 
-                try {
-                    recordServices.update(decommissioningList.getWrappedRecord());
-                } catch (RecordServicesException e) {
-                    LOGGER.info("Could not unlink folder from DecommissioningList");
-                }
-            }
             if(recordIDs.add(folder.getId())) {
                 try {
                     recordServices.physicallyDeleteNoMatterTheStatus(folder, User.GOD, new RecordPhysicalDeleteOptions());
@@ -127,30 +110,137 @@ public class RMRecordDeletionServices {
         RMSchemasRecordsServices rm = new RMSchemasRecordsServices(collection, appLayerFactory);
         SearchServices searchServices = appLayerFactory.getModelLayerFactory().newSearchServices();
         RecordServices recordServices = appLayerFactory.getModelLayerFactory().newRecordServices();
-        LogicalSearchQuery query = new LogicalSearchQuery().setCondition(from(rm.containerRecord.schema())
-                .where(Schemas.PRINCIPAL_PATH).isContainingText(administrativeUnit.getId())).sortDesc(Schemas.PRINCIPAL_PATH);
 
-        SearchResponseIterator<Record> containerIterator = searchServices.recordsIterator(query);
+        SearchResponseIterator<Record> containerIterator = searchServices.recordsIterator(new LogicalSearchQuery().setCondition(from(rm.containerRecord.schema())
+                .where(Schemas.PRINCIPAL_PATH).isContainingText(administrativeUnit.getId())).sortDesc(Schemas.PRINCIPAL_PATH));
+
         Set<String> recordIDs = new HashSet<>();
-
         while(containerIterator.hasNext()) {
             Record container = containerIterator.next();
-            List<DecommissioningList> decommissioningLists = rm.searchDecommissioningLists(
-                    where(rm.decommissioningList.containers()).isContaining(asList(container.getId())));
-            for(DecommissioningList decommissioningList: decommissioningLists) {
-                decommissioningList.removeContainerDetail(container.getId());
+            unlinkContainerFromDecommissioningLists(container, collection, appLayerFactory);
 
-                try {
-                    recordServices.update(decommissioningList.getWrappedRecord());
-                } catch (RecordServicesException e) {
-                    LOGGER.info("Could not unlink container from DecommissioningList");
-                }
-            }
             if(recordIDs.add(container.getId())) {
                 try {
                     recordServices.physicallyDeleteNoMatterTheStatus(container, User.GOD, new RecordPhysicalDeleteOptions());
                 } catch (Exception e) {
                     LOGGER.info("Could not delete container " + container.getId());
+                }
+            }
+        }
+    }
+
+    static private void cleanTaskInAdministrativeUnitRecursively(String collection,
+                                                                       AdministrativeUnit administrativeUnit,
+                                                                       AppLayerFactory appLayerFactory) {
+
+        SearchServices searchServices = appLayerFactory.getModelLayerFactory().newSearchServices();
+        RecordServices recordServices = appLayerFactory.getModelLayerFactory().newRecordServices();
+        TasksSchemasRecordsServices schemas = new TasksSchemasRecordsServices(collection, appLayerFactory);
+        LogicalSearchQuery query = new LogicalSearchQuery().setCondition(from(schemas.userTask.schema())
+                .where(Schemas.PRINCIPAL_PATH).isContainingText(administrativeUnit.getId())).sortDesc(Schemas.PRINCIPAL_PATH);
+
+        SearchResponseIterator<Record> userTaskIterator = searchServices.recordsIterator(query);
+        Set<String> recordIDs = new HashSet<>();
+
+        while(userTaskIterator.hasNext()) {
+            Record userTask = userTaskIterator.next();
+            if(recordIDs.add(userTask.getId())) {
+                try {
+                    recordServices.physicallyDeleteNoMatterTheStatus(userTask, User.GOD, new RecordPhysicalDeleteOptions());
+                } catch (Exception e) {
+                    LOGGER.info("Could not delete task " + userTask.getId());
+                }
+            }
+        }
+    }
+
+    static private void unlinkDocumentFromDecommissioningLists(Record document, String collection, AppLayerFactory appLayerFactory) {
+
+        RMSchemasRecordsServices rm = new RMSchemasRecordsServices(collection, appLayerFactory);
+        RecordServices recordServices = appLayerFactory.getModelLayerFactory().newRecordServices();
+        List<DecommissioningList> decommissioningLists = rm.searchDecommissioningLists(
+                where(rm.decommissioningList.documents()).isContaining(asList(document.getId())));
+        for(DecommissioningList decommissioningList: decommissioningLists) {
+            ArrayList<String> decommissioningListDocuments = new ArrayList<>(decommissioningList.getDocuments());
+            decommissioningListDocuments.remove(document.getId());
+            decommissioningList.setDocuments(decommissioningListDocuments);
+            try {
+                recordServices.update(decommissioningList.getWrappedRecord());
+            } catch (RecordServicesException e) {
+                LOGGER.info("Could not unlink document from decommissioningList");
+            }
+        }
+    }
+
+    static private void unlinkFolderFromDecommissioningLists(Record folder, String collection, AppLayerFactory appLayerFactory) {
+
+        RMSchemasRecordsServices rm = new RMSchemasRecordsServices(collection, appLayerFactory);
+        RecordServices recordServices = appLayerFactory.getModelLayerFactory().newRecordServices();
+        List<DecommissioningList> decommissioningLists = rm.searchDecommissioningLists(
+                where(rm.decommissioningList.folders()).isContaining(asList(folder.getId())));
+        for(DecommissioningList decommissioningList: decommissioningLists) {
+            decommissioningList.removeFolderDetail(folder.getId());
+
+            try {
+                recordServices.update(decommissioningList.getWrappedRecord());
+            } catch (RecordServicesException e) {
+                LOGGER.info("Could not unlink folder from decommissioningList");
+            }
+        }
+    }
+
+    static private void unlinkContainerFromDecommissioningLists(Record container, String collection, AppLayerFactory appLayerFactory) {
+
+        RMSchemasRecordsServices rm = new RMSchemasRecordsServices(collection, appLayerFactory);
+        RecordServices recordServices = appLayerFactory.getModelLayerFactory().newRecordServices();
+        List<DecommissioningList> decommissioningLists = rm.searchDecommissioningLists(
+                where(rm.decommissioningList.containers()).isContaining(asList(container.getId())));
+        for(DecommissioningList decommissioningList: decommissioningLists) {
+            decommissioningList.removeContainerDetail(container.getId());
+
+            try {
+                recordServices.update(decommissioningList.getWrappedRecord());
+            } catch (RecordServicesException e) {
+                LOGGER.info("Could not unlink container from decommissioningList");
+            }
+        }
+    }
+
+    static private void unlinkDocumentFromTasks(Record document, List<Record> taskList, String collection, AppLayerFactory appLayerFactory) {
+
+        RecordServices recordServices = appLayerFactory.getModelLayerFactory().newRecordServices();
+        for(Record task: taskList) {
+            MetadataSchema curTaskSchema = appLayerFactory.getModelLayerFactory().getMetadataSchemasManager()
+                    .getSchemaTypes(collection).getSchema(task.getSchemaCode());
+            List<String> linkedDocumentsIDs = task.get(curTaskSchema.getMetadata(RMTask.LINKED_DOCUMENTS));
+            linkedDocumentsIDs = new ArrayList<>(linkedDocumentsIDs);
+            if(linkedDocumentsIDs.contains(document.getId())) {
+                linkedDocumentsIDs.remove(document.getId());
+                task.set(curTaskSchema.getMetadata(RMTask.LINKED_DOCUMENTS), linkedDocumentsIDs);
+                try {
+                    recordServices.update(task);
+                } catch (RecordServicesException e) {
+                    LOGGER.info("Could not unlink document from task");
+                }
+            }
+        }
+    }
+
+    static private void unlinkFolderFromTasks(Record folder, List<Record> taskList, String collection, AppLayerFactory appLayerFactory) {
+
+        RecordServices recordServices = appLayerFactory.getModelLayerFactory().newRecordServices();
+        for(Record task: taskList) {
+            MetadataSchema curTaskSchema = appLayerFactory.getModelLayerFactory().getMetadataSchemasManager()
+                    .getSchemaTypes(collection).getSchema(task.getSchemaCode());
+            List<String> linkedDocumentsIDs = task.get(curTaskSchema.getMetadata(RMTask.LINKED_DOCUMENTS));
+            linkedDocumentsIDs = new ArrayList<>(linkedDocumentsIDs);
+            if(linkedDocumentsIDs.contains(folder.getId())) {
+                linkedDocumentsIDs.remove(folder.getId());
+                task.set(curTaskSchema.getMetadata(RMTask.LINKED_DOCUMENTS), linkedDocumentsIDs);
+                try {
+                    recordServices.update(task);
+                } catch (RecordServicesException e) {
+                    LOGGER.info("Could not unlink folder from task");
                 }
             }
         }
--- sdk/src/com/constellio/app/modules/rm/services/RMRecordDeletionServicesAcceptanceTest.java
+++ sdk/src/com/constellio/app/modules/rm/services/RMRecordDeletionServicesAcceptanceTest.java
@@ -3,20 +3,25 @@ package com.constellio.app.modules.rm.services;
 import com.constellio.app.modules.rm.RMTestRecords;
 import com.constellio.app.modules.rm.wrappers.ContainerRecord;
 import com.constellio.app.modules.rm.wrappers.DecommissioningList;
+import com.constellio.app.modules.rm.wrappers.RMTask;
+import com.constellio.app.modules.tasks.model.wrappers.Task;
+import com.constellio.app.modules.tasks.services.TasksSchemasRecordsServices;
 import com.constellio.model.entities.records.Record;
+import com.constellio.model.entities.schemas.MetadataSchema;
 import com.constellio.model.entities.schemas.Schemas;
+import com.constellio.model.services.records.RecordServices;
+import com.constellio.model.services.records.RecordServicesException;
 import com.constellio.model.services.search.SearchServices;
 import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
 import com.constellio.sdk.tests.ConstellioTest;
+import com.constellio.sdk.tests.annotations.InDevelopmentTest;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.anyConditions;
-import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.from;
-import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.where;
+import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.*;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,18 +31,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RMRecordDeletionServicesAcceptanceTest extends ConstellioTest{
     RMTestRecords records = new RMTestRecords(zeCollection);
     SearchServices searchServices;
+    RecordServices recordServices;
     RMSchemasRecordsServices rm;
 
     @Before
     public void setup() {
         prepareSystem(
-                withZeCollection().withConstellioRMModule().withConstellioESModule().withAllTestUsers()
+                withZeCollection().withConstellioRMModule().withConstellioESModule().withTasksModule().withAllTestUsers()
                         .withRMTest(records).withFoldersAndContainersOfEveryStatus().withDocumentsDecommissioningList()
         );
         inCollection(zeCollection).setCollectionTitleTo("Collection de test");
 
         rm = new RMSchemasRecordsServices(zeCollection, getAppLayerFactory());
         searchServices = getModelLayerFactory().newSearchServices();
+        recordServices = getModelLayerFactory().newRecordServices();
     }
 
     @Test
@@ -117,6 +124,86 @@ public class RMRecordDeletionServicesAcceptanceTest extends ConstellioTest{
         assertThat(numberOfDecomListContainingRecords).isEqualTo(0);
     }
 
+    @Test
+    public void whenCleaningAnAdministrativeUnitThenLinkedTaskAreDeleted()
+            throws Exception {
+        Record administrativeUnitContainingTasks = searchServices.searchSingleResult(from(rm.administrativeUnit.schema())
+                .where(rm.administrativeUnit.code()).isEqualTo("30C"));
+        Record document = searchServices.search(new LogicalSearchQuery().setCondition(
+                from(rm.document.schema()).where(Schemas.TITLE)
+                        .isEqualTo("Grenouille - Document procès verbal numérique avec un autre exemplaire"))).get(0);
+        Record folder = searchServices.search(new LogicalSearchQuery().setCondition(
+                from(rm.folder.schema()).where(Schemas.IDENTIFIER).isEqualTo("A49"))).get(0);
+
+        TasksSchemasRecordsServices taskSchemas = new TasksSchemasRecordsServices(zeCollection, getAppLayerFactory());
+        RMTask taskDocument = new RMTask(taskSchemas.newTask().setTitle("taskDocument")).setLinkedDocuments(asList(document.getId()))
+                .setAdministrativeUnit(administrativeUnitContainingTasks.getId());
+        RMTask taskFolder = new RMTask(taskSchemas.newTask().setTitle("taskFolder")).setLinkedFolders(asList(folder.getId()))
+                .setAdministrativeUnit(administrativeUnitContainingTasks.getId());
+        RMTask taskWithBoth = new RMTask(taskSchemas.newTask().setTitle("taskWithBoth")).setLinkedDocuments(asList(document.getId()))
+                .setLinkedFolders(asList(folder.getId())).setAdministrativeUnit(administrativeUnitContainingTasks.getId());
+        recordServices.add(taskDocument.getWrappedRecord());
+        recordServices.add(taskFolder.getWrappedRecord());
+        recordServices.add(taskWithBoth.getWrappedRecord());
+
+        RMRecordDeletionServices.cleanAdministrativeUnit(zeCollection, administrativeUnitContainingTasks.getId(), getAppLayerFactory());
+
+        Record newTaskDocument = searchServices.searchSingleResult(from(taskSchemas.userTask.schema())
+                .where(Schemas.IDENTIFIER).isEqualTo(taskDocument.getId()));
+        Record newTaskFolder = searchServices.searchSingleResult(from(taskSchemas.userTask.schema())
+                .where(Schemas.IDENTIFIER).isEqualTo(taskDocument.getId()));
+        Record newTaskWithBoth = searchServices.searchSingleResult(from(taskSchemas.userTask.schema())
+                .where(Schemas.IDENTIFIER).isEqualTo(taskDocument.getId()));
+        assertThat(newTaskDocument).isNull();
+        assertThat(newTaskFolder).isNull();
+        assertThat(newTaskWithBoth).isNull();
+    }
+
+    @Test
+    public void whenCleaningAnAdministrativeUnitThenTasksFromOtherUnitUnlinkReferencedDocsAndFolders()
+            throws Exception {
+        Record administrativeUnitContainingTasks = searchServices.searchSingleResult(from(rm.administrativeUnit.schema())
+                .where(rm.administrativeUnit.code()).isEqualTo("30C"));
+        Record administrativeUnitCleaned = searchServices.searchSingleResult(from(rm.administrativeUnit.schema())
+                .where(rm.administrativeUnit.code()).isEqualTo("10A"));
+        Record document = searchServices.search(new LogicalSearchQuery().setCondition(
+                from(rm.document.schema()).where(Schemas.TITLE)
+                        .isEqualTo("Grenouille - Document procès verbal numérique avec un autre exemplaire"))).get(0);
+        Record folder = searchServices.search(new LogicalSearchQuery().setCondition(
+                from(rm.folder.schema()).where(Schemas.IDENTIFIER).isEqualTo("A49"))).get(0);
+
+        TasksSchemasRecordsServices taskSchemas = new TasksSchemasRecordsServices(zeCollection, getAppLayerFactory());
+        RMTask taskDocument = new RMTask(taskSchemas.newTask().setTitle("taskDocument")).setLinkedDocuments(asList(document.getId()))
+                .setAdministrativeUnit(administrativeUnitContainingTasks.getId());
+        RMTask taskFolder = new RMTask(taskSchemas.newTask().setTitle("taskFolder")).setLinkedFolders(asList(folder.getId()))
+                .setAdministrativeUnit(administrativeUnitContainingTasks.getId());
+        RMTask taskWithBoth = new RMTask(taskSchemas.newTask().setTitle("taskWithBoth")).setLinkedDocuments(asList(document.getId()))
+                .setLinkedFolders(asList(folder.getId())).setAdministrativeUnit(administrativeUnitContainingTasks.getId());
+        recordServices.add(taskDocument.getWrappedRecord());
+        recordServices.add(taskFolder.getWrappedRecord());
+        recordServices.add(taskWithBoth.getWrappedRecord());
+
+        assertThat(getLinkedRecords(taskDocument)).hasSize(1);
+        assertThat(getLinkedRecords(taskFolder)).hasSize(1);
+        assertThat(getLinkedRecords(taskWithBoth)).hasSize(2);
+
+        RMRecordDeletionServices.cleanAdministrativeUnit(zeCollection, administrativeUnitCleaned.getId(), getAppLayerFactory());
+
+        Record newTaskDocument = searchServices.searchSingleResult(from(taskSchemas.userTask.schema())
+                .where(Schemas.IDENTIFIER).isEqualTo(taskDocument.getId()));
+        Record newTaskFolder = searchServices.searchSingleResult(from(taskSchemas.userTask.schema())
+                .where(Schemas.IDENTIFIER).isEqualTo(taskDocument.getId()));
+        Record newTaskWithBoth = searchServices.searchSingleResult(from(taskSchemas.userTask.schema())
+                .where(Schemas.IDENTIFIER).isEqualTo(taskDocument.getId()));
+
+        assertThat(newTaskDocument).isNotNull();
+        assertThat(newTaskFolder).isNotNull();
+        assertThat(newTaskWithBoth).isNotNull();
+        assertThat(getLinkedRecords(newTaskDocument)).isEmpty();
+        assertThat(getLinkedRecords(newTaskFolder)).isEmpty();
+        assertThat(getLinkedRecords(newTaskWithBoth)).isEmpty();
+    }
+
     public List<String> extractIdentifier(List<DecommissioningList> decommissioningLists) {
         List<String> identifierList = new ArrayList<>();
         for(DecommissioningList decommissioningList: decommissioningLists) {
@@ -133,5 +220,22 @@ public class RMRecordDeletionServicesAcceptanceTest extends ConstellioTest{
                 where(rm.decommissioningList.documents()).isContaining(documentIDs),
                 where(rm.decommissioningList.containers()).isContaining(containerIDs)
         ));
+    }
+
+    public List<String> getLinkedRecords(RMTask task) {
+        ArrayList<String> linkedRecords = new ArrayList<>(task.getLinkedDocuments());
+        linkedRecords.addAll(task.getLinkedFolders());
+        return linkedRecords;
+    }
+
+    public List<String> getLinkedRecords(Record task) {
+        MetadataSchema schema = getAppLayerFactory().getModelLayerFactory().getMetadataSchemasManager()
+                .getSchemaTypes(zeCollection).getSchema(task.getSchemaCode());
+        List<String> linkedDocuments = task.get(schema.getMetadata(RMTask.LINKED_DOCUMENTS));
+        List<String> linkedFolder = task.get(schema.getMetadata(RMTask.LINKED_DOCUMENTS));
+        ArrayList<String> linkedRecords = new ArrayList<>();
+        linkedRecords.addAll(linkedDocuments);
+        linkedRecords.addAll(linkedFolder);
+        return linkedRecords;
     }
 }
