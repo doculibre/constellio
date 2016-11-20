3630db07efb965b18bb01c73259deb67a998a61d
francisbaril <baril.francis@gmail.com>
2016-11-20 12:19:09 -0500

    unstable

---
+++ sdk/src/com/constellio/model/services/schemas/builders/MetadataBuilder_CustomAttributesTest.java
@@ -0,0 +1,78 @@
+package com.constellio.model.services.schemas.builders;
+
+import static com.constellio.model.entities.schemas.MetadataValueType.STRING;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class MetadataBuilder_CustomAttributesTest extends MetadataBuilderTest {
+
+	@Test
+	public void givenCustomAttributesUndefinedOnMetadataWithoutInheritanceWhenBuildingThenEmptySet()
+			throws Exception {
+		inheritedMetadataBuilder.setType(STRING);
+
+		build();
+
+		assertThat(metadataWithoutInheritance.getCustomAttributes()).isEmpty();
+	}
+
+	@Test
+	public void givenCustomAttributesUndefinedOnMetadataWithoutInheritanceWhenModifyingThenEmptySet()
+			throws Exception {
+		inheritedMetadataBuilder.setType(STRING);
+
+		buildAndModify();
+
+		assertThat(metadataWithoutInheritanceBuilder.getCustomAttributes()).isEmpty();
+	}
+
+	@Test
+	public void givenCustomAttributesDefinedOnMetadataWithoutInheritanceWhenBuildingThenDefined()
+			throws Exception {
+		metadataWithoutInheritanceBuilder.setType(STRING).addCustomAttribute("flag1").addCustomAttribute("flag2");
+
+		build();
+
+		assertThat(metadataWithoutInheritance.getCustomAttributes()).containsOnly("flag1", "flag2");
+	}
+
+	@Test
+	public void givenCustomAttributesDefinedOnMetadataWithoutInheritanceWhenModifyingThenDefined()
+			throws Exception {
+		metadataWithoutInheritanceBuilder.setType(STRING).addCustomAttribute("flag1").removeCustomAttribute("flag1");
+
+		buildAndModify();
+
+		assertThat(metadataWithoutInheritanceBuilder.getCustomAttributes()).isEmpty();
+	}
+
+	@Test
+	public void givenCustomAttributesOnMetadataWithInheritanceWhenBuildingThenSetToInheritedValue()
+			throws Exception {
+		inheritedMetadataBuilder.setType(STRING).addCustomAttribute("flag1").addCustomAttribute("flag2");
+
+		build();
+
+		assertThat(metadataWithInheritance.getCustomAttributes()).containsOnly("flag1", "flag2");
+	}
+
+	@Test
+	public void givenCustomAttributesOnMetadataWithInheritanceWhenModifyingThenSetToInheritedValue()
+			throws Exception {
+		inheritedMetadataBuilder.setType(STRING).addCustomAttribute("flag1").addCustomAttribute("flag2");
+
+		buildAndModify();
+
+		assertThat(metadataWithInheritanceBuilder.getCustomAttributes()).containsOnly("flag1", "flag2");
+	}
+
+	@Test
+	public void givenCustomAttributesModifiedInInheritedMetadataBuilderThenModifiedInMetadataWithHeritance()
+			throws Exception {
+		inheritedMetadataBuilder.setType(STRING).addCustomAttribute("flag1").addCustomAttribute("flag2");
+		assertThat(metadataWithInheritanceBuilder.getCustomAttributes()).containsOnly("flag1", "flag2");
+
+	}
+
+}
