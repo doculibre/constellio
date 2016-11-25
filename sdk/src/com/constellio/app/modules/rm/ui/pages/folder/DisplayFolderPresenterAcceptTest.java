128e252f9d5b59743276ff1fdc14b1fe4f1a580d
Gabriel <gabriel.lefrancois@constellio.com>
2016-11-25 10:11:25 -0500

    new alert when record checked in

--- sdk/src/com/constellio/app/modules/rm/ui/pages/folder/DisplayFolderPresenterAcceptTest.java
+++ sdk/src/com/constellio/app/modules/rm/ui/pages/folder/DisplayFolderPresenterAcceptTest.java
@@ -620,14 +620,18 @@ public class DisplayFolderPresenterAcceptTest extends ConstellioTest {
 		assertThat(emailToSend.getTo()).hasSize(1);
 		assertThat(emailToSend.getTo().get(0).getName()).isEqualTo(users.bobIn(zeCollection).getTitle());
 		assertThat(emailToSend.getTo().get(0).getEmail()).isEqualTo(users.bobIn(zeCollection).getEmail());
-		assertThat(emailToSend.getSubject()).isEqualTo("Alerte lorsque le folder est disponible " + folderC30.getTitle());
+		assertThat(emailToSend.getSubject()).isEqualTo("Le folder suivant est disponible: " + folderC30.getTitle());
 		assertThat(emailToSend.getTemplate()).isEqualTo(RMEmailTemplateConstants.ALERT_AVAILABLE_ID);
 		assertThat(emailToSend.getError()).isNull();
 		assertThat(emailToSend.getTryingCount()).isEqualTo(0);
-		assertThat(emailToSend.getParameters()).hasSize(2);
+		assertThat(emailToSend.getParameters()).hasSize(4);
 		assertThat(emailToSend.getParameters().get(0)).isEqualTo("returnDate" + EmailToSend.PARAMETER_SEPARATOR + shishOClock);
 		assertThat(emailToSend.getParameters().get(1))
 				.isEqualTo("title" + EmailToSend.PARAMETER_SEPARATOR + folderC30.getTitle());
+		assertThat(emailToSend.getParameters().get(2))
+				.isEqualTo("constellioURL" + EmailToSend.PARAMETER_SEPARATOR + "http://localhost:8080/constellio/");
+		assertThat(emailToSend.getParameters().get(3))
+				.isEqualTo("recordURL" + EmailToSend.PARAMETER_SEPARATOR + "http://localhost:8080/constellio/#!displayFolder/C30");
 	}
 
 	//
