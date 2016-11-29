a74ea2feab9a46a2a29d431fc2f8a1681b93eeea
Nicolas Bélisle <nicolas.belisle@doculibre.com>
2016-11-29 10:18:30 -0500

    New plugin 023 and administrative unit report.

--- app/src/com/constellio/app/modules/rm/extensions/api/reports/RMReportBuilderFactories.java
+++ app/src/com/constellio/app/modules/rm/extensions/api/reports/RMReportBuilderFactories.java
@@ -1,10 +1,10 @@
 package com.constellio.app.modules.rm.extensions.api.reports;
 
-import static com.constellio.data.frameworks.extensions.SingleValueExtension.DEFAULT_VALUE;
-
 import com.constellio.app.modules.rm.model.enums.DecommissioningType;
+import com.constellio.app.modules.rm.reports.builders.administration.plan.AdministrativeUnitReportParameters;
 import com.constellio.app.modules.rm.reports.builders.decommissioning.ContainerRecordReportFactory;
-import com.constellio.app.modules.rm.reports.builders.decommissioning.ContainerRecordReportFactory.ContainerRecordReportParameters;
+import com.constellio.app.modules.rm.reports.builders.decommissioning.ContainerRecordReportParameters;
+import com.constellio.app.modules.rm.wrappers.AdministrativeUnit;
 import com.constellio.app.modules.rm.wrappers.ContainerRecord;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.app.ui.framework.reports.NewReportWriterFactory;
@@ -12,13 +12,20 @@ import com.constellio.app.ui.framework.reports.ReportWriterFactory;
 import com.constellio.data.frameworks.extensions.SingleValueExtension;
 import com.constellio.data.utils.Provider;
 
+import static com.constellio.data.frameworks.extensions.SingleValueExtension.DEFAULT_VALUE;
+
 public class RMReportBuilderFactories {
 
 	public SingleValueExtension<NewReportWriterFactory<ContainerRecordReportParameters>> transferContainerRecordBuilderFactory = new SingleValueExtension<>();
 
+	public SingleValueExtension<NewReportWriterFactory<AdministrativeUnitReportParameters>> administrativeUnitRecordBuilderFactory = new SingleValueExtension<>();
+
 	public RMReportBuilderFactories(AppLayerFactory appLayerFactory) {
 		//TODO Nicolas : Déplacer ce register dans le plugin des rapports
 		transferContainerRecordBuilderFactory.register(DEFAULT_VALUE, new ContainerRecordReportFactory(appLayerFactory));
+
+		//completed
+		//administrativeUnitRecordBuilderFactory.register(DEFAULT_VALUE, new AdministrativeUnitReportFactory(appLayerFactory));
 	}
 
 	/**
@@ -40,5 +47,4 @@ public class RMReportBuilderFactories {
 			return getWrappedRecord().getDecommissioningType() == DecommissioningType.TRANSFERT_TO_SEMI_ACTIVE;
 		}
 	}
-
 }
---
+++ app/src/com/constellio/app/modules/rm/ui/pages/reports/RMNewReportsPresenter.java
@@ -0,0 +1,126 @@
+package com.constellio.app.modules.rm.ui.pages.reports;
+
+import com.constellio.app.extensions.AppLayerCollectionExtensions;
+import com.constellio.app.modules.rm.ConstellioRMModule;
+import com.constellio.app.modules.rm.constants.RMPermissionsTo;
+import com.constellio.app.modules.rm.extensions.api.RMModuleExtensions;
+import com.constellio.app.modules.rm.navigation.RMViews;
+import com.constellio.app.modules.rm.reports.builders.administration.plan.AdministrativeUnitReportParameters;
+import com.constellio.app.modules.rm.wrappers.AdministrativeUnit;
+import com.constellio.app.ui.framework.components.NewReportPresenter;
+import com.constellio.app.ui.framework.reports.NewReportWriterFactory;
+import com.constellio.app.ui.pages.base.BasePresenter;
+import com.constellio.model.entities.records.wrappers.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RMNewReportsPresenter extends BasePresenter<RMReportsView> implements NewReportPresenter {
+
+	private static final boolean BY_ADMINISTRATIVE_UNIT = true;
+	private String schemaTypeValue;
+
+	public RMNewReportsPresenter(RMReportsView view) {
+		super(view);
+	}
+
+	@Override
+	public List<String> getSupportedReports() {
+		return Arrays.asList("Reports.ClassificationPlan",
+				"Reports.DetailedClassificationPlan",
+				"Reports.ClassificationPlanByAdministrativeUnit",
+				"Reports.ConservationRulesList",
+				"Reports.ConservationRulesListByAdministrativeUnit",
+				"Reports.AdministrativeUnits",
+				"Reports.AdministrativeUnitsAndUsers",
+				"Reports.Users");
+	}
+
+	public NewReportWriterFactory getReport(String report) {
+		AppLayerCollectionExtensions appCollectionExtentions = appLayerFactory.getExtensions().forCollection(collection);
+		RMModuleExtensions rmModuleExtensions = appCollectionExtentions.forModule(ConstellioRMModule.ID);
+
+		switch (report) {
+		case "Reports.fakeReport2":
+			//return new ExampleReportFactoryWithoutRecords();
+		case "Reports.ClassificationPlan":
+			//return new ClassificationPlanReportViewImpl(false);
+		case "Reports.DetailedClassificationPlan":
+			//return new ClassificationPlanReportViewImpl(true);
+		case "Reports.ConservationRulesList":
+			///return new ConservationRulesReportViewImpl();
+		case "Reports.ConservationRulesListByAdministrativeUnit":
+			//return new ConservationRulesReportViewImpl(BY_ADMINISTRATIVE_UNIT, schemaTypeValue);
+		case "Reports.AdministrativeUnits":
+			return rmModuleExtensions.getReportBuilderFactories().administrativeUnitRecordBuilderFactory.getValue();
+		case "Reports.AdministrativeUnitsAndUsers":
+			return rmModuleExtensions.getReportBuilderFactories().administrativeUnitRecordBuilderFactory.getValue();
+		case "Reports.Users":
+			//return new UserReportViewImpl();
+		case "Reports.ClassificationPlanByAdministrativeUnit":
+			//return new ClassificationPlanReportViewImpl(false, schemaTypeValue);
+		}
+
+		throw new RuntimeException("BUG: Unknown report: " + report);
+
+	}
+
+	@Override
+	public Object getReportParameters(String report) {
+		switch (report) {
+			case "Reports.fakeReport2":
+				//return new ExampleReportFactoryWithoutRecords();
+			case "Reports.ClassificationPlan":
+				//return new ClassificationPlanReportViewImpl(false);
+			case "Reports.DetailedClassificationPlan":
+				//return new ClassificationPlanReportViewImpl(true);
+			case "Reports.ConservationRulesList":
+				///return new ConservationRulesReportViewImpl();
+			case "Reports.ConservationRulesListByAdministrativeUnit":
+				//return new ConservationRulesReportViewImpl(BY_ADMINISTRATIVE_UNIT, schemaTypeValue);
+			case "Reports.AdministrativeUnits":
+				return new AdministrativeUnitReportParameters(false);
+			case "Reports.AdministrativeUnitsAndUsers":
+				return new AdministrativeUnitReportParameters(true);
+			case "Reports.Users":
+				//return new UserReportViewImpl();
+			case "Reports.ClassificationPlanByAdministrativeUnit":
+				//return new ClassificationPlanReportViewImpl(false, schemaTypeValue);
+		}
+
+		throw new RuntimeException("BUG: Unknown report: " + report);
+	}
+
+	public boolean isWithSchemaType(String report) {
+		switch (report) {
+		case "Reports.ConservationRulesListByAdministrativeUnit":
+		case "Reports.ClassificationPlanByAdministrativeUnit":
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	public String getSchemaTypeValue(String report) {
+		switch (report) {
+		case "Reports.ConservationRulesListByAdministrativeUnit":
+		case "Reports.ClassificationPlanByAdministrativeUnit":
+			return AdministrativeUnit.SCHEMA_TYPE;
+		default:
+			return null;
+		}
+	}
+
+	public void setSchemaTypeValue(String schemaTypeValue) {
+		this.schemaTypeValue = schemaTypeValue;
+	}
+
+	public void backButtonClicked() {
+		view.navigate().to(RMViews.class).archiveManagement();
+	}
+
+	@Override
+	protected boolean hasPageAccess(String params, User user) {
+		return user.has(RMPermissionsTo.MANAGE_REPORTS).globally();
+	}
+}
