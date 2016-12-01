7cfb5b7abfea92cc7b57b41bdd5bccc2d1b392db
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2016-12-01 13:21:00 -0500

    fix date format

--- app/src/com/constellio/app/modules/rm/extensions/RMCheckInAlertsRecordExtension.java
+++ app/src/com/constellio/app/modules/rm/extensions/RMCheckInAlertsRecordExtension.java
@@ -102,7 +102,7 @@ public class RMCheckInAlertsRecordExtension extends RecordExtension {
 					emailToSend.setTemplate(RMEmailTemplateConstants.ALERT_AVAILABLE_ID);
 					List<String> parameters = new ArrayList<>();
 					parameters.add("subject" + EmailToSend.PARAMETER_SEPARATOR + subject);
-					parameters.add("returnDate" + EmailToSend.PARAMETER_SEPARATOR + returnDate);
+					parameters.add("returnDate" + EmailToSend.PARAMETER_SEPARATOR + returnDate.toString("yyyy-MM-dd  HH:mm:ss"));
 					String rmObjectTitle = rmObject.getTitle();
 					parameters.add("title" + EmailToSend.PARAMETER_SEPARATOR + rmObjectTitle);
 					String constellioUrl = eimConfigs.getConstellioUrl();
