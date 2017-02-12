6aaf88a73a763dbf0d1cc883677dad436db6477f
dakota.indien <dakota.indien@gmail.com>
2017-02-12 08:07:12 -0500

    fix

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo_7_1_francis.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo_7_1_francis.java
@@ -8,6 +8,7 @@ import com.constellio.app.entities.modules.MigrationResourcesProvider;
 import com.constellio.app.entities.modules.MigrationScript;
 import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypeBuilder;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
 
@@ -32,23 +33,23 @@ public class RMMigrationTo_7_1_francis implements MigrationScript {
 
 		@Override
 		protected void migrate(MetadataSchemaTypesBuilder typesBuilder) {
-			MetadataSchemaTypeBuilder folderSchemaType = typesBuilder.getSchemaType(Folder.SCHEMA_TYPE);
+			MetadataSchemaBuilder folderSchema = typesBuilder.getSchema(Folder.DEFAULT_SCHEMA);
 
 			//TODO : Tester si ces métadonnées peuvent être rendu volatiles
-			folderSchemaType.getMetadata(Folder.APPLICABLE_COPY_RULES).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.ACTIVE_RETENTION_CODE).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.ACTIVE_RETENTION_TYPE).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.SEMIACTIVE_RETENTION_CODE).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.SEMIACTIVE_RETENTION_TYPE).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.INACTIVE_DISPOSAL_TYPE).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.COPY_RULES_EXPECTED_TRANSFER_DATES).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.COPY_RULES_EXPECTED_DEPOSIT_DATES).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.COPY_RULES_EXPECTED_DESTRUCTION_DATES).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.PERMISSION_STATUS).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.MEDIA_TYPE).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.MEDIUM_TYPES).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.MAIN_COPY_RULE).setVolatility(VOLATILE_EAGER);
-			folderSchemaType.getMetadata(Folder.DECOMMISSIONING_DATE).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.APPLICABLE_COPY_RULES).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.ACTIVE_RETENTION_CODE).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.ACTIVE_RETENTION_TYPE).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.SEMIACTIVE_RETENTION_CODE).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.SEMIACTIVE_RETENTION_TYPE).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.INACTIVE_DISPOSAL_TYPE).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.COPY_RULES_EXPECTED_TRANSFER_DATES).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.COPY_RULES_EXPECTED_DEPOSIT_DATES).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.COPY_RULES_EXPECTED_DESTRUCTION_DATES).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.PERMISSION_STATUS).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.MEDIA_TYPE).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.MEDIUM_TYPES).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.MAIN_COPY_RULE).setVolatility(VOLATILE_EAGER);
+			folderSchema.getMetadata(Folder.DECOMMISSIONING_DATE).setVolatility(VOLATILE_EAGER);
 		}
 	}
 
