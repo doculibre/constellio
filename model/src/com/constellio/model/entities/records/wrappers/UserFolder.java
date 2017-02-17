ac06ab6950d0d6ce067721fdf66991370fbe5f24
Marco <ncix123$>
2017-02-17 10:14:43 -0500

    Correction du parent dans userFolder

--- model/src/com/constellio/model/entities/records/wrappers/UserFolder.java
+++ model/src/com/constellio/model/entities/records/wrappers/UserFolder.java
@@ -12,19 +12,19 @@ public class UserFolder extends RecordWrapper {
 
     public static final String DEFAULT_SCHEMA = SCHEMA_TYPE + "_default";
 
-    public static final String USER_FOLDER = "userFolder";
+    public static final String PARENT = "parent";
 
 
     public UserFolder(Record record, MetadataSchemaTypes types, String typeRequirement) {
         super(record, types, typeRequirement);
     }
 
-    public UserFolder getUserFolder() {
-        return get(USER_FOLDER);
+    public UserFolder getParent() {
+        return get(PARENT);
     }
 
-    public UserFolder setUserFolder(UserFolder userFolder) {
-        set(USER_FOLDER, userFolder);
+    public UserFolder setParent(UserFolder userFolder) {
+        set(PARENT, userFolder);
         return this;
     }
 }
