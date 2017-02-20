41be611585b1dbe8c1c629e0a0b199c3a7db1fcf
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2017-02-19 22:06:09 -0500

    unstable: fixed container StorageSpace buttons

--- app/src/com/constellio/app/modules/rm/ui/components/container/fields/ContainerStorageSpaceLookupField.java
+++ app/src/com/constellio/app/modules/rm/ui/components/container/fields/ContainerStorageSpaceLookupField.java
@@ -2,15 +2,14 @@ package com.constellio.app.modules.rm.ui.components.container.fields;
 
 import com.constellio.app.modules.rm.ui.components.folder.fields.CustomFolderField;
 import com.constellio.app.modules.rm.wrappers.StorageSpace;
-import com.constellio.app.modules.rm.wrappers.type.ContainerRecordType;
 import com.constellio.app.services.factories.ConstellioFactories;
 import com.constellio.app.ui.application.ConstellioUI;
 import com.constellio.app.ui.entities.UserVO;
-import com.constellio.app.ui.framework.buttons.WindowButton;
 import com.constellio.app.ui.framework.components.converters.RecordIdToCaptionConverter;
 import com.constellio.app.ui.framework.components.fields.lookup.LookupRecordField;
 import com.constellio.app.ui.framework.data.RecordLookupTreeDataProvider;
 import com.constellio.app.ui.framework.data.RecordTextInputDataProvider;
+import com.constellio.app.ui.framework.data.trees.LinkableRecordTreeNodesDataProvider;
 import com.constellio.app.ui.pages.base.SessionContext;
 import com.constellio.model.entities.Taxonomy;
 import com.constellio.model.entities.records.Record;
@@ -19,14 +18,13 @@ import com.constellio.model.entities.schemas.MetadataSchemaType;
 import com.constellio.model.services.factories.ModelLayerFactory;
 import com.constellio.model.services.schemas.MetadataSchemasManager;
 import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
+import com.constellio.model.services.search.query.logical.condition.LogicalSearchCondition;
 import com.constellio.model.services.taxonomies.TaxonomiesManager;
-import com.constellio.model.services.taxonomies.TaxonomySearchRecord;
+import com.constellio.model.services.taxonomies.TaxonomiesSearchFilter;
 import com.constellio.model.services.users.UserServices;
-import com.vaadin.data.Property;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Window;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
@@ -46,6 +44,7 @@ public class ContainerStorageSpaceLookupField extends LookupRecordField implemen
     public ContainerStorageSpaceLookupField(String containerRecordType) {
         this(StorageSpace.SCHEMA_TYPE, null, containerRecordType);
         this.containerRecordType =  containerRecordType;
+
     }
 
     private ContainerStorageSpaceLookupField(String schemaTypeCode, String schemaCode, String containerRecordType) {
@@ -61,71 +60,8 @@ public class ContainerStorageSpaceLookupField extends LookupRecordField implemen
     @Override
     protected Component initContent() {
         HorizontalLayout horizontalLayout = ((HorizontalLayout) super.initContent());
-        horizontalLayout.addComponent(buildNewLookupButton(), 2);
-        horizontalLayout.addComponent(buildNewLookupButton2(), 3);
+        horizontalLayout.addComponent(buildNewLookupButton2(), 2);
         return horizontalLayout;
-    }
-
-    private Component buildNewLookupButton() {
-        final WindowButton lookupButton = new WindowButton($("ContainerStorageLookupField.suggested"), $("ContainerStorageLookupField.suggested")) {
-            @Override
-            protected Component buildWindowContent() {
-                return new LookupWindowContent(getWindow()) {
-                    @Override
-                    public List<LookupTreeDataProvider<String>> getLookupTreeDataProviders() {
-                        return asList();
-                    }
-
-                    @Override
-                    public TextInputDataProvider geSuggestInputDataProvider() {
-                        ConstellioFactories constellioFactories = ConstellioFactories.getInstance();
-                        SessionContext sessionContext = ConstellioUI.getCurrentSessionContext();
-
-                        return new RecordTextInputDataProvider(constellioFactories, sessionContext, StorageSpace.SCHEMA_TYPE, false) {
-                            @Override
-                            public List<String> getData(String text, int startIndex, int count) {
-                                return getModelLayerFactory().newSearchServices().searchRecordIds(buildQuery().setStartRow(startIndex).setNumberOfRows(count));
-                            }
-
-                            @Override
-                            public int size(String text) {
-                                return (int) getModelLayerFactory().newSearchServices().getResultsCount(buildQuery());
-                            }
-
-                            private LogicalSearchQuery buildQuery() {
-                                MetadataSchemaType storageSpaceType = getModelLayerFactory().getMetadataSchemasManager()
-                                        .getSchemaTypes(getCurrentCollection()).getSchemaType(schemaTypeCode);
-                                MetadataSchemaType containerTypeType = getModelLayerFactory().getMetadataSchemasManager()
-                                        .getSchemaTypes(getCurrentCollection()).getSchemaType(ContainerRecordType.SCHEMA_TYPE);
-                                return new LogicalSearchQuery().setCondition(from(storageSpaceType).whereAllConditions(
-                                        where(storageSpaceType.getDefaultSchema().get(StorageSpace.NUMBER_OF_CHILD)).isEqualTo(0),
-                                        where(storageSpaceType.getDefaultSchema().get(StorageSpace.AVAILABLE_SIZE)).isGreaterOrEqualThan(0),
-                                        anyConditions(
-                                                where(storageSpaceType.getDefaultSchema().get(StorageSpace.CONTAINER_TYPE)).isContaining(asList(containerRecordType)),
-                                                where(storageSpaceType.getDefaultSchema().get(StorageSpace.CONTAINER_TYPE)).isNull()
-                                        )
-                                ));
-                            }
-                        };
-                    }
-                };
-            }
-
-
-        };
-        lookupButton.addStyleName(OPEN_WINDOW_BUTTON_STYLE_NAME);
-
-        addValueChangeListener(new ValueChangeListener() {
-            @Override
-            public void valueChange(Property.ValueChangeEvent event) {
-                if (lookupButton.getWindow() != null) {
-                    Window lookupWindow = lookupButton.getWindow();
-                    lookupWindow.close();
-                }
-            }
-        });
-
-        return lookupButton;
     }
 
     private Component buildNewLookupButton2() {
@@ -194,28 +130,27 @@ public class ContainerStorageSpaceLookupField extends LookupRecordField implemen
         for (Taxonomy taxonomy : taxonomies) {
             String taxonomyCode = taxonomy.getCode();
             if (StringUtils.isNotBlank(taxonomyCode)) {
-                dataProviders.add(new RecordLookupTreeDataProvider(schemaTypeCode, taxonomyCode, writeAccess) {
-                    @Override
-                    protected void saveResultInCache(TaxonomySearchRecord searchRecord) {
-                        StorageSpace storageSpace = new StorageSpace(searchRecord.getRecord(),
-                                modelLayerFactory.getMetadataSchemasManager().getSchemaTypes(collection));
-                    }
-
-                    @Override
-                    public boolean isSelectable(String selection) {
-                        StorageSpace storageSpace = new StorageSpace(getRecord(selection),
-                                modelLayerFactory.getMetadataSchemasManager().getSchemaTypes(collection));
-
-                        return (storageSpace.getNumberOfChild().equals(0) && storageSpace.getAvailableSize() > 0 &&
-                                (storageSpace.getContainerType() == null || storageSpace.getContainerType().isEmpty() || storageSpace.getContainerType().contains(containerRecordType)));
-                    }
-
-                    public Record getRecord(String id) {
-                        return modelLayerFactory.newRecordServices().getDocumentById(id);
-                    }
-                });
+                dataProviders.add(new RecordLookupTreeDataProvider(schemaTypeCode, writeAccess, getDataProvider(taxonomyCode, containerRecordType)));
             }
         }
         return !dataProviders.isEmpty() ? dataProviders.toArray(new RecordLookupTreeDataProvider[0]) : null;
+    }
+
+    static public LinkableRecordTreeNodesDataProvider getDataProvider(String taxonomyCode, String containerRecordType) {
+        ConstellioFactories constellioFactories = ConstellioFactories.getInstance();
+        SessionContext sessionContext = ConstellioUI.getCurrentSessionContext();
+        MetadataSchemaType storageSpaceType = constellioFactories.getModelLayerFactory().getMetadataSchemasManager()
+                .getSchemaTypes(sessionContext.getCurrentCollection()).getSchemaType(StorageSpace.SCHEMA_TYPE);
+        LogicalSearchCondition searchCondition = from(storageSpaceType).whereAllConditions(
+                where(storageSpaceType.getDefaultSchema().get(StorageSpace.NUMBER_OF_CHILD)).isEqualTo(0),
+                where(storageSpaceType.getDefaultSchema().get(StorageSpace.AVAILABLE_SIZE)).isGreaterOrEqualThan(0),
+                anyConditions(
+                        where(storageSpaceType.getDefaultSchema().get(StorageSpace.CONTAINER_TYPE)).isContaining(asList(containerRecordType)),
+                        where(storageSpaceType.getDefaultSchema().get(StorageSpace.CONTAINER_TYPE)).isNull()
+                ));
+        TaxonomiesSearchFilter taxonomiesSearchFilter = new TaxonomiesSearchFilter();
+        taxonomiesSearchFilter.setLinkableConceptsCondition(searchCondition);
+
+        return new LinkableRecordTreeNodesDataProvider(taxonomyCode, StorageSpace.SCHEMA_TYPE, false, taxonomiesSearchFilter);
     }
 }
