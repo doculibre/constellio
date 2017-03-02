da3b33351a2e78cab21ec497c327e0104d7c1fa5
Charles <charles.blanchette@doculibre.com>
2017-03-02 11:01:19 -0500

    Merge de Plugin024-charles dans master et préparation pour le push

--- model/src/com/constellio/model/entities/schemas/Schemas.java
+++ model/src/com/constellio/model/entities/schemas/Schemas.java
@@ -54,6 +54,9 @@ public class Schemas {
 	public static final Metadata LOGICALLY_DELETED_ON = add(new Metadata("logicallyDeletedOn_dt", DATE_TIME, false));
 	public static final Metadata ERROR_ON_PHYSICAL_DELETION = add(new Metadata("errorOnPhysicalDeletion_s", BOOLEAN, false));
 
+	public static final Metadata SEARCH_FIELD = add(new Metadata("search_txt", TEXT, true));
+	public static final Metadata FRENCH_SEARCH_FIELD = add(new Metadata("search_txt_fr", TEXT, true));
+	public static final Metadata ENGLISH_SEARCH_FIELD = add(new Metadata("search_txt_en", TEXT, true));
 	public static final Metadata SPELL_CHECK_FIELD = add(new Metadata("_spell_text", TEXT, true));
 
 	//TODO : Remove
