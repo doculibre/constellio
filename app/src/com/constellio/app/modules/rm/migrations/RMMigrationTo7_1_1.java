13ab3c2ade7ea7eeeacf92d17972edfd86d2c4ba
Charles <charles.blanchette@doculibre.com>
2017-03-10 14:49:44 -0500

    Ajout schéma borrowing

---
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo7_1_1.java
@@ -0,0 +1,56 @@
+package com.constellio.app.modules.rm.migrations;
+
+import static com.constellio.model.entities.schemas.MetadataValueType.DATE_TIME;
+import static com.constellio.model.entities.schemas.MetadataValueType.STRING;
+
+import com.constellio.app.entities.modules.MetadataSchemasAlterationHelper;
+import com.constellio.app.entities.modules.MigrationHelper;
+import com.constellio.app.entities.modules.MigrationResourcesProvider;
+import com.constellio.app.entities.modules.MigrationScript;
+import com.constellio.app.modules.rm.wrappers.Borrowing;
+import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.model.entities.records.wrappers.Event;
+import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
+
+/**
+ * Created by Charles Blanchette on 2017-03-10.
+ */
+public class RMMigrationTo7_1_1 extends MigrationHelper implements MigrationScript {
+
+    @Override
+    public String getVersion() {
+        return "7.1.1";
+    }
+
+    @Override
+    public void migrate(String collection, MigrationResourcesProvider provider, AppLayerFactory factory)
+            throws Exception {
+        new SchemaAlterationsFor7_1_1(collection, provider, factory).migrate();
+    }
+
+    public static class SchemaAlterationsFor7_1_1 extends MetadataSchemasAlterationHelper {
+
+        protected SchemaAlterationsFor7_1_1(String collection, MigrationResourcesProvider provider, AppLayerFactory factory) {
+            super(collection, provider, factory);
+        }
+
+        @Override
+        protected void migrate(MetadataSchemaTypesBuilder typesBuilder) {
+            MetadataSchemaBuilder builder = typesBuilder.getSchemaType(Event.SCHEMA_TYPE)
+                    .createCustomSchema(Borrowing.SCHEMA_TYPE);
+
+            builder.create(Borrowing.REQUEST_DATE).setType(DATE_TIME).setEssential(true).defineDataEntry().asManual();
+
+            builder.create(Borrowing.BORROWING_DATE).setType(DATE_TIME).setEssential(true).defineDataEntry().asManual();
+
+            builder.create(Borrowing.RETURN_DATE).setType(DATE_TIME).setEssential(true).defineDataEntry().asManual();
+
+            builder.create(Borrowing.RETURN_USERNAME).setType(STRING).setEssential(true).defineDataEntry().asManual();
+
+            builder.create(Borrowing.RETURN_USER_ID).setType(STRING).setEssential(true).defineDataEntry().asManual();
+        }
+
+    }
+
+}
---
+++ app/src/com/constellio/app/modules/rm/wrappers/Borrowing.java
@@ -0,0 +1,68 @@
+package com.constellio.app.modules.rm.wrappers;
+
+import com.constellio.model.entities.records.Record;
+import com.constellio.model.entities.records.wrappers.Event;
+import com.constellio.model.entities.schemas.MetadataSchemaTypes;
+import org.joda.time.LocalDateTime;
+
+/**
+ * Created by Charles Blanchette on 2017-03-10.
+ */
+public class Borrowing extends Event {
+    public static final String SCHEMA_TYPE = "borrowing";
+    public static final String DEFAULT_SCHEMA = SCHEMA_TYPE + "_default";
+    public static final String REQUEST_DATE = "requestDate";
+    public static final String BORROWING_DATE = "borrowingDate";
+    public static final String RETURN_DATE = "returnDate";
+    public static final String RETURN_USERNAME = "returnUsername";
+    public static final String RETURN_USER_ID = "returnUser";
+
+    public Borrowing(Record record, MetadataSchemaTypes types) {
+        super(record, types);
+    }
+
+    public Borrowing setRequestDate(LocalDateTime requestDate) {
+        set(REQUEST_DATE, requestDate);
+        return this;
+    }
+
+    public String getRequestDate() {
+        return get(REQUEST_DATE);
+    }
+
+    public Borrowing setBorrowingDate(LocalDateTime borrowingDate) {
+        set(BORROWING_DATE, borrowingDate);
+        return this;
+    }
+
+    public String getBorrowingDate() {
+        return get(BORROWING_DATE);
+    }
+
+    public Borrowing setReturnDate(LocalDateTime returnDate) {
+        set(RETURN_DATE, returnDate);
+        return this;
+    }
+
+    public String getReturnDate() {
+        return get(RETURN_DATE);
+    }
+
+    public Borrowing setReturnUsername(String returnUsername) {
+        set(RETURN_USERNAME, returnUsername);
+        return this;
+    }
+
+    public String getReturnUsername() {
+        return get(RETURN_USERNAME);
+    }
+
+    public Borrowing setReturnUserId(String returnUserId){
+        set(RETURN_USER_ID, returnUserId);
+        return this;
+    }
+
+    public String getReturnUserId(){
+        return get(RETURN_USER_ID);
+    }
+}
