0089ff548b38ff0ae8716724cd8b7f0248d7503b
Maxime Cote <maxime.cote@doculibre.com>
2017-03-27 17:17:33 -0400

    Change for Java 8, new intellij, new Gradle and SonarQube

--- app/src/com/constellio/app/ui/pages/management/labels/LabelFormImpl.java
+++ app/src/com/constellio/app/ui/pages/management/labels/LabelFormImpl.java
@@ -1,14 +1,11 @@
 package com.constellio.app.ui.pages.management.labels;
 
-import com.constellio.app.modules.rm.ui.components.document.DocumentForm;
-import com.constellio.app.modules.rm.ui.components.document.fields.CustomDocumentField;
 import com.constellio.app.services.factories.ConstellioFactories;
 import com.constellio.app.ui.application.ConstellioUI;
 import com.constellio.app.ui.entities.RecordVO;
 import com.constellio.app.ui.framework.components.RecordFieldFactory;
 import com.constellio.app.ui.framework.components.RecordForm;
 import com.constellio.app.ui.pages.base.SessionContext;
-import com.google.gwt.thirdparty.javascript.rhino.head.ast.Label;
 
 /**
  * Created by Marco on 2017-01-20.
