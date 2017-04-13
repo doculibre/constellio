3f770ef38776fce882b4aba31665641a900a50dc
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2017-04-13 10:39:59 -0400

    fixed compilation error

--- sdk/src/com/constellio/app/modules/rm/extensions/RMSelectionPanelExtensionAcceptanceTest.java
+++ sdk/src/com/constellio/app/modules/rm/extensions/RMSelectionPanelExtensionAcceptanceTest.java
@@ -126,7 +126,7 @@ public class RMSelectionPanelExtensionAcceptanceTest extends ConstellioTest {
         List<String> existingIds = getModelLayerFactory().newSearchServices().searchRecordIds(new LogicalSearchQuery().setCondition(fromAllSchemasIn(zeCollection).
                 where(Schemas.PATH).isStartingWithText(records.getFolder_A20().getPaths().get(0))
         ));
-        extension.classifyButtonClicked(records.folder_A20, "", true, param);
+        extension.classifyButtonClicked(records.folder_A20, "", records.ruleId_1, true, param);
 
         List<Record> recordList = getModelLayerFactory().newSearchServices().search(new LogicalSearchQuery().setCondition(fromAllSchemasIn(zeCollection)
                 .whereAllConditions(
@@ -149,7 +149,7 @@ public class RMSelectionPanelExtensionAcceptanceTest extends ConstellioTest {
         List<String> existingIds = getModelLayerFactory().newSearchServices().searchRecordIds(new LogicalSearchQuery().setCondition(fromAllSchemasIn(zeCollection).
                 where(Schemas.PATH).isStartingWithText(records.getCategory_X().getPaths().get(0))
         ));
-        extension.classifyButtonClicked("", records.categoryId_X, false, param);
+        extension.classifyButtonClicked("", records.categoryId_X, records.ruleId_1, false, param);
 
         List<Record> recordList = getModelLayerFactory().newSearchServices().search(new LogicalSearchQuery().setCondition(fromAllSchemasIn(zeCollection)
                 .whereAllConditions(
