89a78c0e187c621d200ed48f84b43ecf32053f4d
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2017-04-18 16:16:30 -0400

    Jira 687

--- app/src/com/constellio/app/ui/pages/base/ConstellioHeaderPresenter.java
+++ app/src/com/constellio/app/ui/pages/base/ConstellioHeaderPresenter.java
@@ -16,6 +16,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
+import com.constellio.model.entities.schemas.*;
 import org.apache.commons.lang3.StringUtils;
 
 import com.constellio.app.api.extensions.params.AvailableActionsParam;
@@ -56,10 +57,6 @@ import com.constellio.model.entities.Language;
 import com.constellio.model.entities.records.Record;
 import com.constellio.model.entities.records.Transaction;
 import com.constellio.model.entities.records.wrappers.User;
-import com.constellio.model.entities.schemas.Metadata;
-import com.constellio.model.entities.schemas.MetadataSchemaType;
-import com.constellio.model.entities.schemas.MetadataSchemaTypes;
-import com.constellio.model.entities.schemas.Schemas;
 import com.constellio.model.services.factories.ModelLayerFactory;
 import com.constellio.model.services.records.RecordServices;
 import com.constellio.model.services.records.RecordServicesException;
@@ -179,8 +176,9 @@ public class ConstellioHeaderPresenter implements SearchCriteriaPresenter {
 		result.add(builder.build(schemaType.getMetadataWithAtomicCode(CommonMetadataBuilder.PATH), header.getSessionContext()));
 		for (Metadata metadata : schemaType.getAllMetadatas()) {
 			if (!schemaType.hasSecurity() || metadataLocalCodes.contains(metadata.getLocalCode())) {
+				boolean isTextOrString = metadata.getType() == MetadataValueType.STRING ||  metadata.getType() == MetadataValueType.TEXT;
 				MetadataDisplayConfig config = schemasDisplayManager().getMetadata(header.getCollection(), metadata.getCode());
-				if (config.isVisibleInAdvancedSearch()) {
+				if (config.isVisibleInAdvancedSearch() && (!isTextOrString || isTextOrString && metadata.isSearchable())) {
 					result.add(builder.build(metadata, header.getSessionContext()));
 				}
 			}
