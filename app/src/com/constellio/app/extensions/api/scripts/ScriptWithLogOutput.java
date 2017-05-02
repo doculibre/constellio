8ba3c146852bb5850313f3e6dae8955b43cfcdb6
francisbaril <baril.francis@gmail.com>
2017-05-02 13:31:48 -0400

    new script framework

---
+++ app/src/com/constellio/app/extensions/api/scripts/ScriptWithLogOutput.java
@@ -0,0 +1,38 @@
+package com.constellio.app.extensions.api.scripts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.constellio.app.services.factories.AppLayerFactory;
+
+public abstract class ScriptWithLogOutput extends Script {
+
+	public ScriptWithLogOutput(AppLayerFactory appLayerFactory, String category, String name) {
+		super(appLayerFactory, category, name);
+	}
+
+	@Override
+	public List<ScriptParameter> getParameters() {
+		return new ArrayList<>();
+	}
+
+	@Override
+	public ScriptOutput getScriptOutput() {
+		return ScriptOutput.toLogFile(getClass().getSimpleName() + ".log");
+	}
+
+	@Override
+	public void execute(ScriptActionLogger outputLogger, ScriptParameterValues parameterValues)
+			throws Exception {
+		outputLogger.info("Starting execution of " + getClass().getSimpleName());
+
+		this.outputLogger = outputLogger;
+		this.parameterValues = parameterValues;
+		execute();
+
+		outputLogger.info("Execution of " + getClass().getSimpleName() + " finished successfully");
+	}
+
+	protected abstract void execute()
+			throws Exception;
+}
