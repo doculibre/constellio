379d5f642f9eec3c8b39f215e0051d7eb3800b83
Vincent Dussault <vincent.dussault@constellio.com>
2017-05-19 12:19:26 -0400

    7.2 merge

--- app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
+++ app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
@@ -125,7 +125,7 @@ public class ConstellioRMModule implements InstallableSystemModule, ModuleWithCo
 				new RMMigrationTo7_1_2(),
 				new RMMigrationTo7_2(),
 				new RMMigrationTo7_2_0_1(),
-				new RMMigrationTo7_2_0_2()
+				new RMMigrationTo7_2_0_2(),
                 new RMMigrationTo7_2_0_3(),
                 new RMMigrationTo7_2_0_4(),
                 new RMMigrationTo7_3()
