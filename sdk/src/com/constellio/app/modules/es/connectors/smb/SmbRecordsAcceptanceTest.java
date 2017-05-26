a34e7f1984e372cfd998e89dccb51b3ac3eab65f
Nicolas Bélisle <nicolas.belisle@doculibre.com>
2017-05-26 11:22:26 -0400

    Fix smb tests

--- sdk/src/com/constellio/app/modules/es/connectors/smb/SmbRecordsAcceptanceTest.java
+++ sdk/src/com/constellio/app/modules/es/connectors/smb/SmbRecordsAcceptanceTest.java
@@ -92,7 +92,7 @@ public class SmbRecordsAcceptanceTest extends ConstellioTest {
 		userWithToken1And2 = users.chuckNorrisIn(zeCollection);
 
 		connectorInstance = connectorManager.createConnector(es.newConnectorSmbInstance().setCode("zeConnector").setEnabled(false)
-				.setTitle("ze connector").setSeeds(asList(share)).setUsername(username).setPassword(password).setDomain(domain)
+				.setTitle("ze connector").setSeeds(asList(folderA, folderB)).setUsername(username).setPassword(password).setDomain(domain)
 				.setTraversalCode("zeTraversal"));
 
 		anotherConnectorInstance = connectorManager
@@ -107,21 +107,17 @@ public class SmbRecordsAcceptanceTest extends ConstellioTest {
 			throws RecordServicesException {
 
 		Transaction transaction = new Transaction();
+		transaction.add(es.newConnectorSmbFolderWithId(folderA, connectorInstance).setLastFetchedStatus(LastFetchedStatus.OK))
+				.setTitle("A").setUrl("smb://A/");
 
-		transaction.add(es.newConnectorSmbFolderWithId("root", connectorInstance))
-				.setTitle("root").setUrl("smb://");
+		transaction.add(es.newConnectorSmbFolderWithId(folderB, connectorInstance).setLastFetchedStatus(LastFetchedStatus.OK))
+				.setTitle("B").setUrl("smb://B/");
 
-		transaction.add(es.newConnectorSmbFolderWithId(folderA, connectorInstance))
-				.setTitle("A").setUrl("smb://A/").setParent("root");
+		transaction.add(es.newConnectorSmbFolderWithId(folderAA, connectorInstance).setLastFetchedStatus(LastFetchedStatus.OK).setParent(folderA))
+				.setTitle("AA").setUrl("smb://A/A/");
 
-		transaction.add(es.newConnectorSmbFolderWithId(folderB, connectorInstance))
-				.setTitle("B").setUrl("smb://B/").setParent("root");
-
-		transaction.add(es.newConnectorSmbFolderWithId(folderAA, connectorInstance))
-				.setTitle("AA").setUrl("smb://A/A/").setParent(folderA);
-
-		transaction.add(es.newConnectorSmbFolderWithId(folderAB, connectorInstance))
-				.setTitle("AB").setUrl("smb://A/B/").setParent(folderA);
+		transaction.add(es.newConnectorSmbFolderWithId(folderAB, connectorInstance).setLastFetchedStatus(LastFetchedStatus.OK).setParent(folderA))
+				.setTitle("AB").setUrl("smb://A/B/");
 
 		transaction.add(es.newConnectorSmbDocumentWithId(documentA1, connectorInstance))
 				.setTitle("1.txt").setUrl("smb://A/1.txt").setParent(folderA).setManualTokens(PUBLIC_TOKEN);
@@ -132,10 +128,10 @@ public class SmbRecordsAcceptanceTest extends ConstellioTest {
 		transaction.add(es.newConnectorSmbDocumentWithId(documentB3, connectorInstance))
 				.setTitle("3.txt").setUrl("smb://B/3.txt").setParent(folderB).setManualTokens("rtoken1");
 
-		transaction.add(es.newConnectorSmbDocumentWithId(documentAA4, connectorInstance))
+		transaction.add(es.newConnectorSmbDocumentWithId(documentAA4, connectorInstance).setParent(folderAA))
 				.setTitle("4.txt").setUrl("smb://A/A/4.txt").setParent(folderAA).setManualTokens(PUBLIC_TOKEN);
 
-		transaction.add(es.newConnectorSmbDocumentWithId(documentAA5, connectorInstance))
+		transaction.add(es.newConnectorSmbDocumentWithId(documentAA5, connectorInstance).setParent(folderAA))
 				.setTitle("5.txt").setUrl("smb://A/A/5.txt").setParent(folderAA).setManualTokens("rtoken2");
 
 		recordServices.execute(transaction);
@@ -156,38 +152,34 @@ public class SmbRecordsAcceptanceTest extends ConstellioTest {
 
 		transaction.add(es.newConnectorSmbFolderWithId(fetchedFolderOfPreviousTraversalFromAnotherConnector,
 				anotherConnectorInstance))
-				.setTitle("B").setUrl("smb://B/").setFetched(true).setTraversalCode("previous").setParent(share);
+				.setTitle("B").setUrl("smb://B/").setFetched(true).setTraversalCode("previous");
 
 		transaction.add(es.newConnectorSmbFolderWithId(unfetchedFolderFromAnotherConnector, anotherConnectorInstance))
-				.setTitle("AA").setUrl("smb://A/A/").setFetched(false).setParent(fetchedFolderFromAnotherConnector);
-
-		transaction.add(es.newConnectorSmbFolderWithId("root", connectorInstance))
-				.setTitle("test").setUrl("smb://").setFetched(true).setTraversalCode("current")
-				.setModifiedOn(shishOClock.plusSeconds(0));
+				.setTitle("AA").setUrl("smb://A/A/").setFetched(false);
 
 		transaction.add(es.newConnectorSmbFolderWithId(fetchedFolder, connectorInstance))
 				.setTitle("A").setUrl("smb://A/").setFetched(true).setTraversalCode("current")
-				.setModifiedOn(shishOClock.plusSeconds(1)).setParent("root");
+				.setModifiedOn(shishOClock.plusSeconds(1));
 
 		transaction.add(es.newConnectorSmbFolderWithId(fetchedFolderOfPreviousTraversal, connectorInstance))
 				.setTitle("B").setUrl("smb://B/").setFetched(true).setTraversalCode("previous")
-				.setModifiedOn(shishOClock.plusSeconds(2)).setParent("root");
+				.setModifiedOn(shishOClock.plusSeconds(2));
 
 		transaction.add(es.newConnectorSmbFolderWithId(unfetchedFolder, connectorInstance))
 				.setTitle("AA").setUrl("smb://A/A/").setFetched(false)
-				.setModifiedOn(shishOClock.plusSeconds(6)).setParent(fetchedFolder);
+				.setModifiedOn(shishOClock.plusSeconds(6));
 
 		transaction.add(es.newConnectorSmbDocumentWithId(fetchedDocument, connectorInstance))
 				.setTitle("1.txt").setUrl("smb://A/1.txt").setParent(fetchedFolder).setFetched(true).setTraversalCode("current")
-				.setModifiedOn(shishOClock.plusSeconds(3)).setParent(fetchedFolder);
+				.setModifiedOn(shishOClock.plusSeconds(3));
 
 		transaction.add(es.newConnectorSmbDocumentWithId(fetchedDocumentOfPreviousTraversal, connectorInstance))
 				.setTitle("2.txt").setUrl("smb://A/2.txt").setParent(fetchedFolder).setFetched(true).setTraversalCode("previous")
-				.setModifiedOn(shishOClock.plusSeconds(4)).setParent(fetchedFolder);
+				.setModifiedOn(shishOClock.plusSeconds(4));
 
 		transaction.add(es.newConnectorSmbDocumentWithId(unfetchedDocument, connectorInstance))
 				.setTitle("3.txt").setUrl("smb://B/3.txt").setParent(fetchedFolder).setFetched(false)
-				.setModifiedOn(shishOClock.plusSeconds(5)).setParent(fetchedFolderOfPreviousTraversal);
+				.setModifiedOn(shishOClock.plusSeconds(5));
 		recordServices.execute(transaction);
 
 		List<ConnectorDocument<?>> documents = es.searchConnectorDocuments(es.connectorDocumentsToFetchQuery(connectorInstance));
