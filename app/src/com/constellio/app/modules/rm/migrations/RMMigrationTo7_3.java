b78702deb29935f82e96650c6d846572fd5294e4
francisbaril <baril.francis@gmail.com>
2017-05-26 16:24:33 -0400

    administrativeUnits now used in container records paths

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo7_3.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo7_3.java
@@ -5,6 +5,7 @@ import com.constellio.app.entities.modules.MigrationResourcesProvider;
 import com.constellio.app.entities.modules.MigrationScript;
 import com.constellio.app.entities.modules.MigrationTools;
 import com.constellio.app.modules.rm.wrappers.Category;
+import com.constellio.app.modules.rm.wrappers.ContainerRecord;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.app.services.schemasDisplay.SchemasDisplayManager;
 import com.constellio.model.entities.schemas.MetadataValueType;
@@ -49,6 +50,10 @@ public class RMMigrationTo7_3 implements MigrationScript {
 			//typesBuilder.getDefaultSchema(Folder.SCHEMA_TYPE).get(Folder.COPY_STATUS_ENTERED).setDefaultValue(CopyType.PRINCIPAL);
 			typesBuilder.getDefaultSchema(Category.SCHEMA_TYPE).create(Category.DEACTIVATE).setType(MetadataValueType.BOOLEAN)
 					.setDefaultValue(null);
+			typesBuilder.getDefaultSchema(ContainerRecord.SCHEMA_TYPE).get(ContainerRecord.ADMINISTRATIVE_UNIT)
+					.setTaxonomyRelationship(false);
+			typesBuilder.getDefaultSchema(ContainerRecord.SCHEMA_TYPE).get(ContainerRecord.ADMINISTRATIVE_UNITS)
+					.setTaxonomyRelationship(true);
 		}
 	}
 
