dfd6d4bb55d45a5135faba455e6e7b5509cb7cd1
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2017-06-06 10:44:24 -0400

    Fix -- DecommissoningList null pointeur resolution when capacity = null.

--- app/src/com/constellio/app/modules/rm/ui/pages/decommissioning/DecommissioningListPresenter.java
+++ app/src/com/constellio/app/modules/rm/ui/pages/decommissioning/DecommissioningListPresenter.java
@@ -131,7 +131,7 @@ public class DecommissioningListPresenter extends SingleSchemaBasePresenter<Deco
 		List<ContainerRecord> containersToValidate = rm
 				.getContainerRecords(new ArrayList<String>(sizeToBePlacedPerContainer.keySet()));
 		for (ContainerRecord container : containersToValidate) {
-			if (container.getAvailableSize() < sizeToBePlacedPerContainer.get(container.getId())) {
+			if (container.getAvailableSize() != null && container.getAvailableSize() < sizeToBePlacedPerContainer.get(container.getId())) {
 				view.showErrorMessage($("DecommissioningListView.notEnoughSpaceInContainer", container.getTitle()));
 				return;
 			}
