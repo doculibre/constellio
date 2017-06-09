430aa2d40016a304dab7bdd376272231d5bac676
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2017-06-09 12:46:58 -0400

    fixed 1 request per user

--- app/src/com/constellio/app/modules/rm/extensions/RMRequestTaskButtonExtension.java
+++ app/src/com/constellio/app/modules/rm/extensions/RMRequestTaskButtonExtension.java
@@ -1,25 +1,5 @@
 package com.constellio.app.modules.rm.extensions;
 
-import static com.constellio.app.ui.i18n.i18n.$;
-import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.from;
-import static java.util.Arrays.asList;
-
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-
-import com.constellio.app.modules.tasks.model.wrappers.TaskStatusType;
-import com.constellio.app.modules.tasks.model.wrappers.types.TaskStatus;
-import com.constellio.model.entities.records.Record;
-import com.constellio.model.entities.schemas.Metadata;
-import com.constellio.model.entities.schemas.MetadataSchemaType;
-import com.constellio.model.entities.schemas.Schemas;
-import com.constellio.model.services.search.SearchServices;
-import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
-import com.constellio.model.services.search.query.logical.condition.LogicalSearchCondition;
-import org.joda.time.LocalDate;
-import org.vaadin.dialogs.ConfirmDialog;
-
 import com.constellio.app.api.extensions.PagesComponentsExtension;
 import com.constellio.app.api.extensions.params.DecorateMainComponentAfterInitExtensionParams;
 import com.constellio.app.modules.rm.RMConfigs;
@@ -33,10 +13,8 @@ import com.constellio.app.modules.rm.ui.pages.folder.DisplayFolderViewImpl;
 import com.constellio.app.modules.rm.wrappers.ContainerRecord;
 import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.modules.tasks.model.wrappers.Task;
-import com.constellio.app.modules.tasks.model.wrappers.request.BorrowRequest;
-import com.constellio.app.modules.tasks.model.wrappers.request.ExtensionRequest;
-import com.constellio.app.modules.tasks.model.wrappers.request.ReactivationRequest;
-import com.constellio.app.modules.tasks.model.wrappers.request.ReturnRequest;
+import com.constellio.app.modules.tasks.model.wrappers.TaskStatusType;
+import com.constellio.app.modules.tasks.model.wrappers.request.*;
 import com.constellio.app.modules.tasks.services.TasksSchemasRecordsServices;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.app.ui.framework.buttons.BaseButton;
@@ -49,19 +27,27 @@ import com.constellio.app.ui.pages.base.BasePresenterUtils;
 import com.constellio.app.ui.pages.base.BaseView;
 import com.constellio.app.ui.pages.base.BaseViewImpl;
 import com.constellio.model.entities.records.wrappers.User;
+import com.constellio.model.entities.schemas.Metadata;
+import com.constellio.model.entities.schemas.MetadataSchemaType;
+import com.constellio.model.entities.schemas.Schemas;
 import com.constellio.model.frameworks.validation.ValidationException;
 import com.constellio.model.services.factories.ModelLayerFactory;
 import com.constellio.model.services.records.RecordServicesException;
+import com.constellio.model.services.search.SearchServices;
 import com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators;
+import com.constellio.model.services.search.query.logical.condition.LogicalSearchCondition;
 import com.vaadin.data.fieldgroup.PropertyId;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.InlineDateField;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextArea;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
+import org.joda.time.LocalDate;
+import org.vaadin.dialogs.ConfirmDialog;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static com.constellio.app.ui.i18n.i18n.$;
+import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.from;
 
 /**
  * Created by Constellio on 2017-03-16.
@@ -79,6 +65,7 @@ public class RMRequestTaskButtonExtension extends PagesComponentsExtension {
 	RMSchemasRecordsServices rmSchemas;
 	BorrowingServices borrowingServices;
 	DecommissioningService decommissioningService;
+	SearchServices searchServices;
 
 	public RMRequestTaskButtonExtension(String collection, AppLayerFactory appLayerFactory) {
 		this.collection = collection;
@@ -88,6 +75,7 @@ public class RMRequestTaskButtonExtension extends PagesComponentsExtension {
 		this.rmSchemas = new RMSchemasRecordsServices(collection, appLayerFactory);
 		this.borrowingServices = new BorrowingServices(collection, modelLayerFactory);
 		this.decommissioningService = new DecommissioningService(collection, appLayerFactory);
+		this.searchServices = modelLayerFactory.newSearchServices();
 	}
 
 	@Override
@@ -445,20 +433,27 @@ public class RMRequestTaskButtonExtension extends PagesComponentsExtension {
 			return;
 		}
 		try {
-			Task borrowRequest;
-			if (isContainer) {
-				String recordId = container.getId();
-				borrowRequest = taskSchemas
-						.newBorrowContainerRequestTask(currentUser.getId(), getAssignees(recordId), recordId, numberOfDays,
-								container.getTitle());
+			long recordResult = getNumberOfRequestFromUser(BorrowRequest.FULL_SCHEMA_NAME, currentUser, folder, container);
+
+			if(recordResult > 0) {
+				view.showErrorMessage($("RMRequestTaskButtonExtension.taskAlreadyCreated"));
 			} else {
-				String recordId = folder.getId();
-				borrowRequest = taskSchemas
-						.newBorrowFolderRequestTask(currentUser.getId(), getAssignees(recordId), recordId, numberOfDays,
-								folder.getTitle());
+				Task borrowRequest;
+				if (isContainer) {
+					String recordId = container.getId();
+					borrowRequest = taskSchemas
+							.newBorrowContainerRequestTask(currentUser.getId(), getAssignees(recordId), recordId, numberOfDays,
+									container.getTitle());
+				} else {
+					String recordId = folder.getId();
+					borrowRequest = taskSchemas
+							.newBorrowFolderRequestTask(currentUser.getId(), getAssignees(recordId), recordId, numberOfDays,
+									folder.getTitle());
+				}
+				modelLayerFactory.newRecordServices().add(borrowRequest);
+				view.showMessage($("RMRequestTaskButtonExtension.borrowSuccess"));
 			}
-			modelLayerFactory.newRecordServices().add(borrowRequest);
-			view.showMessage($("RMRequestTaskButtonExtension.borrowSuccess"));
+
 		} catch (RecordServicesException e) {
 			e.printStackTrace();
 			view.showErrorMessage($("RMRequestTaskButtonExtension.errorWhileCreatingTask"));
@@ -472,23 +467,10 @@ public class RMRequestTaskButtonExtension extends PagesComponentsExtension {
 		User currentUser = context.getCurrentUser();
 
 		try {
-			SearchServices searchServices  = modelLayerFactory.newSearchServices();
-			LogicalSearchQuery logicalSearchQuery = new LogicalSearchQuery();
 
-			TasksSchemasRecordsServices tasksSchemasRecordsServices = new TasksSchemasRecordsServices(collection, appLayerFactory);
-			MetadataSchemaType documentSchemaType = tasksSchemasRecordsServices.taskSchemaType();
-			Metadata metadataLinkedFolder = documentSchemaType.getAllMetadatas().getMetadataWithLocalCode(Task.LINKED_FOLDERS);
-			Metadata metadataStatus = documentSchemaType.getAllMetadatas().getMetadataWithLocalCode(Task.STATUS_TYPE);
+			long recordResult = getNumberOfRequestFromUser(ReturnRequest.FULL_SCHEMA_NAME, currentUser, folder, container);
 
-			LogicalSearchCondition logicalSearchCondition = from(documentSchemaType).where(Schemas.SCHEMA).isEqualTo(ReturnRequest.FULL_SCHEMA_NAME)
-					.andWhere(metadataLinkedFolder).isEqualTo(folder).andWhere(metadataStatus).isEqualTo(TaskStatusType.STANDBY);
-
-			logicalSearchQuery.setCondition(logicalSearchCondition);
-
-			List<Record> recordResult = searchServices.search(logicalSearchQuery);
-
-
-			if(recordResult.size() > 0) {
+			if(recordResult > 0) {
 				view.showErrorMessage($("RMRequestTaskButtonExtension.taskAlreadyCreated"));
 			}
 			else if (folder != null) {
@@ -521,22 +503,28 @@ public class RMRequestTaskButtonExtension extends PagesComponentsExtension {
 		User currentUser = context.getCurrentUser();
 		LocalDate localDate = new LocalDate(req.getValue());
 		try {
-			if (folder != null) {
-				String folderId = folder.getId();
-				Task reactivationRequest = taskSchemas
-						.newReactivateFolderRequestTask(currentUser.getId(), getAssignees(folderId), folderId, folder.getTitle(),
-								localDate);
-				modelLayerFactory.newRecordServices().add(reactivationRequest);
-			} else if (container != null) {
-				String containerId = container.getId();
-				Task reactivationRequest = taskSchemas
-						.newReactivationContainerRequestTask(currentUser.getId(), getAssignees(containerId), containerId,
-								container.getTitle(), localDate);
-				modelLayerFactory.newRecordServices().add(reactivationRequest);
+			long recordResult = getNumberOfRequestFromUser(ReactivationRequest.FULL_SCHEMA_NAME, currentUser, folder, container);
+
+			if(recordResult > 0) {
+				view.showErrorMessage($("RMRequestTaskButtonExtension.taskAlreadyCreated"));
 			} else {
-				view.showErrorMessage($("RMRequestTaskButtonExtension.errorWhileCreatingTask"));
+				if (folder != null) {
+					String folderId = folder.getId();
+					Task reactivationRequest = taskSchemas
+							.newReactivateFolderRequestTask(currentUser.getId(), getAssignees(folderId), folderId, folder.getTitle(),
+									localDate);
+					modelLayerFactory.newRecordServices().add(reactivationRequest);
+				} else if (container != null) {
+					String containerId = container.getId();
+					Task reactivationRequest = taskSchemas
+							.newReactivationContainerRequestTask(currentUser.getId(), getAssignees(containerId), containerId,
+									container.getTitle(), localDate);
+					modelLayerFactory.newRecordServices().add(reactivationRequest);
+				} else {
+					view.showErrorMessage($("RMRequestTaskButtonExtension.errorWhileCreatingTask"));
+				}
+				view.showMessage($("RMRequestTaskButtonExtension.reactivationSuccess"));
 			}
-			view.showMessage($("RMRequestTaskButtonExtension.reactivationSuccess"));
 		} catch (RecordServicesException e) {
 			e.printStackTrace();
 			view.showErrorMessage($("RMRequestTaskButtonExtension.errorWhileCreatingTask"));
@@ -549,22 +537,28 @@ public class RMRequestTaskButtonExtension extends PagesComponentsExtension {
 		ContainerRecord container = context.getContainer();
 		User currentUser = context.getCurrentUser();
 		try {
-			if (folder != null) {
-				String folderId = folder.getId();
-				Task borrowExtensionRequest = taskSchemas
-						.newBorrowFolderExtensionRequestTask(currentUser.getId(), getAssignees(folderId), folderId,
-								folder.getTitle(), new LocalDate(req.getValue()));
-				modelLayerFactory.newRecordServices().add(borrowExtensionRequest);
-			} else if (container != null) {
-				String containerId = container.getId();
-				Task borrowExtensionRequest = taskSchemas
-						.newBorrowContainerExtensionRequestTask(currentUser.getId(), getAssignees(containerId), containerId,
-								container.getTitle(), new LocalDate(req.getValue()));
-				modelLayerFactory.newRecordServices().add(borrowExtensionRequest);
+			long recordResult = getNumberOfRequestFromUser(ExtensionRequest.FULL_SCHEMA_NAME, currentUser, folder, container);
+
+			if(recordResult > 0) {
+				view.showErrorMessage($("RMRequestTaskButtonExtension.taskAlreadyCreated"));
 			} else {
-				view.showErrorMessage($("RMRequestTaskButtonExtension.errorWhileCreatingTask"));
+				if (folder != null) {
+					String folderId = folder.getId();
+					Task borrowExtensionRequest = taskSchemas
+							.newBorrowFolderExtensionRequestTask(currentUser.getId(), getAssignees(folderId), folderId,
+									folder.getTitle(), new LocalDate(req.getValue()));
+					modelLayerFactory.newRecordServices().add(borrowExtensionRequest);
+				} else if (container != null) {
+					String containerId = container.getId();
+					Task borrowExtensionRequest = taskSchemas
+							.newBorrowContainerExtensionRequestTask(currentUser.getId(), getAssignees(containerId), containerId,
+									container.getTitle(), new LocalDate(req.getValue()));
+					modelLayerFactory.newRecordServices().add(borrowExtensionRequest);
+				} else {
+					view.showErrorMessage($("RMRequestTaskButtonExtension.errorWhileCreatingTask"));
+				}
+				view.showMessage($("RMRequestTaskButtonExtension.borrowExtensionSuccess"));
 			}
-			view.showMessage($("RMRequestTaskButtonExtension.borrowExtensionSuccess"));
 		} catch (RecordServicesException e) {
 			e.printStackTrace();
 			view.showErrorMessage($("RMRequestTaskButtonExtension.errorWhileCreatingTask"));
@@ -611,6 +605,28 @@ public class RMRequestTaskButtonExtension extends PagesComponentsExtension {
 			this.type = type;
 			return this;
 		}
+	}
+
+	public long getNumberOfRequestFromUser(String fullSchemaName, User currentUser, Folder folder, ContainerRecord containerRecord) {
+		SearchServices searchServices  = modelLayerFactory.newSearchServices();
+
+		TasksSchemasRecordsServices tasksSchemasRecordsServices = new TasksSchemasRecordsServices(collection, appLayerFactory);
+		MetadataSchemaType taskSchemaType = tasksSchemasRecordsServices.taskSchemaType();
+		Metadata metadataLinkedFolder = taskSchemaType.getAllMetadatas().getMetadataWithLocalCode(Task.LINKED_FOLDERS);
+		Metadata metadataLinkedContainer = taskSchemaType.getAllMetadatas().getMetadataWithLocalCode(Task.LINKED_CONTAINERS);
+		Metadata metadataStatus = taskSchemaType.getAllMetadatas().getMetadataWithLocalCode(Task.STATUS_TYPE);
+		Metadata metadataApplicant = taskSchemaType.getAllMetadatas().getMetadataWithLocalCode(RequestTask.APPLICANT);
+		LogicalSearchCondition logicalSearchCondition;
+
+		if(folder != null) {
+			logicalSearchCondition = from(taskSchemaType).where(Schemas.SCHEMA).isEqualTo(fullSchemaName)
+					.andWhere(metadataLinkedFolder).isEqualTo(folder).andWhere(metadataStatus).isEqualTo(TaskStatusType.STANDBY).andWhere(metadataApplicant).isEqualTo(currentUser.getId());
+		} else {
+			logicalSearchCondition = from(taskSchemaType).where(Schemas.SCHEMA).isEqualTo(fullSchemaName)
+					.andWhere(metadataLinkedContainer).isEqualTo(containerRecord).andWhere(metadataStatus).isEqualTo(TaskStatusType.STANDBY).andWhere(metadataApplicant).isEqualTo(currentUser.getId());
+		}
+
+		return searchServices.getResultsCount(logicalSearchCondition);
 	}
 
 	public class Context {
