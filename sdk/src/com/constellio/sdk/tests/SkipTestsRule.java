fc521024210aa12f910433e090f789c0e4447690
Maxime Cote <maxime.cote@doculibre.com>
2017-06-15 12:18:34 -0400

    new Skiptestrule for gradle runner

--- sdk/src/com/constellio/sdk/tests/SkipTestsRule.java
+++ sdk/src/com/constellio/sdk/tests/SkipTestsRule.java
@@ -9,12 +9,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import com.constellio.sdk.tests.annotations.*;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.internal.AssumptionViolatedException;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+
+import com.constellio.sdk.tests.annotations.CloudTest;
+import com.constellio.sdk.tests.annotations.DoNotRunOnIntegrationServer;
+import com.constellio.sdk.tests.annotations.DriverTest;
+import com.constellio.sdk.tests.annotations.InDevelopmentTest;
+import com.constellio.sdk.tests.annotations.InternetTest;
+import com.constellio.sdk.tests.annotations.LoadTest;
+import com.constellio.sdk.tests.annotations.MainTest;
+import com.constellio.sdk.tests.annotations.MainTestDefaultStart;
+import com.constellio.sdk.tests.annotations.PerformanceTest;
+import com.constellio.sdk.tests.annotations.SlowTest;
+import com.constellio.sdk.tests.annotations.UiTest;
 
 public class SkipTestsRule implements TestRule {
 
@@ -249,7 +260,7 @@ public class SkipTestsRule implements TestRule {
 			return sunJavaCommand.contains(testClass.getName() + ":" + currentTestName);
 
 		} else {
-			return false;
+			return true;
 		}
 	}
 
