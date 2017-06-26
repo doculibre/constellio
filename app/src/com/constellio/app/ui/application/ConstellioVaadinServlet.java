32d5de2d50070a60f6015c266366d0ee84c96d44
francisbaril <baril.francis@gmail.com>
2017-06-26 13:34:37 -0400

    service monitoring page

--- app/src/com/constellio/app/ui/application/ConstellioVaadinServlet.java
+++ app/src/com/constellio/app/ui/application/ConstellioVaadinServlet.java
@@ -1,8 +1,12 @@
 package com.constellio.app.ui.application;
 
+import java.io.IOException;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.constellio.app.services.factories.ConstellioFactories;
 import com.vaadin.annotations.VaadinServletConfiguration;
@@ -13,28 +17,60 @@ import com.vaadin.server.VaadinServlet;
 @VaadinServletConfiguration(productionMode = false, ui = ConstellioUI.class)
 public class ConstellioVaadinServlet extends VaadinServlet {
 
-    @Override
+	boolean initialized = false;
+	Thread initThread;
+
+	@Override
 	public void init(ServletConfig servletConfig)
 			throws ServletException {
-    	super.init(servletConfig);
-    	ConstellioFactories.getInstance();
+		System.out.println("ConstellioVaadinServlet.init");
+		super.init(servletConfig);
+
+		initThread = new Thread() {
+			@Override
+			public void run() {
+				System.out.println("ConstellioVaadinServlet.init>ConstellioFactories.getInstance()...");
+				ConstellioFactories.getInstance();
+				initialized = true;
+			}
+		};
+		initThread.start();
+
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		if (!initialized) {
+			System.out.println("ConstellioVaadinServlet.service (waiting)");
+			try {
+				initThread.join();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		System.out.println("ConstellioVaadinServlet.service");
+		super.service(request, response);
 	}
 
 	/**
-     * Adapted to support responsive design.
-     * 
-     * See https://vaadin.com/forum#!/thread/1676923
-     * @see com.vaadin.server.VaadinServlet#servletInitialized()
-     */
+	 * Adapted to support responsive design.
+	 *
+	 * See https://vaadin.com/forum#!/thread/1676923
+	 * @see com.vaadin.server.VaadinServlet#servletInitialized()
+	 */
 
-    @Override
-    protected final void servletInitialized() throws ServletException {
-        super.servletInitialized();
-        getService().addSessionInitListener(new ConstellioSessionInitListener());
-    }
-    
-    public static ConstellioVaadinServlet getCurrent() {
-    	return (ConstellioVaadinServlet) VaadinServlet.getCurrent();
-    }
+	@Override
+	protected final void servletInitialized()
+			throws ServletException {
+		super.servletInitialized();
+		getService().addSessionInitListener(new ConstellioSessionInitListener());
+	}
+
+	public static ConstellioVaadinServlet getCurrent() {
+		return (ConstellioVaadinServlet) VaadinServlet.getCurrent();
+	}
 
 }
