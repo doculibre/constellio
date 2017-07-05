0aeeae7231db53dd7a340b2cdd3254289d36446b
francisbaril <baril.francis@gmail.com>
2017-07-04 23:04:31 -0400

    replaced english i18ns

--- app/src/com/constellio/app/modules/complementary/ESRMRobotsModule.java
+++ app/src/com/constellio/app/modules/complementary/ESRMRobotsModule.java
@@ -1,34 +1,39 @@
 package com.constellio.app.modules.complementary;
 
-import com.constellio.app.entities.modules.ComboMigrationScript;
-import com.constellio.app.entities.modules.InstallableModule;
-import com.constellio.app.entities.modules.MigrationScript;
-import com.constellio.app.entities.modules.ModuleWithComboMigration;
-import com.constellio.app.entities.navigation.NavigationConfig;
-import com.constellio.app.extensions.AppLayerCollectionExtensions;
-import com.constellio.app.modules.complementary.esRmRobots.actions.ClassifyConnectorDocumentInFolderActionExecutor;
-import com.constellio.app.modules.complementary.esRmRobots.actions.ClassifyConnectorFolderDirectlyInThePlanActionExecutor;
-import com.constellio.app.modules.complementary.esRmRobots.actions.ClassifyConnectorFolderInParentFolderActionExecutor;
-import com.constellio.app.modules.complementary.esRmRobots.actions.ClassifyConnectorFolderInTaxonomyActionExecutor;
-import com.constellio.app.modules.complementary.esRmRobots.extensions.EsRmRobotsActionParametersFieldFactoryExtension;
-import com.constellio.app.modules.complementary.esRmRobots.extensions.EsRmRobotsMappingExtension;
-import com.constellio.app.modules.complementary.esRmRobots.migrations.*;
-import com.constellio.app.modules.es.ConstellioESModule;
-import com.constellio.app.modules.es.extensions.api.ESModuleExtensions;
-import com.constellio.app.modules.rm.ConstellioRMModule;
-import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
-import com.constellio.app.modules.robots.ConstellioRobotsModule;
-import com.constellio.app.modules.robots.services.RobotSchemaRecordServices;
-import com.constellio.app.modules.robots.services.RobotsManager;
-import com.constellio.app.services.factories.AppLayerFactory;
-import com.constellio.model.entities.configs.SystemConfiguration;
+import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Arrays.asList;
+import com.constellio.app.entities.modules.ComboMigrationScript;
+import com.constellio.app.entities.modules.InstallableModule;
+import com.constellio.app.entities.modules.MigrationScript;
+import com.constellio.app.entities.modules.ModuleWithComboMigration;
+import com.constellio.app.entities.navigation.NavigationConfig;
+import com.constellio.app.extensions.AppLayerCollectionExtensions;
+import com.constellio.app.modules.complementary.esRmRobots.extensions.EsRmRobotsActionParametersFieldFactoryExtension;
+import com.constellio.app.modules.complementary.esRmRobots.extensions.EsRmRobotsMappingExtension;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationCombo;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo5_1_2;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo5_1_5;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo5_1_6;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo5_1_7;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo5_1_9;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo6_0;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo6_1;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo6_2_2_1;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo7_0_1;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo7_1;
+import com.constellio.app.modules.complementary.esRmRobots.migrations.ESRMRobotsMigrationTo7_3_1;
+import com.constellio.app.modules.es.ConstellioESModule;
+import com.constellio.app.modules.es.extensions.api.ESModuleExtensions;
+import com.constellio.app.modules.rm.ConstellioRMModule;
+import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
+import com.constellio.app.modules.robots.ConstellioRobotsModule;
+import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.model.entities.configs.SystemConfiguration;
 
 public class ESRMRobotsModule implements InstallableModule, ModuleWithComboMigration {
 	public static final String ID = "es_rm_robots";
@@ -62,6 +67,7 @@ public class ESRMRobotsModule implements InstallableModule, ModuleWithComboMigra
 		migrations.add(new ESRMRobotsMigrationTo6_2_2_1());
 		migrations.add(new ESRMRobotsMigrationTo7_0_1());
 		migrations.add(new ESRMRobotsMigrationTo7_1());
+		migrations.add(new ESRMRobotsMigrationTo7_3_1());
 
 		return migrations;
 	}
