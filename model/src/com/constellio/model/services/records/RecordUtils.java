e349acacbff253fc38ee62d0d942a00fa22143c1
dakota.indien <dakota.indien@gmail.com>
2017-07-11 16:27:19 -0400

    bug fix

--- model/src/com/constellio/model/services/records/RecordUtils.java
+++ model/src/com/constellio/model/services/records/RecordUtils.java
@@ -44,18 +44,19 @@ public class RecordUtils {
 
 		long size = 0;
 
-		if (recordDTO.getCopyFields() != null) {
-			for (Object object : recordDTO.getCopyFields().values()) {
-				size += sizeOf(object);
-			}
+		if (recordDTO != null) {
+			if (recordDTO.getCopyFields() != null) {
+				for (Object object : recordDTO.getCopyFields().values()) {
+					size += sizeOf(object);
+				}
 
-		}
-		if (recordDTO.getFields() != null) {
-			for (Object object : recordDTO.getFields().values()) {
-				size += sizeOf(object);
+			}
+			if (recordDTO.getFields() != null) {
+				for (Object object : recordDTO.getFields().values()) {
+					size += sizeOf(object);
+				}
 			}
 		}
-
 		return size;
 	}
 
