1bd76ddbb8de0d22e942d7fcf2588d5b9832111e
Marco <n.damours.00@gmail.com>
2017-07-14 14:55:38 -0400

    Merge report to master

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo7_4.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo7_4.java
@@ -6,11 +6,9 @@ import com.constellio.app.entities.modules.MigrationScript;
 import com.constellio.app.modules.reports.wrapper.Printable;
 import com.constellio.app.modules.rm.constants.RMRoles;
 import com.constellio.app.modules.rm.model.calculators.container.ContainerRecordLocalizationCalculator;
-import com.constellio.app.modules.rm.wrappers.ContainerRecord;
-import com.constellio.app.modules.rm.wrappers.Document;
-import com.constellio.app.modules.rm.wrappers.Folder;
-import com.constellio.app.modules.rm.wrappers.PrintableLabel;
+import com.constellio.app.modules.rm.wrappers.*;
 import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.model.entities.Language;
 import com.constellio.model.entities.schemas.MetadataValueType;
 import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
@@ -44,6 +42,9 @@ public class RMMigrationTo7_4 implements MigrationScript {
 
         @Override
         protected void migrate(MetadataSchemaTypesBuilder typesBuilder) {
+            MetadataSchemaBuilder metadataSchemaBuilder = typesBuilder.getSchemaType(Printable.SCHEMA_TYPE).createCustomSchema(PrintableReport.SCHEMA_TYPE);
+            metadataSchemaBuilder.create(PrintableReport.REPORT_TYPE).setType(MetadataValueType.STRING).addLabel(Language.French, "Type de rapport").setUndeletable(true).setEssential(true).defineDataEntry().asManual();
+            metadataSchemaBuilder.create(PrintableReport.REPORT_SCHEMA).setType(MetadataValueType.STRING).addLabel(Language.French, "Schema lier au rapport").setUndeletable(true).setEssential(true).defineDataEntry().asManual();
             MetadataSchemaBuilder containerRecord = typesBuilder.getSchema(ContainerRecord.DEFAULT_SCHEMA);
 
             containerRecord.getMetadata(ContainerRecord.DECOMMISSIONING_TYPE).setDefaultRequirement(true);
