bb918fa37df4ba4e4bbf090b359233309a543427
francisbaril <baril.francis@gmail.com>
2017-07-31 10:12:32 -0400

    logging ignores

--- sdk/src/com/constellio/sdk/tests/SkipTestsRule.java
+++ sdk/src/com/constellio/sdk/tests/SkipTestsRule.java
@@ -103,6 +103,24 @@ public class SkipTestsRule implements TestRule {
 			this.blackList = getFilterList("tests.blacklist", properties);
 			this.skipCloud = !"cloud".equals(properties.get("dao.records.type"));
 			this.skipIgnite = !"ignite".equals(properties.get("dao.cache"));
+
+			System.out.println("skipAllTests:" + skipAllTests);
+			System.out.println("skipSlow:" + skipSlow);
+			System.out.println("skipImportTests:" + skipImportTests);
+			System.out.println("skipReal:" + skipReal);
+			System.out.println("skipLoad:" + skipLoad);
+			System.out.println("checkRollback:" + checkRollback);
+			System.out.println("skipInternetTest:" + skipInternetTest);
+			System.out.println("skipInDevelopment:" + skipInDevelopment);
+			System.out.println("skipDriver:" + skipDriver);
+			System.out.println("skipTestsWithGradle:" + skipTestsWithGradle);
+			System.out.println("runPerformance:" + runPerformance);
+			System.out.println("skipUI:" + skipUI);
+			System.out.println("whiteList:" + whiteList);
+			System.out.println("blackList:" + blackList);
+			System.out.println("skipCloud:" + skipCloud);
+			System.out.println("skipIgnite:" + skipIgnite);
+
 		}
 	}
 
@@ -156,6 +174,7 @@ public class SkipTestsRule implements TestRule {
 		mainTest = mainTestAnnotation != null;
 
 		if (skipTestsWithGradle && isRunnedByGradle()) {
+			System.out.println("Ignore 1");
 			return true;
 		}
 
@@ -165,17 +184,20 @@ public class SkipTestsRule implements TestRule {
 		}
 
 		if (!testClassDirectlyTargetted && isClassFiltered(testClass)) {
+			System.out.println("Ignore 2");
 			return true;
 		}
 
 		Class<?> testSuperClass = testClass.getSuperclass();
 		if (!testClassDirectlyTargetted && testSuperClass != null && "ConstellioImportAcceptTest"
 				.equals(testSuperClass.getSimpleName()) && skipImportTests) {
+			System.out.println("Ignore 3");
 			return true;
 		}
 
 		if (mainTestAnnotation != null) {
 			if (!testClassDirectlyTargetted) {
+				System.out.println("Ignore 4");
 				return true;
 
 			} else if (hasMainTestOnlyOneStarter(testClass)) {
@@ -208,26 +230,37 @@ public class SkipTestsRule implements TestRule {
 		}
 
 		if (doNotRunOnIntegrationServer != null && TestUtils.isIntegrationServer()) {
+			System.out.println("Ignore 5");
 			return true;
 		} else if (loadTest != null && skipLoad) {
+			System.out.println("Ignore 6");
 			return true;
 		} else if (!testClassDirectlyTargetted && uiTest != null && skipUI) {
+			System.out.println("Ignore 7");
 			return true;
 		} else if (!testClassDirectlyTargetted && driverTest != null && skipDriver) {
+			System.out.println("Ignore 8");
 			return true;
 		} else if (!testClassDirectlyTargetted && internetTest != null && skipInternetTest) {
+			System.out.println("Ignore 9");
 			return true;
 		} else if (!testClassDirectlyTargetted && slowTest != null && skipSlow) {
+			System.out.println("Ignore 10");
 			return true;
 		} else if (!testClassDirectlyTargetted && performanceTest != null && !runPerformance) {
+			System.out.println("Ignore 11");
 			return true;
 		} else if (inDevelopmentTestAnnotation != null && skipInDevelopment) {
+			System.out.println("Ignore 12");
 			return true;
 		} else if (!testClassDirectlyTargetted && isRealTest && skipReal) {
+			System.out.println("Ignore 13");
 			return true;
 		} else if (!testClassDirectlyTargetted && cloudTest != null && skipCloud) {
+			System.out.println("Ignore 14");
 			return true;
 		} else if (!testClassDirectlyTargetted && igniteTest != null && skipIgnite) {
+			System.out.println("Ignore 15");
 			return true;
 		} else {
 			return false;
