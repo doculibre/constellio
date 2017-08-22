1c181d2a0827c2cf3732d5a6905ac4fcc02a516a
nDamours <n.damours.00@gmail.com>
2017-08-22 14:40:04 -0400

    added temporaryMetadata savestate and test

---
+++ sdk/src/com/constellio/app/services/migrations/scripts/CoreMigrationTo_7_4_AcceptanceTest.java
@@ -0,0 +1,26 @@
+package com.constellio.app.services.migrations.scripts;
+
+import com.constellio.app.modules.rm.wrappers.Folder;
+import com.constellio.model.entities.schemas.entries.DataEntryType;
+import com.constellio.model.services.records.extractions.RecordPopulateServices;
+import com.constellio.model.services.schemas.MetadataSchemasManager;
+import com.constellio.sdk.tests.ConstellioTest;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CoreMigrationTo_7_4_AcceptanceTest extends ConstellioTest {
+
+    @Test
+    public void startApplicationWithSaveState(){
+        RecordPopulateServices.LOG_CONTENT_MISSING = false;
+        givenTransactionLogIsEnabled();
+
+        getCurrentTestSession().getFactoriesTestFeatures()
+                .givenSystemInState(getTestResourceFile("saveStateWithTemporaryMetadata.zip")).withPasswordsReset()
+                .withFakeEncryptionServices();
+        MetadataSchemasManager manager = getModelLayerFactory().getMetadataSchemasManager();
+        assertThat(manager.getSchemaTypes(zeCollection).getSchema(Folder.DEFAULT_SCHEMA).getMetadata("temporaryMetadata")).isNotNull();
+        assertThat(manager.getSchemaTypes(zeCollection).getSchema(Folder.DEFAULT_SCHEMA).getMetadata("temporaryMetadata").getDataEntry().getType()).isEqualTo(DataEntryType.CALCULATED);
+    }
+}
