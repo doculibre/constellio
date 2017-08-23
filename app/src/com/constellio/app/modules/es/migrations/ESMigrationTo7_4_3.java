d420da8296e55836db7deb78087ff03bf427f5ec
nDamours <n.damours.00@gmail.com>
2017-08-23 09:52:00 -0400

    fixed i18n

---
+++ app/src/com/constellio/app/modules/es/migrations/ESMigrationTo7_4_3.java
@@ -0,0 +1,32 @@
+package com.constellio.app.modules.es.migrations;
+
+import com.constellio.app.entities.modules.MetadataSchemasAlterationHelper;
+import com.constellio.app.entities.modules.MigrationHelper;
+import com.constellio.app.entities.modules.MigrationResourcesProvider;
+import com.constellio.app.entities.modules.MigrationScript;
+import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
+
+public class ESMigrationTo7_4_3 extends MigrationHelper implements MigrationScript {
+    @Override
+    public String getVersion() {
+        return "7.4.3";
+    }
+
+    @Override
+    public void migrate(String collection, MigrationResourcesProvider migrationResourcesProvider, AppLayerFactory appLayerFactory) throws Exception {
+        new SchemaAlterationFor7_4_3(collection, migrationResourcesProvider, appLayerFactory).migrate();
+    }
+
+    static class SchemaAlterationFor7_4_3 extends MetadataSchemasAlterationHelper {
+
+        protected SchemaAlterationFor7_4_3(String collection, MigrationResourcesProvider migrationResourcesProvider, AppLayerFactory appLayerFactory) {
+            super(collection, migrationResourcesProvider, appLayerFactory);
+        }
+
+        @Override
+        protected void migrate(MetadataSchemaTypesBuilder typesBuilder) {
+
+        }
+    }
+}
