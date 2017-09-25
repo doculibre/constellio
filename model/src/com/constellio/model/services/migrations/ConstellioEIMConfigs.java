1872a388a46dfdc86e85bf1012deef3fbfb90f96
Charles <charles.blanchette@doculibre.com>
2017-09-25 14:29:25 -0400

    Changement du nombre de temps par défault pour la suppression des événement

--- model/src/com/constellio/model/services/migrations/ConstellioEIMConfigs.java
+++ model/src/com/constellio/model/services/migrations/ConstellioEIMConfigs.java
@@ -168,7 +168,7 @@ public class ConstellioEIMConfigs {
 		//
 		add(LAST_BACKUP_DAY = others.createString("lastBackupDay").whichIsHidden());
 
-		add(KEEP_EVENTS_FOR_X_MONTH = others.createInteger("eventKeptPeriod").withDefaultValue(60).whichIsHidden());
+		add(KEEP_EVENTS_FOR_X_MONTH = others.createInteger("eventKeptPeriod").withDefaultValue(99999).whichIsHidden());
 
 		configurations = Collections.unmodifiableList(modifiableConfigs);
 	}
