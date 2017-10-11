43a97268ce3709871cc5d7706c32cb0cfe337cc9
nDamours <n.damours.00@gmail.com>
2017-10-11 15:08:19 -0400

    added xml filling feature for tests xml generation

--- app/src/com/constellio/app/modules/rm/services/reports/parameters/XmlReportGeneratorParameters.java
+++ app/src/com/constellio/app/modules/rm/services/reports/parameters/XmlReportGeneratorParameters.java
@@ -1,5 +1,6 @@
 package com.constellio.app.modules.rm.services.reports.parameters;
 
+import com.constellio.app.modules.rm.services.reports.XmlReportGenerator;
 import com.constellio.model.entities.records.Record;
 
 import java.util.List;
@@ -7,9 +8,16 @@ import java.util.List;
 public class XmlReportGeneratorParameters extends XmlGeneratorParameters {
     private int numberOfCopies;
 
+    public XmlReportGeneratorParameters() { }
+
     public XmlReportGeneratorParameters(int numberOfCopies, Record... recordsElements) {
         super(recordsElements);
         this.numberOfCopies = numberOfCopies;
+    }
+
+    public XmlReportGeneratorParameters setNumberOfCopies(int numberOfCopies) {
+        this.numberOfCopies = numberOfCopies;
+        return this;
     }
 
     public int getNumberOfCopies(){
