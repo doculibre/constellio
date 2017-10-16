e8207c4e56fae6c51433931a843b27024180fc3b
Vincent Dussault <vincent.dussault@constellio.com>
2017-10-15 23:03:11 -0400

    Hide retention rule field for subfolder.

--- app/src/com/constellio/app/modules/rm/ui/pages/folder/AddEditFolderPresenter.java
+++ app/src/com/constellio/app/modules/rm/ui/pages/folder/AddEditFolderPresenter.java
@@ -651,38 +651,42 @@ public class AddEditFolderPresenter extends SingleSchemaBasePresenter<AddEditFol
 				Folder.UNIFORM_SUBDIVISION_ENTERED);
 
 		if (retentionRuleField != null) {
-			String currentValue = retentionRuleField.getFieldValue();
-			// Discover what options are available
-			List<String> availableOptions = decommissioningService().getRetentionRulesForCategory(
-					categoryField.getFieldValue(), uniformSubdivisionField.getFieldValue(), StatusFilter.ACTIVES);
-
-			// Set the options if they changed
-			if (!retentionRuleField.getOptions().equals(availableOptions)) {
-				retentionRuleField.setOptions(availableOptions);
-			}
-
-			// Set the value if necessary
-			if (availableOptions.size() > 1) {
-				if (currentValue != null && !availableOptions.contains(currentValue)) {
-					folderVO.setRetentionRule((String) null);
-					retentionRuleField.setFieldValue(null);
-				}
-				if (!retentionRuleField.isVisible()) {
-					setFieldVisible(retentionRuleField, true, Folder.RETENTION_RULE_ENTERED);
-				}
-			} else if (availableOptions.size() == 1) {
-				if (!availableOptions.get(0).equals(currentValue)) {
-					String onlyAvailableOption = availableOptions.get(0);
-					folderVO.setRetentionRule(onlyAvailableOption);
-					retentionRuleField.setFieldValue(availableOptions.get(0));
-				}
+			if (folderVO.getParentFolder() != null) {
 				setFieldVisible(retentionRuleField, false, Folder.RETENTION_RULE_ENTERED);
 			} else {
-				if (currentValue != null) {
-					folderVO.setRetentionRule((String) null);
+				String currentValue = retentionRuleField.getFieldValue();
+				// Discover what options are available
+				List<String> availableOptions = decommissioningService().getRetentionRulesForCategory(
+						categoryField.getFieldValue(), uniformSubdivisionField.getFieldValue(), StatusFilter.ACTIVES);
+
+				// Set the options if they changed
+				if (!retentionRuleField.getOptions().equals(availableOptions)) {
+					retentionRuleField.setOptions(availableOptions);
 				}
-				if (retentionRuleField.isVisible()) {
+
+				// Set the value if necessary
+				if (availableOptions.size() > 1) {
+					if (currentValue != null && !availableOptions.contains(currentValue)) {
+						folderVO.setRetentionRule((String) null);
+						retentionRuleField.setFieldValue(null);
+					}
+					if (!retentionRuleField.isVisible()) {
+						setFieldVisible(retentionRuleField, true, Folder.RETENTION_RULE_ENTERED);
+					}
+				} else if (availableOptions.size() == 1) {
+					if (!availableOptions.get(0).equals(currentValue)) {
+						String onlyAvailableOption = availableOptions.get(0);
+						folderVO.setRetentionRule(onlyAvailableOption);
+						retentionRuleField.setFieldValue(availableOptions.get(0));
+					}
 					setFieldVisible(retentionRuleField, false, Folder.RETENTION_RULE_ENTERED);
+				} else {
+					if (currentValue != null) {
+						folderVO.setRetentionRule((String) null);
+					}
+					if (retentionRuleField.isVisible()) {
+						setFieldVisible(retentionRuleField, false, Folder.RETENTION_RULE_ENTERED);
+					}
 				}
 			}
 		}
