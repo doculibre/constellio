a806d436aeb2e32e2c3c53cb23e0227d08c29f41
nDamours <n.damours.00@gmail.com>
2017-11-24 10:55:45 -0500

    fixed bug in metadata report when it could not find metadata in schema.

--- app/src/com/constellio/app/modules/rm/services/reports/XmlGenerator.java
+++ app/src/com/constellio/app/modules/rm/services/reports/XmlGenerator.java
@@ -123,6 +123,9 @@ public abstract class XmlGenerator {
     }
 
     public static List<Element> createMetadataTagFromMetadataOfTypeStructure(Metadata metadata, Record recordElement, String collection, AppLayerFactory factory) {
+        if(!hasMetadata(recordElement, metadata)) {
+            return Collections.emptyList();
+        }
         List<Element> listOfMetadataTags = new ArrayList<>();
         if (metadata.getLocalCode().toLowerCase().contains("copyrule")) {
             List<ModifiableStructure> metadataValue;
@@ -162,6 +165,9 @@ public abstract class XmlGenerator {
     }
 
     public static List<Element> createMetadataTagFromMetadataOfTypeEnum(Metadata metadata, Record recordElement, Namespace namespace) {
+        if(!hasMetadata(recordElement, metadata)) {
+            return Collections.emptyList();
+        }
         List<Element> listOfMetadataTags = new ArrayList<>();
         EnumWithSmallCode metadataValue = recordElement.get(metadata);
         String code, title;
@@ -185,6 +191,9 @@ public abstract class XmlGenerator {
     }
 
     public static List<Element> createMetadataTagFromMetadataOfTypeReference(Metadata metadata, Record recordElement, String collection, AppLayerFactory factory, Namespace namespace) {
+        if(!hasMetadata(recordElement, metadata)) {
+            return Collections.emptyList();
+        }
         RecordServices recordServices = factory.getModelLayerFactory().newRecordServices();
         MetadataSchemasManager metadataSchemasManager = factory.getModelLayerFactory().getMetadataSchemasManager();
         MetadataSchema recordSchemaType = metadataSchemasManager.getSchemaTypeOf(recordElement).getDefaultSchema();
@@ -275,4 +284,13 @@ public abstract class XmlGenerator {
     }
 
     public abstract XmlGeneratorParameters getXmlGeneratorParameters();
+
+    private static boolean hasMetadata (Record record, Metadata metadata) {
+        try {
+            record.get(metadata);
+            return true;
+        } catch(MetadataSchemasRuntimeException.NoSuchMetadata e) {
+            return false;
+        }
+    }
 }
