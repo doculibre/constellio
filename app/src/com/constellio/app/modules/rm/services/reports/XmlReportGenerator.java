49db15732681e9fe1dec7261fe89a075c9819195
Vincent Dussault <vincent.dussault@constellio.com>
2017-11-27 13:35:47 -0500

    XML report generator sort fix

--- app/src/com/constellio/app/modules/rm/services/reports/XmlReportGenerator.java
+++ app/src/com/constellio/app/modules/rm/services/reports/XmlReportGenerator.java
@@ -1,5 +1,17 @@
 package com.constellio.app.modules.rm.services.reports;
 
+import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.from;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
 import com.constellio.app.modules.rm.services.reports.parameters.XmlReportGeneratorParameters;
 import com.constellio.app.modules.rm.wrappers.Category;
 import com.constellio.app.modules.rm.wrappers.Folder;
@@ -13,16 +25,6 @@ import com.constellio.model.services.schemas.MetadataSchemasManager;
 import com.constellio.model.services.search.SearchServices;
 import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
 import com.constellio.model.services.search.query.logical.condition.LogicalSearchCondition;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.from;
 
 public class XmlReportGenerator extends XmlGenerator {
 
@@ -109,11 +111,33 @@ public class XmlReportGenerator extends XmlGenerator {
         return Collections.singletonList(metadataXmlElement);
     }
 
-    private Record[] getRecordFromIds(String schemaType, List<String> ids) {
-        SearchServices searchServices = getFactory().getModelLayerFactory().newSearchServices();
+    private Record[] getRecordFromIds(String schemaType, final List<String> ids) {
+        List<Record> allRecords = new ArrayList<>();
+    	SearchServices searchServices = getFactory().getModelLayerFactory().newSearchServices();
         MetadataSchemasManager metadataSchemasManager = getFactory().getModelLayerFactory().getMetadataSchemasManager();
-        LogicalSearchCondition condition = from(metadataSchemasManager.getSchemaTypes(getCollection()).getSchemaType(schemaType)).where(Schemas.IDENTIFIER).isIn(ids);
-        return searchServices.search(new LogicalSearchQuery(condition)).toArray(new Record[0]);
+        List<List<String>> splittedIds = getChunkList(ids, 1000);
+        for (List<String> idChunk : splittedIds) {
+            LogicalSearchCondition condition = from(metadataSchemasManager.getSchemaTypes(getCollection()).getSchemaType(schemaType)).where(Schemas.IDENTIFIER).isIn(idChunk);
+            List<Record> recordChunk = searchServices.search(new LogicalSearchQuery(condition));
+            allRecords.addAll(recordChunk);
+		}
+        allRecords.sort(new Comparator<Record>() {
+			@Override
+			public int compare(Record o1, Record o2) {
+				Integer indexOfo1 = ids.indexOf(o1.getId());
+				Integer indexOfo2 = ids.indexOf(o2.getId());
+				return indexOfo1.compareTo(indexOfo2);
+			}
+        });
+        return allRecords.toArray(new Record[0]);
+    }
+    
+    private <T> List<List<T>> getChunkList(List<T> largeList , int chunkSize) {
+        List<List<T>> chunkList = new ArrayList<>();
+        for (int i = 0 ; i <  largeList.size() ; i += chunkSize) {
+            chunkList.add(largeList.subList(i , i + chunkSize >= largeList.size() ? largeList.size() : i + chunkSize));
+        }
+        return chunkList;
     }
 
     private List<Element> fillEmptyTags(List<Element> originalElements) {
