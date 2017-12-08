d3803b7b1d2212ec80b7f4bf83f925e5a4bea4c2
Nicolas Bélisle <nicolas.belisle@doculibre.com>
2017-12-08 17:13:03 -0500

    Support for proxies

--- app/src/com/constellio/app/servlet/ConstellioCmisAtomPubServlet.java
+++ app/src/com/constellio/app/servlet/ConstellioCmisAtomPubServlet.java
@@ -2,8 +2,13 @@ package com.constellio.app.servlet;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.chemistry.opencmis.server.impl.atompub.CmisAtomPubServlet;
+
+import java.io.IOException;
 
 public class ConstellioCmisAtomPubServlet extends CmisAtomPubServlet {
 
@@ -13,5 +18,20 @@ public class ConstellioCmisAtomPubServlet extends CmisAtomPubServlet {
 		if (ConstellioCmisServices.enabled) {
 			super.init(config);
 		}
+	}
+
+	@Override
+	protected void service(final HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
+		HttpServletRequestWrapper requestProxyWrapper = new HttpServletRequestWrapper(request) {
+			public String getScheme() {
+				String scheme = request.getHeader("X-Forwarded-Proto");
+				if (!"http".equalsIgnoreCase(scheme) && !"https".equalsIgnoreCase(scheme)) {
+					scheme = request.getScheme();
+				}
+				return scheme;
+			}
+		};
+		super.service(requestProxyWrapper, response);
 	}
 }
