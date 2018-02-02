3da3b93f89d1f1f65c74777d1b95fdb04a00c3b5
Vincent Dussault <vincent.dussault@constellio.com>
2018-02-02 18:39:29 -0500

    Minimize/maximize base window

--- app/src/com/constellio/app/ui/framework/components/BaseWindow.java
+++ app/src/com/constellio/app/ui/framework/components/BaseWindow.java
@@ -3,7 +3,7 @@ package com.constellio.app.ui.framework.components;
 import com.constellio.app.services.factories.ConstellioFactories;
 import com.constellio.app.ui.application.ConstellioUI;
 import com.constellio.app.ui.pages.base.SessionContext;
-import com.vaadin.server.Responsive;
+import com.vaadin.server.Page;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.JavaScript;
 import com.vaadin.ui.Window;
@@ -13,6 +13,13 @@ public class BaseWindow extends Window {
 	public static final int OVER_ADVANCED_SEARCH_FORM_Z_INDEX = 20001;
 	
 	private Integer zIndex = null;
+	
+	private float widthBeforeMinimize;
+	private float heightBeforeMinimize;
+	private Unit widthUnitsBeforeMinimize;
+	private Unit heightUnitsBeforeMinimize;
+	private int positionXBeforeMinimize;
+	private int positionYBeforeMinimize;
 
 	public BaseWindow() {
 		init();
@@ -27,6 +34,37 @@ public class BaseWindow extends Window {
 		super(caption, content);
 		init();
 	}
+	
+	public void minimize() {
+		widthBeforeMinimize = getWidth();
+		heightBeforeMinimize = getHeight();
+		
+		widthUnitsBeforeMinimize = getWidthUnits();
+		heightUnitsBeforeMinimize = getHeightUnits();
+		
+		positionXBeforeMinimize = getPositionX();
+		positionYBeforeMinimize = getPositionY();
+		
+		int browserWidth = Page.getCurrent().getBrowserWindowWidth();
+		int browserHeight = Page.getCurrent().getBrowserWindowHeight();
+		
+		float minimizedWith = 150;
+		float mimizedHeight = 90;
+		int minimizedPositionX = (int) (browserWidth - minimizedWith);
+		int minimizedPositionY = (int) (browserHeight - mimizedHeight);
+		
+		setWidth(minimizedWith + "px");
+		setHeight(mimizedHeight + "px");
+		setPositionX(minimizedPositionX);
+		setPositionY(minimizedPositionY);
+	}
+	
+	public void restoreMinimized() {
+		setWidth(widthBeforeMinimize, widthUnitsBeforeMinimize);
+		setHeight(heightBeforeMinimize, heightUnitsBeforeMinimize);
+		setPositionX(positionXBeforeMinimize);
+		setPositionY(positionYBeforeMinimize);
+	} 
 	
 	private void init() {
 	}
