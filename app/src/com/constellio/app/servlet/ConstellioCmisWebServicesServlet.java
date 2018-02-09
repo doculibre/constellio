6c689dd884e8ce7d690a9c2647242bdccb3c69b9
Nicolas Bélisle <nicolas.belisle@doculibre.com>
2018-02-08 22:30:56 -0500

    Fix issue with Apache Chemistry SOAP and Azure AD client.

--- app/src/com/constellio/app/servlet/ConstellioCmisWebServicesServlet.java
+++ app/src/com/constellio/app/servlet/ConstellioCmisWebServicesServlet.java
@@ -6,7 +6,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.chemistry.opencmis.server.impl.webservices.CallContextHandlerInterceptor;
 import org.apache.chemistry.opencmis.server.impl.webservices.CmisWebServicesServlet;
+import org.apache.chemistry.opencmis.server.impl.webservices.SoapActionRemoveInterceptor;
+import org.apache.chemistry.opencmis.server.impl.webservices.UsernameTokenInterceptor;
+import org.apache.cxf.Bus;
+import org.apache.cxf.binding.soap.SoapMessage;
+import org.apache.cxf.binding.soap.interceptor.AbstractSoapInterceptor;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.interceptor.Interceptor;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.PhaseInterceptor;
+
+import java.util.Collection;
+import java.util.Set;
 
 public class ConstellioCmisWebServicesServlet extends CmisWebServicesServlet {
 
@@ -39,5 +52,67 @@ public class ConstellioCmisWebServicesServlet extends CmisWebServicesServlet {
 			}
 		};
 		super.handleRequest(requestProxyWrapper, response);
+	}
+
+	/**
+	 * Adds and configures interceptors for OpenCMIS.
+	 *
+	 * Override this method to add more interceptors.
+	 */
+	protected void configureInterceptors(Bus bus) {
+		bus.getInInterceptors().add(new SoapInterceptorDecorator(new SoapActionRemoveInterceptor()));
+		bus.getInInterceptors().add(new SoapInterceptorDecorator(new CallContextHandlerInterceptor(getCallContextHandler())));
+		bus.getInInterceptors().add(new SoapInterceptorDecorator(new UsernameTokenInterceptor()));
+	}
+
+	/**
+	 * Only handles SoapMessage
+	 */
+	private static class SoapInterceptorDecorator implements Interceptor<Message>, PhaseInterceptor<Message> {
+
+		AbstractSoapInterceptor interceptor;
+
+		public SoapInterceptorDecorator(AbstractSoapInterceptor interceptor) {
+			this.interceptor = interceptor;
+		}
+
+		@Override
+		public void handleMessage(Message message) throws Fault {
+			if (message instanceof SoapMessage) {
+				interceptor.handleMessage((SoapMessage) message);
+			}
+		}
+
+		@Override
+		public void handleFault(Message message) {
+			if (message instanceof SoapMessage) {
+				interceptor.handleMessage((SoapMessage) message);
+			}
+		}
+
+		@Override
+		public Set<String> getAfter() {
+			return interceptor.getAfter();
+		}
+
+		@Override
+		public Set<String> getBefore() {
+			return interceptor.getBefore();
+		}
+
+		@Override
+		public String getId() {
+			return interceptor.getId();
+		}
+
+		@Override
+		public String getPhase() {
+			return interceptor.getPhase();
+		}
+
+		@Override
+		public Collection<PhaseInterceptor<? extends Message>> getAdditionalInterceptors() {
+			return interceptor.getAdditionalInterceptors();
+		}
 	}
 }
