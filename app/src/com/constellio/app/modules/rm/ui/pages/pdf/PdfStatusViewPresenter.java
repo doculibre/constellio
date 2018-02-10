ff306c6b9e66cfedd2dec9629a45365aa0f2fe26
Hery Raveloson <hery.raveloson@constellio.com>
2018-02-09 22:05:11 -0500

    Ajout de username dans le batch

--- app/src/com/constellio/app/modules/rm/ui/pages/pdf/PdfStatusViewPresenter.java
+++ app/src/com/constellio/app/modules/rm/ui/pages/pdf/PdfStatusViewPresenter.java
@@ -8,16 +8,15 @@ import com.constellio.app.modules.rm.ui.pages.pdf.table.PdfStatusMessageProvider
 import com.constellio.app.services.factories.ConstellioFactories;
 import com.constellio.app.ui.application.ConstellioUI;
 import com.constellio.app.ui.pages.base.BasePresenter;
-import com.constellio.model.entities.batchprocess.AsyncTask;
-import com.constellio.model.entities.batchprocess.AsyncTaskBatchProcess;
-import com.constellio.model.entities.batchprocess.AsyncTaskCreationRequest;
-import com.constellio.model.entities.batchprocess.BatchProcess;
+import com.constellio.model.entities.batchprocess.*;
 import com.constellio.model.entities.records.wrappers.User;
+import com.constellio.model.frameworks.validation.ValidationException;
 import com.constellio.model.services.batch.manager.BatchProcessesManager;
 import com.constellio.model.services.factories.ModelLayerFactory;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
+import java.util.concurrent.Executors;
 
 public class PdfStatusViewPresenter extends BasePresenter<PdfStatusView> {
     private static final String PDF_GENERATION = "PdfGeneration";
@@ -64,7 +63,13 @@ public class PdfStatusViewPresenter extends BasePresenter<PdfStatusView> {
 
         PdfGeneratorAsyncTask asyncTask = new PdfGeneratorAsyncTask(documentIds, null,
                 pdfFileName, pdfFileName, userName, withMetadata);
-        batchProcessesManager.addAsyncTask(new AsyncTaskCreationRequest(asyncTask, collection, PDF_GENERATION));
+
+        /*progressInfo = asyncTask.getProgressInfo();*/
+
+        AsyncTaskCreationRequest request = new AsyncTaskCreationRequest(asyncTask, collection, PDF_GENERATION);
+        request.setUsername(userName);
+
+        batchProcessesManager.addAsyncTask(request);
 
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -87,6 +92,23 @@ public class PdfStatusViewPresenter extends BasePresenter<PdfStatusView> {
                 }
             }
         }, 2000, 2000);
+
+        /*Executors.defaultThreadFactory().newThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    AsyncTaskExecutionParams params = new AsyncTaskExecutionParams() {
+                        @Override
+                        public String getCollection() {
+                            return view.getCollection();
+                        }
+                    };
+                    asyncTask.execute(params);
+                } catch (ValidationException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();*/
     }
 
     @Nullable
