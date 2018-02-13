6d4bb6a26fdc1898862ccdba7340dd205948e1fe
francisbaril <baril.francis@gmail.com>
2018-02-13 10:53:33 -0500

    removed core migration no longer required

--- app/src/com/constellio/app/services/migrations/ConstellioEIM.java
+++ app/src/com/constellio/app/services/migrations/ConstellioEIM.java
@@ -58,7 +58,6 @@ public class ConstellioEIM {
 		scripts.add(new CoreMigrationTo_7_6_6());
 		scripts.add(new CoreMigrationTo_7_6_6_45());
 		scripts.add(new CoreMigrationTo_7_6_9());
-		scripts.add(new CoreMigrationTo_7_7());
 		scripts.add(new CoreMigrationTo_7_7_0_1());
 
 		return scripts;
