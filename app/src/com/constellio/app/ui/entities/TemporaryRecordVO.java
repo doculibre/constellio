904dffd4c461a7341063a6bb687a098b8aa3db26
Vincent Dussault <vincent.dussault@constellio.com>
2018-02-15 14:18:45 -0500

    Consolidated PDF window adjustements

---
+++ app/src/com/constellio/app/ui/entities/TemporaryRecordVO.java
@@ -0,0 +1,58 @@
+package com.constellio.app.ui.entities;
+
+import static com.constellio.model.entities.records.wrappers.TemporaryRecord.CONTENT;
+import static com.constellio.model.entities.records.wrappers.TemporaryRecord.DAY_BEFORE_DESTRUCTION;
+import static com.constellio.model.entities.records.wrappers.TemporaryRecord.DESTRUCTION_DATE;
+
+import java.util.List;
+
+import org.joda.time.LocalDateTime;
+
+import com.constellio.app.ui.entities.ContentVersionVO;
+import com.constellio.app.ui.entities.MetadataValueVO;
+import com.constellio.app.ui.entities.RecordVO;
+import com.constellio.model.entities.schemas.Schemas;
+
+public class TemporaryRecordVO extends RecordVO {
+
+	public TemporaryRecordVO(String id, List<MetadataValueVO> metadataValues, VIEW_MODE viewMode) {
+		super(id, metadataValues, viewMode);
+	}
+	
+	public TemporaryRecordVO(RecordVO recordVO) {
+		super(recordVO.getId(), recordVO.getMetadataValues(), recordVO.getViewMode());
+	}
+
+    public LocalDateTime getCreatedOn() {
+        return get(Schemas.CREATED_ON);
+    }
+
+    public void setCreatedOn(LocalDateTime creationDate) {
+        set(Schemas.CREATED_ON, creationDate);
+    }
+
+    public LocalDateTime getDestructionDate() {
+        return get(DESTRUCTION_DATE);
+    }
+
+    public void setDestructionDate(LocalDateTime destructionDate) {
+        set(DESTRUCTION_DATE, destructionDate);
+    }
+
+	public ContentVersionVO getContent() {
+		return get(CONTENT);
+	}
+
+	public void setContent(ContentVersionVO content) {
+		set(CONTENT, content);
+	}
+
+    public int getNumberOfDaysBeforeDestruction(){
+        return Integer.parseInt((String) get(DAY_BEFORE_DESTRUCTION));
+    }
+
+    public void setNumberOfDaysBeforeDestruction(int numberOfDaysBeforeDestruction) {
+        set(DAY_BEFORE_DESTRUCTION, numberOfDaysBeforeDestruction);
+    }
+
+}
