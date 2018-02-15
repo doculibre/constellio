e881b667c7677976383e2065dc9cb2807b3ed9ff
Charles <charles.blanchette@doculibre.com>
2018-02-15 13:06:40 -0500

    Fix pdfGeneratorAsyncTask

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo7_7_0.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo7_7_0.java
@@ -32,7 +32,7 @@ public class RMMigrationTo7_7_0 extends MigrationHelper implements MigrationScri
 
         @Override
         protected void migrate(MetadataSchemaTypesBuilder typesBuilder) {
-            MetadataSchemaBuilder builder = typesBuilder.getSchemaType(TemporaryRecord.SCHEMA_TYPE).createCustomSchema(DocumentListPDF.SCHEMA_NAME)
+            MetadataSchemaBuilder builder = typesBuilder.getSchemaType(TemporaryRecord.SCHEMA_TYPE).createCustomSchema(DocumentListPDF.SCHEMA)
                     .addLabel(Language.French,"Liste de document pdf")
                     .addLabel(Language.English, "Document pdf list").getDefaultSchema();
         }
