3cb8ff29d06cd5b21354376bbfa494a0d5e17f55
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2018-02-28 11:33:22 -0500

    fixed some labels

---
+++ app/src/com/constellio/app/modules/es/migrations/ESMigrationTo7_7_0_42.java
@@ -0,0 +1,37 @@
+package com.constellio.app.modules.es.migrations;
+
+import com.constellio.app.entities.modules.MetadataSchemasAlterationHelper;
+import com.constellio.app.entities.modules.MigrationHelper;
+import com.constellio.app.entities.modules.MigrationResourcesProvider;
+import com.constellio.app.entities.modules.MigrationScript;
+import com.constellio.app.modules.es.model.connectors.http.ConnectorHttpDocument;
+import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
+
+import static com.constellio.model.entities.schemas.MetadataValueType.STRING;
+
+public class ESMigrationTo7_7_0_42 extends MigrationHelper implements MigrationScript {
+	@Override
+	public String getVersion() {
+		return "7.7.0.42";
+	}
+
+	@Override
+	public void migrate(String collection, MigrationResourcesProvider migrationResourcesProvider, AppLayerFactory appLayerFactory)
+			throws Exception {
+		new SchemaAlterationFor7_7_0_42(collection, migrationResourcesProvider, appLayerFactory).migrate();
+	}
+
+	class SchemaAlterationFor7_7_0_42 extends MetadataSchemasAlterationHelper {
+
+		protected SchemaAlterationFor7_7_0_42(String collection, MigrationResourcesProvider migrationResourcesProvider,
+				AppLayerFactory appLayerFactory) {
+			super(collection, migrationResourcesProvider, appLayerFactory);
+		}
+
+		@Override
+		protected void migrate(MetadataSchemaTypesBuilder typesBuilder) {
+			//Change labels
+		}
+	}
+}
