1fb4ca23ef030c6961b934fc668f5be72c1a74ed
Nicolas Bélisle <nicolas.belisle@doculibre.com>
2018-03-31 14:12:27 -0400

    Tested Zookeeper leader election.

--- data/src/com/constellio/data/dao/services/leaderElection/ZookeeperLeaderElectionManager.java
+++ data/src/com/constellio/data/dao/services/leaderElection/ZookeeperLeaderElectionManager.java
@@ -1,45 +1,38 @@
 package com.constellio.data.dao.services.leaderElection;
 
-import static org.apache.curator.framework.recipes.leader.LeaderLatch.CloseMode.SILENT;
-
-import java.io.IOException;
-import java.util.UUID;
-
+import com.constellio.data.dao.services.factories.DataLayerFactory;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.leader.LeaderLatch;
 import org.apache.curator.utils.CloseableUtils;
-import org.apache.ignite.Ignite;
-import org.apache.ignite.spi.discovery.DiscoverySpi;
-import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
-import org.apache.ignite.spi.discovery.tcp.ipfinder.zk.TcpDiscoveryZookeeperIpFinder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.constellio.data.dao.services.factories.DataLayerFactory;
+import java.util.UUID;
 
 public class ZookeeperLeaderElectionManager implements LeaderElectionManager {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ZookeeperLeaderElectionManager.class);
 	private static final String PATH = "/constellio/leader";
-	private static final String ID = UUID.randomUUID().toString();
 
 	DataLayerFactory dataLayerFactory;
-	private LeaderLatch leaderLatch;
+	LeaderLatch leaderLatch;
+	String ID = UUID.randomUUID().toString();
 
 	public ZookeeperLeaderElectionManager(DataLayerFactory dataLayerFactory) {
 		this.dataLayerFactory = dataLayerFactory;
 	}
 
 	public boolean isCurrentNodeLeader() {
-		return leaderLatch.hasLeadership();
-
+		boolean leader = leaderLatch.hasLeadership();
+		return leader;
 	}
 
 	@Override
 	public void initialize() {
+		initialize(PATH);
+	}
+
+	void initialize(String path) {
 		CuratorFramework curatorFramework = dataLayerFactory.getCuratorFramework();
 
-		this.leaderLatch = new LeaderLatch(curatorFramework, PATH, ID);
+		this.leaderLatch = new LeaderLatch(curatorFramework, path, ID);
 		try {
 			leaderLatch.start();
 		} catch (Exception e) {
