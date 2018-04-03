330e9f6eb86f50e1ed59bd9e618ddccde311b8f5
francisbaril <baril.francis@gmail.com>
2018-04-02 23:09:07 -0400

    unknown problem

--- sdk/src/HttpURLFetchingServiceAcceptTest.java
+++ sdk/src/HttpURLFetchingServiceAcceptTest.java
@@ -3,14 +3,14 @@ import org.junit.Test;
 import com.constellio.app.modules.es.connectors.http.fetcher.HttpURLFetchingService;
 import com.constellio.sdk.tests.ConstellioTest;
 
-public class HttpURLFetchingServiceAcceptTest extends ConstellioTest{
+public class HttpURLFetchingServiceAcceptTest extends ConstellioTest {
 
 	@Test
 	public void unknownProblem()
 			throws Exception {
 
-
 		HttpURLFetchingService service = new HttpURLFetchingService(10000);
-		service.fetch("http://www.csst.qc.ca/Pages/index.aspx");
+		service.fetch("http://www.cnesst.gouv.qc.ca");
+
 	}
 }
