6cdbbc33475b0d8fe83d935bc6ff9ea5a0708af3
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2018-04-12 10:02:00 -0400

    added taxonomy parent in ListAuthorizationsViewImpl

--- app/src/com/constellio/app/ui/pages/management/authorizations/ListAuthorizationsViewImpl.java
+++ app/src/com/constellio/app/ui/pages/management/authorizations/ListAuthorizationsViewImpl.java
@@ -4,7 +4,9 @@ import static com.constellio.app.ui.i18n.i18n.$;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import com.constellio.app.ui.framework.components.converters.TaxonomyRecordIdToContextCaptionConverter;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.LocalDate;
 import org.vaadin.dialogs.ConfirmDialog;
@@ -240,7 +242,7 @@ public abstract class ListAuthorizationsViewImpl extends BaseViewImpl implements
 		Table table = new BaseTable(getClass().getName(), tableCaption, container);
 		table.setPageLength(container.size());
 		table.addStyleName(source == AuthorizationSource.OWN ? AUTHORIZATIONS : INHERITED_AUTHORIZATIONS);
-		new Authorizations(source, getDisplayMode(), presenter.seeRolesField(), presenter.seeAccessField()).attachTo(table);
+		new Authorizations(source, getDisplayMode(), presenter.seeRolesField(), presenter.seeAccessField(), getSessionContext().getCurrentLocale()).attachTo(table);
 		return table;
 	}
 
@@ -410,14 +412,16 @@ public abstract class ListAuthorizationsViewImpl extends BaseViewImpl implements
 		private boolean seeRolesField;
 		private boolean seeAccessField;
 		private boolean seeMetadataField;
+		private Locale currentLocale;
 		private final JodaDateToStringConverter converter;
 
-		public Authorizations(AuthorizationSource source, DisplayMode mode, boolean seeRolesField, boolean seeAccessField) {
+		public Authorizations(AuthorizationSource source, DisplayMode mode, boolean seeRolesField, boolean seeAccessField, Locale currentLocale) {
 			this.source = source;
 			this.mode = mode;
 			this.seeRolesField = seeRolesField;
 			this.seeAccessField = seeAccessField;
 			this.seeMetadataField = source == AuthorizationSource.INHERITED_FROM_METADATA;
+			this.currentLocale = currentLocale;
 			converter = new JodaDateToStringConverter();
 		}
 
@@ -497,9 +501,13 @@ public abstract class ListAuthorizationsViewImpl extends BaseViewImpl implements
 		@Override
 		public Object generateCell(Table source, Object itemId, Object columnId) {
 			AuthorizationVO authorization = (AuthorizationVO) itemId;
+			TaxonomyRecordIdToContextCaptionConverter taxonomyCaptionConverter = new TaxonomyRecordIdToContextCaptionConverter();
 			switch ((String) columnId) {
 			case CONTENT:
-				return new ReferenceDisplay(authorization.getRecord());
+				ReferenceDisplay referenceDisplay = new ReferenceDisplay(authorization.getRecord());
+				referenceDisplay.setCaption(taxonomyCaptionConverter.convertToPresentation(authorization.getRecord(), String.class,
+						currentLocale));
+				return referenceDisplay;
 			case PRINCIPALS:
 				return buildPrincipalColumn(authorization.getGroups(), authorization.getUsers());
 			case ACCESS:
