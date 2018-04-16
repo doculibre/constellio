518ce17220b92bd8eeea743207886d3161418fa0
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2018-04-16 09:02:18 -0400

    fixed connectors performance and memory usage

--- app/src/com/constellio/app/modules/es/ESConfigs.java
+++ app/src/com/constellio/app/modules/es/ESConfigs.java
@@ -3,10 +3,54 @@ package com.constellio.app.modules.es;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.model.entities.configs.SystemConfiguration;
+import com.constellio.model.entities.configs.SystemConfigurationGroup;
+import com.constellio.model.services.configs.SystemConfigurationsManager;
+import com.constellio.model.services.factories.ModelLayerFactory;
+
+import static com.constellio.app.modules.rm.ConstellioRMModule.ID;
 
 public class ESConfigs {
 
 	static List<SystemConfiguration> configurations = new ArrayList<>();
+
+	public static final SystemConfiguration CONNECTOR_NUMBER_OF_RECORDS_PER_BATCH,
+			CONNECTOR_NUMBER_OF_THREADS;
+
+	static {
+		SystemConfigurationGroup others = new SystemConfigurationGroup(ConstellioESModule.ID, "others");
+
+		add(CONNECTOR_NUMBER_OF_RECORDS_PER_BATCH = others.createInteger("connectorNumberOfRecordsPerBatch").withDefaultValue(50).whichIsHidden());
+
+		add(CONNECTOR_NUMBER_OF_THREADS = others.createInteger("connectorNumberOfThreads").whichIsHidden());
+	}
+
+	static void add(SystemConfiguration configuration) {
+		configurations.add(configuration);
+	}
+
+	SystemConfigurationsManager manager;
+
+	public ESConfigs(AppLayerFactory appLayerFactory) {
+		this.manager = appLayerFactory.getModelLayerFactory().getSystemConfigurationsManager();
+	}
+
+	public ESConfigs(ModelLayerFactory modelLayerFactory) {
+		this.manager = modelLayerFactory.getSystemConfigurationsManager();
+	}
+
+	public ESConfigs(SystemConfigurationsManager manager) {
+		this.manager = manager;
+	}
+
+	public int getConnectorNumberOfRecordsPerBatch() {
+		return manager.getValue(CONNECTOR_NUMBER_OF_RECORDS_PER_BATCH);
+	}
+
+	public int getConnectorNumberOfThreads() {
+		Integer configValue = manager.getValue(CONNECTOR_NUMBER_OF_THREADS);
+		return configValue != null ? configValue: Runtime.getRuntime().availableProcessors();
+	}
 
 }
--- app/src/com/constellio/app/modules/es/services/crawler/DefaultConnectorEventObserver.java
+++ app/src/com/constellio/app/modules/es/services/crawler/DefaultConnectorEventObserver.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.constellio.app.modules.es.ESConfigs;
 import com.constellio.app.modules.es.model.connectors.smb.ConnectorSmbDocument;
 import com.constellio.app.modules.es.model.connectors.smb.ConnectorSmbFolder;
 import com.constellio.model.services.factories.ModelLayerFactory;
@@ -52,7 +53,9 @@ public class DefaultConnectorEventObserver implements ConnectorEventObserver {
 		this.connectorLogger = connectorLogger;
 		this.resourceName = resourceName;
 		this.userServices = es.getModelLayerFactory().newUserServices();
-		BulkRecordTransactionHandlerOptions options = new BulkRecordTransactionHandlerOptions().withRecordsPerBatch(50);
+		ESConfigs esConfigs = new ESConfigs(es.getModelLayerFactory());
+		BulkRecordTransactionHandlerOptions options = new BulkRecordTransactionHandlerOptions()
+				.withRecordsPerBatch(esConfigs.getConnectorNumberOfRecordsPerBatch()).withNumberOfThreads(esConfigs.getConnectorNumberOfThreads());
 		options.getTransactionOptions().setUnicityValidationsEnabled(false);
 		this.handler = new BulkRecordTransactionHandler(es.getRecordServices(), resourceName, options);
 		this.mappingService = new ConnectorMappingService(es);
