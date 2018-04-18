6467efd14bd6dddf5082ab66580e6edb1254cc59
Charles <charles.blanchette@doculibre.com>
2018-04-18 09:46:38 -0400

    Multi languale support

---
+++ app/src/com/constellio/app/ui/pages/management/valueDomains/ListValueDomainParam.java
@@ -0,0 +1,22 @@
+package com.constellio.app.ui.pages.management.valueDomains;
+
+public class ListValueDomainParam {
+     String titleEn;
+     String titleFr;
+
+    public String getTitleEn() {
+        return titleEn;
+    }
+
+    public void setTitleEn(String titleEn) {
+        this.titleEn = titleEn;
+    }
+
+    public String getTitleFr() {
+        return titleFr;
+    }
+
+    public void setTitleFr(String titleFr) {
+        this.titleFr = titleFr;
+    }
+}
