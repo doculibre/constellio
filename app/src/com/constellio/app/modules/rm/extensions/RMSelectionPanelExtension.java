7f4ca759e53f00e0610c8acdf7bec78c067f7c86
Charles <charles.blanchette@doculibre.com>
2018-04-18 11:24:15 -0400

    Multi languale support

--- app/src/com/constellio/app/modules/rm/extensions/RMSelectionPanelExtension.java
+++ app/src/com/constellio/app/modules/rm/extensions/RMSelectionPanelExtension.java
@@ -172,10 +172,11 @@ public class RMSelectionPanelExtension extends SelectionPanelExtension {
 
     public void addDuplicateButton(final AvailableActionsParam param) {
         WindowButton duplicateButton = new WindowButton($("ConstellioHeader.selection.actions.duplicate"), $("ConstellioHeader.selection.actions.duplicate")
-                , WindowButton.WindowConfiguration.modalDialog("50%", "20%")) {
+                , WindowButton.WindowConfiguration.modalDialog("550px", "200px")) {
             @Override
             protected Component buildWindowContent() {
                 VerticalLayout verticalLayout = new VerticalLayout();
+                verticalLayout.setSizeFull();
                 verticalLayout.setSpacing(true);
                 final LookupFolderField field = new LookupFolderField(true);
                 field.focus();
@@ -203,7 +204,7 @@ public class RMSelectionPanelExtension extends SelectionPanelExtension {
                 HorizontalLayout hLayout = new HorizontalLayout();
                 hLayout.setSizeFull();
                 hLayout.addComponent(saveButton);
-                hLayout.setComponentAlignment(saveButton, Alignment.BOTTOM_CENTER);
+                hLayout.setComponentAlignment(saveButton, Alignment.MIDDLE_CENTER);
                 verticalLayout.addComponent(hLayout);
                 return verticalLayout;
             }
