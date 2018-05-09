90f4f28addb095b2882de737795e3a8e5ad79af7
rababMoubine <rabab.moubine@doculibre.com>
2018-05-09 13:56:28 -0400

    Code review corrections

--- sdk/src/com/constellio/dev/VaultMigrationScript.java
+++ sdk/src/com/constellio/dev/VaultMigrationScript.java
@@ -3,6 +3,7 @@ package com.constellio.dev;
 import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.model.entities.records.*;
+import com.constellio.model.entities.records.wrappers.Collection;
 import com.constellio.model.entities.schemas.Metadata;
 import com.constellio.model.entities.schemas.MetadataSchemaType;
 import com.constellio.model.entities.schemas.MetadataSchemaTypes;
@@ -13,19 +14,20 @@ import com.constellio.model.services.records.RecordServices;
 import com.constellio.model.services.records.RecordServicesException;
 import com.constellio.model.services.search.SearchServices;
 import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static com.constellio.app.utils.ScriptsUtils.startLayerFactoriesWithoutBackgroundThreads;
 import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.from;
 
 public class VaultMigrationScript {
 
-	static String currentCollection;
+//	static String currentCollection;
 	static AppLayerFactory appLayerFactory;
 	static ModelLayerFactory modelLayerFactory;
 	static SearchServices searchServices;
@@ -41,58 +43,64 @@ public class VaultMigrationScript {
 
 	}
 
-	public static List<String> improveHashCodes(AppLayerFactory appLayerFactory, String collection) throws Exception {
+	public static Set<String> improveHashCodes(AppLayerFactory appLayerFactory) throws Exception {
 		modelLayerFactory = appLayerFactory.getModelLayerFactory();
 		searchServices = modelLayerFactory.newSearchServices();
 		recordServices = modelLayerFactory.newRecordServices();
-		final List<String> listOfHashCodes = new ArrayList<>();
-		MetadataSchemaTypes metadataSchemaTypes = modelLayerFactory.getMetadataSchemasManager().getSchemaTypes(collection);
-		List<MetadataSchemaType> metadataSchemaTypeList = metadataSchemaTypes.getSchemaTypes();
-		for (final MetadataSchemaType metadataSchemaType : metadataSchemaTypeList) {
-			for (Metadata metadata : metadataSchemaType.getAllMetadatas()) {
-				if (MetadataValueType.CONTENT.equals(metadata.getType())) {
-					final String metadataCode = metadata.getCode();
-					ActionExecutorInBatch actionExecutorInBatch = new ActionExecutorInBatch(searchServices, "", 1000) {
+		final Set<String> setOfHashCodes = new HashSet<>();
+		List<String> collections = modelLayerFactory.getCollectionsListManager().getCollections();
+		for(String collection : collections){
+			MetadataSchemaTypes metadataSchemaTypes = modelLayerFactory.getMetadataSchemasManager().getSchemaTypes(collection);
+			List<MetadataSchemaType> metadataSchemaTypeList = metadataSchemaTypes.getSchemaTypes();
+			for (final MetadataSchemaType metadataSchemaType : metadataSchemaTypeList) {
+				for (Metadata metadata : metadataSchemaType.getAllMetadatas()) {
+					if (MetadataValueType.CONTENT.equals(metadata.getType())) {
+						final String metadataCode = metadata.getCode();
+						ActionExecutorInBatch actionExecutorInBatch = new ActionExecutorInBatch(searchServices, "", 1000) {
 
-						@Override
-						public void doActionOnBatch(List<Record> records) throws Exception {
-							Metadata contentMetadata = metadataSchemaType.getMetadata(metadataCode);
-							Transaction transaction = new Transaction();
-							for (Record record : records) {
-								Content recordContent = record.get(contentMetadata);
-								ContentImpl content = (ContentImpl) recordContent;
-								content.changeHashCodesOfAllVersions();
-								listOfHashCodes.addAll(content.getHashOfAllVersions());
+							@Override
+							public void doActionOnBatch(List<Record> records) throws Exception {
+								Metadata contentMetadata = metadataSchemaType.getMetadata(metadataCode);
+								Transaction transaction = new Transaction();
+								for (Record record : records) {
+									Content recordContent = record.get(contentMetadata);
+									ContentImpl content = (ContentImpl) recordContent;
+									content.changeHashCodesOfAllVersions();
+									setOfHashCodes.addAll(content.getHashOfAllVersions());
+								}
+								transaction.update(records);
+								recordServices.execute(transaction);
 							}
-							transaction.update(records);
-							recordServices.execute(transaction);
-						}
-					};
+						};
 
-					actionExecutorInBatch.execute(new LogicalSearchQuery(from(metadataSchemaType).where(metadata).isNotNull()));
+						actionExecutorInBatch.execute(new LogicalSearchQuery(from(metadataSchemaType).where(metadata).isNotNull()));
+					}
 				}
 			}
 		}
-		return listOfHashCodes;
+		return setOfHashCodes;
 	}
 
-	public static void migrateVault(File parentFile,List<String> listOfHashCodes) throws IOException, RecordServicesException {
+	public static void migrateVault(File parentFile,Set<String> listOfHashCodes) throws Exception {
 //      Longueur du hash : 28
 //      Longueur du parsedHash : 36
-		List<File> leafFiles = getFiles(parentFile);
+		java.util.Collection<File> leafFiles = FileUtils.listFiles(parentFile,null,true);
+		Iterator<File> iterator = leafFiles.iterator();
 		File fileToMove;
-		for (int index = 0; index<leafFiles.size(); index++){
-			fileToMove = leafFiles.get(index);
-			if((fileToMove.getName().length() < 28 && !fileToMove.getName().contains("parsed")) ||
-					(fileToMove.getName().length() < 36 && fileToMove.getName().contains("parsed"))){
-				moveFileToRightPath(fileToMove, parentFile,listOfHashCodes, true);
-			}else if(fileToMove.getName().contains("+")){
-				moveFileToRightPath(fileToMove, parentFile,listOfHashCodes, false);
+			while(iterator.hasNext()) {
+				fileToMove = iterator.next();
+				Boolean firstCondition = fileToMove.getName().length() < 28 && !fileToMove.getName().contains("parsed") && !fileToMove.getName().contains("preview");
+				Boolean secondCondition = fileToMove.getName().length() < 36 && fileToMove.getName().contains("parsed");
+				Boolean thirdCondition = fileToMove.getName().length() < 37 && fileToMove.getName().contains("preview");
+				if (firstCondition || secondCondition || thirdCondition) {
+					moveFileToRightPath(fileToMove, parentFile, listOfHashCodes, true);
+				} else if (fileToMove.getName().contains("+")) {
+					moveFileToRightPath(fileToMove, parentFile, listOfHashCodes, false);
+				}
 			}
-		}
 	}
 
-	private static String rename(List<String> listOfHashCodes, String oldName){
+	private static String rename(Set<String> listOfHashCodes, String oldName){
 		String newName = "";
 		boolean contain = false;
 		String [] oldNameParts =oldName.split("_");
@@ -112,7 +120,7 @@ public class VaultMigrationScript {
 		return newName;
 	}
 
-	private static void moveFileToRightPath(File fileToMove, File parentFile,List<String> listOfHashCodes, boolean changeName) throws IOException {
+	private static void moveFileToRightPath(File fileToMove, File parentFile,Set<String> listOfHashCodes, boolean changeName) throws IOException {
 		File folderToRemove = fileToMove.getParentFile();
 		String newName = "";
 		while(!folderToRemove.getName().equals(parentFile.getName()) && changeName){
@@ -125,26 +133,19 @@ public class VaultMigrationScript {
 			newName = newName.replace("+","-");
 		}
 
-		if((newName.length() < 28 && !newName.contains("parsed"))){
-			newName = rename(listOfHashCodes,newName);
-		}else if(newName.length() < 36 && newName.contains("parsed")){
+		Boolean firstCondition = newName.length() < 28 && !newName.contains("parsed") && !newName.contains("preview");
+		Boolean secondCondition = newName.length() < 36 && newName.contains("parsed");
+		Boolean thirdCondition = newName.length() < 37 && newName.contains("preview");
+		if(firstCondition || secondCondition || thirdCondition){
 			newName = rename(listOfHashCodes,newName);
 		}
-
 		folderToRemove = fileToMove.getParentFile();
 
 		String parentFilePath =  parentFile.getAbsolutePath();
-		createFolder(parentFilePath ,"/" + StringUtils.substring(newName,0,1));
-		createFolder(parentFilePath ,"/" + StringUtils.substring(newName,0,1) + "/" + StringUtils.substring(newName,0,2));
-		File folder = createFolder(parentFilePath, "/" + StringUtils.substring(newName,0,1)+ "/" + StringUtils.substring(newName,0,2) + "/" + StringUtils.substring(newName,0,3));
+		File folder =  new File(parentFilePath+ "/" + StringUtils.substring(newName,0,1)+ "/" + StringUtils.substring(newName,0,2) + "/" + StringUtils.substring(newName,0,3));
+		folder.mkdirs();
 		fileToMove.renameTo(new File(folder.getAbsolutePath() + "/" + newName ));
 		removePathIfEmpty(folderToRemove);
-	}
-
-	private static File createFolder(String parentFilePath, String newPath){
-		File newFolder = new File(parentFilePath+ newPath);
-		newFolder.mkdir();
-		return  newFolder;
 	}
 
 	private static void removePathIfEmpty(File file){
@@ -155,26 +156,12 @@ public class VaultMigrationScript {
 		}
 	}
 
-	private static List<File> getFiles(File file){
-		File[] children = file.listFiles();
-		List<File> listOfFiles = new ArrayList<>();
-		for( int  index=0; index< children.length; index++){
-			if(children[index].isFile()){
-				listOfFiles.add(children[index]);
-			}
-			else{
-				listOfFiles.addAll(getFiles(children[index]));
-			}
-		}
-		return listOfFiles;
-	}
-
 	public static void main(String argv[])
 			throws Exception {
 //		RecordPopulateServices.LOG_CONTENT_MISSING = false;
 		startBackend();
 
-		improveHashCodes(appLayerFactory, currentCollection);
+		improveHashCodes(appLayerFactory);
 	}
 
 }
