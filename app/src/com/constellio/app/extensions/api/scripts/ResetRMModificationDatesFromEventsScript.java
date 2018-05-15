9bbe0933eea2a9fe06d37b4983c708b05f608685
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2018-05-15 10:08:53 -0400

    added script to set back modification dates

---
+++ app/src/com/constellio/app/extensions/api/scripts/ResetRMModificationDatesFromEventsScript.java
@@ -0,0 +1,65 @@
+package com.constellio.app.extensions.api.scripts;
+
+import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
+import com.constellio.app.modules.rm.wrappers.Document;
+import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.data.dao.services.contents.ContentDao;
+import com.constellio.model.entities.records.ConditionnedActionExecutorInBatchBuilder;
+import com.constellio.model.entities.records.Content;
+import com.constellio.model.entities.records.Record;
+import com.constellio.model.entities.records.wrappers.Event;
+import com.constellio.model.entities.records.wrappers.EventType;
+import com.constellio.model.entities.schemas.Schemas;
+import com.constellio.model.services.contents.ContentManager;
+import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
+import com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.from;
+import static java.util.Arrays.asList;
+
+public class ResetRMModificationDatesFromEventsScript extends ScriptWithLogOutput {
+    public ResetRMModificationDatesFromEventsScript(AppLayerFactory appLayerFactory) {
+        super(appLayerFactory, "Record", "ResetRMModificationDatesFromEventsScript");
+    }
+
+    @Override
+    protected void execute() throws Exception {
+        for(String collection : appLayerFactory
+                .getCollectionsManager().getCollectionCodesExcludingSystem()) {
+            final RMSchemasRecordsServices rm = new RMSchemasRecordsServices(collection, appLayerFactory);
+            final HashMap<String, LocalDateTime> documentsLastModificationDate = new HashMap<>();
+
+            onCondition(LogicalSearchQueryOperators.from(rm.eventSchemaType()).where(rm.eventType()).is(EventType.MODIFY_DOCUMENT))
+            .modifyingRecordsWithImpactHandling(new ConditionnedActionExecutorInBatchBuilder.RecordScript() {
+                    @Override
+                    public void modifyRecord(Record record) {
+                        Event event = rm.wrapEvent(record);
+                        String recordId = event.getRecordId();
+                        LocalDateTime createdOn = event.getCreatedOn();
+                        LocalDateTime previousDate = documentsLastModificationDate.get(recordId);
+                        if(previousDate == null || previousDate.isBefore(createdOn)) {
+                            documentsLastModificationDate.put(recordId, createdOn);
+                        }
+                    }
+                }
+            );
+
+            onCondition(LogicalSearchQueryOperators.from(rm.documentSchemaType()).returnAll())
+            .modifyingRecordsWithImpactHandling(new ConditionnedActionExecutorInBatchBuilder.RecordScript() {
+                    @Override
+                    public void modifyRecord(Record record) {
+                        LocalDateTime lastModificationDate = documentsLastModificationDate.get(record.getId());
+                        if(lastModificationDate != null) {
+                            record.set(Schemas.MODIFIED_ON, lastModificationDate);
+                        }
+                    }
+                }
+            );
+        }
+    }
+}
