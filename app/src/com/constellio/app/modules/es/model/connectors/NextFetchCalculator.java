3daefbaa658464f900dd209dc7624c58153c7814
Hery Raveloson <hery.raveloson@constellio.com>
2018-06-03 00:52:23 -0400

    Mise à 6h le nextFecth des documents avec erreur

--- app/src/com/constellio/app/modules/es/model/connectors/NextFetchCalculator.java
+++ app/src/com/constellio/app/modules/es/model/connectors/NextFetchCalculator.java
@@ -4,6 +4,7 @@ import static java.util.Arrays.asList;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.joda.time.LocalDateTime;
 
 import com.constellio.app.modules.es.model.connectors.http.enums.FetchFrequency;
@@ -18,12 +19,19 @@ public class NextFetchCalculator implements MetadataValueCalculator<LocalDateTim
 	LocalDependency<LocalDateTime> fetchedDateTimeParam = LocalDependency.toADateTime(ConnectorDocument.FETCHED_DATETIME);
 	LocalDependency<Double> fetchDelayParam = LocalDependency.toANumber(ConnectorDocument.FETCH_DELAY);
 	LocalDependency<FetchFrequency> fetchFrequencyParam = LocalDependency.toAnEnum(ConnectorDocument.FETCH_FREQUENCY);
+	LocalDependency<String> fetchErrorCode = LocalDependency.toAString(ConnectorDocument.ERROR_CODE);
 
 	@Override
 	public LocalDateTime calculate(CalculatorParameters parameters) {
 		LocalDateTime fetchedDateTime = parameters.get(fetchedDateTimeParam);
 		Double fetchDelay = parameters.get(fetchDelayParam);
-		return fetchedDateTime == null || fetchDelay == null ? null : fetchedDateTime.plusDays(fetchDelay.intValue());
+		String errorCode = parameters.get(fetchErrorCode);
+
+		if(StringUtils.isNotBlank(errorCode)) {
+			return fetchedDateTime == null ? null : fetchedDateTime.plusHours(6);
+		} else {
+			return fetchedDateTime == null || fetchDelay == null ? null : fetchedDateTime.plusDays(fetchDelay.intValue());
+		}
 	}
 
 	@Override
@@ -43,6 +51,6 @@ public class NextFetchCalculator implements MetadataValueCalculator<LocalDateTim
 
 	@Override
 	public List<? extends Dependency> getDependencies() {
-		return asList(fetchedDateTimeParam, fetchDelayParam, fetchFrequencyParam);
+		return asList(fetchedDateTimeParam, fetchDelayParam, fetchFrequencyParam, fetchErrorCode);
 	}
 }
