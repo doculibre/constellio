6fc17fd3cca7fddbef0b64bec1d7e8c42fed1c25
Charles <charles.blanchette@doculibre.com>
2018-06-06 11:26:15 -0400

    CorePermission change

--- model/src/com/constellio/model/entities/CorePermissions.java
+++ model/src/com/constellio/model/entities/CorePermissions.java
@@ -49,6 +49,8 @@ public class CorePermissions {
 	public static final String MANAGE_SYNONYMS = permission(COLLECTION_MANAGEMENT, "manageSynonyms", Toggle.ADVANCED_SEARCH_CONFIGS);
 	public static final String EXCLUDE_AND_RAISE_SEARCH_RESULT = permission(COLLECTION_MANAGEMENT, "excludeAndRaiseOnSearchResult",
 			Toggle.ADVANCED_SEARCH_CONFIGS);
+	public static final String DELETE_CORRECTION_SUGGESTION = permission(COLLECTION_MANAGEMENT, "deleteCorrectorSuggestion", Toggle.ADVANCED_SEARCH_CONFIGS);
+
 
 	public static final List<String> COLLECTION_MANAGEMENT_PERMISSIONS = PERMISSIONS.getGroup(COLLECTION_MANAGEMENT);
 
@@ -67,7 +69,6 @@ public class CorePermissions {
 	public static final String MANAGE_LABELS = permission(SYSTEM_MANAGEMENT, "manageLabels");
 	public static final String MANAGE_PRINTABLE_REPORT = permission(SYSTEM_MANAGEMENT, "managePrintableReport");
 	public static final String VIEW_SYSTEM_BATCH_PROCESSES = permission(SYSTEM_MANAGEMENT, "viewSystemBatchProcesses");
-	public static final String DELETE_CORRECTION_SUGGESTION = permission(SYSTEM_MANAGEMENT, "deleteCorrectorSuggestion", Toggle.ADVANCED_SEARCH_CONFIGS);
 
 	public static final List<String> SYSTEM_MANAGEMENT_PERMISSIONS = PERMISSIONS.getGroup(SYSTEM_MANAGEMENT);
 
