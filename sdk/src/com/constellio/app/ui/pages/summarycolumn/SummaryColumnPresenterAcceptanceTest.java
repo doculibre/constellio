0dab2de14a2d7c2c9126f5c36332f259dc66e2c6
jplamondon <jonathan.plamondon@constellio.com>
2018-06-08 21:58:09 -0400

    SummaryColumn improuvement

--- sdk/src/com/constellio/app/ui/pages/summarycolumn/SummaryColumnPresenterAcceptanceTest.java
+++ sdk/src/com/constellio/app/ui/pages/summarycolumn/SummaryColumnPresenterAcceptanceTest.java
@@ -1,6 +1,7 @@
 package com.constellio.app.ui.pages.summarycolumn;
 
 import com.constellio.app.modules.rm.RMTestRecords;
+import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
 import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.ui.framework.builders.MetadataToVOBuilder;
 import com.constellio.app.ui.pages.base.SessionContext;
@@ -16,6 +17,7 @@ import org.mockito.Mock;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -31,6 +33,7 @@ public class SummaryColumnPresenterAcceptanceTest extends ConstellioTest {
     SummaryColumnPresenter summaryColumnPresenter;
     Users users;
     RMTestRecords records = new RMTestRecords(zeCollection);
+    RMSchemasRecordsServices rmSchemasRecordsServices;
 
     @Before
     public void setUp() {
@@ -46,6 +49,8 @@ public class SummaryColumnPresenterAcceptanceTest extends ConstellioTest {
         when(sessionContext.getCurrentLocale()).thenReturn(Locale.FRENCH);
 
         summaryColumnPresenter = new SummaryColumnPresenter(view, Folder.DEFAULT_SCHEMA);
+
+        rmSchemasRecordsServices = new RMSchemasRecordsServices(zeCollection, getAppLayerFactory());
     }
 
     @Test
@@ -69,5 +74,38 @@ public class SummaryColumnPresenterAcceptanceTest extends ConstellioTest {
         assertThat(summaryColumnParams.getMetadataVO().getCode()).isEqualTo(Schemas.TITLE.getCode());
         assertThat(summaryColumnParams.getPrefix()).isEqualTo("a");
         assertThat(summaryColumnParams.getDisplayCondition()).isEqualTo(SummaryColumnParams.DisplayCondition.ALWAYS);
+    }
+
+    @Test
+    public void givenOneMetatSummaryInFolderEmployeThenMetadataHaveMetadataSummy() {
+        SummaryColumnPresenter summaryColumnPresenterFolderEmploye = new SummaryColumnPresenter(view, "folder_employe");
+
+        MetadataToVOBuilder builder = new MetadataToVOBuilder();
+        SummaryColumnParams summaryColumnParams1 = new SummaryColumnParams();
+        summaryColumnParams1.setPrefix("a");
+        summaryColumnParams1.setDisplayCondition(SummaryColumnParams.DisplayCondition.ALWAYS);
+        summaryColumnParams1.setMetadataVO(builder.build(rmSchemasRecordsServices.folder.description(), FakeSessionContext.adminInCollection(zeCollection)));
+
+        summaryColumnPresenter.addMetadaForSummary(summaryColumnParams1);
+
+        summaryColumnPresenterFolderEmploye.getMetadatas();
+        SummaryColumnParams summaryColumnParams2 = new SummaryColumnParams();
+        summaryColumnParams2.setPrefix("b");
+        summaryColumnParams2.setDisplayCondition(SummaryColumnParams.DisplayCondition.COMPLETED);
+        summaryColumnParams2.setMetadataVO(builder.build(rmSchemasRecordsServices.folder.title(), FakeSessionContext.adminInCollection(zeCollection)));
+
+        summaryColumnPresenterFolderEmploye.addMetadaForSummary(summaryColumnParams2);
+
+        Metadata metadata = summaryColumnPresenterFolderEmploye.getSummaryMetadata();
+
+        List summaryColomnList = (List) metadata.getCustomParameter().get(SummaryColumnPresenter.SUMMARY_COLOMN);
+
+        Map<String,Object> summarymap = (Map<String, Object>) summaryColomnList.get(0);
+
+        assertThat(summaryColomnList).isNotNull();
+        assertThat(summaryColomnList.size()).isEqualTo(1);
+        assertThat(summarymap.get(SummaryColumnPresenter.METADATA_CODE)).isEqualTo(rmSchemasRecordsServices.folder.title().getCode());
+        assertThat(summarymap.get(SummaryColumnPresenter.PREFIX)).isEqualTo("b");
+        assertThat(summarymap.get(SummaryColumnPresenter.IS_ALWAYS_SHOWN)).isEqualTo(true);
     }
 }
