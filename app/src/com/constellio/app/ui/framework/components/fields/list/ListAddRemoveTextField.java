02aff08119e27784ea9d15fb3dca7f622eb5b1eb
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2018-06-18 01:33:41 -0400

    Added support to input mask on multivalue metadatas

--- app/src/com/constellio/app/ui/framework/components/fields/list/ListAddRemoveTextField.java
+++ app/src/com/constellio/app/ui/framework/components/fields/list/ListAddRemoveTextField.java
@@ -1,12 +1,17 @@
 package com.constellio.app.ui.framework.components.fields.list;
 
 import com.constellio.app.ui.framework.components.fields.BaseTextField;
+import com.constellio.model.utils.MaskUtils;
+import com.constellio.model.utils.MaskUtilsException;
+import com.vaadin.data.Validator;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextField;
 
 @SuppressWarnings("unchecked")
 public class ListAddRemoveTextField extends ListAddRemoveField<String, TextField> {
+
+	private String inputMask = null;
 
 	@Override
 	protected Component initContent() {
@@ -19,7 +24,26 @@ public class ListAddRemoveTextField extends ListAddRemoveField<String, TextField
 
 	@Override
 	protected TextField newAddEditField() {
-		return new BaseTextField();
+		BaseTextField baseTextField = new BaseTextField();
+		if(inputMask != null) {
+			baseTextField.setInputMask(inputMask);
+//			baseTextField.addValidator(new Validator() {
+//				@Override
+//				public void validate(Object value) throws InvalidValueException {
+//					try {
+//						if(value != null) {
+//							MaskUtils.validate(inputMask, (String) value);
+//						}
+//					} catch (MaskUtilsException e) {
+//						throw new InvalidValueException(e.getMessage());
+//					}
+//				}
+//			});
+		}
+		return baseTextField;
 	}
 
+	public void setInputMask(String inputMask) {
+		this.inputMask = inputMask;
+	}
 }
