4fa34da1ea0bf4d446f71888d8ee57951a818a34
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2018-06-28 11:41:59 -0400

    added script to rm migration

--- app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
+++ app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
@@ -173,7 +173,8 @@ public class ConstellioRMModule implements InstallableSystemModule, ModuleWithCo
 				new RMMigrationTo7_7_0_42(),
 				new RMMigrationTo7_7_1(),
 				new RMMigrationTo7_7_2(),
-				new RMMigrationTo7_7_3()
+				new RMMigrationTo7_7_3(),
+				new RMMigrationTo8_0_2()
 		);
 	}
 
