a3fce3cadd38401ed0d421daf903a14e288da58b
rababMoubine <rabab.moubine@doculibre.com>
2018-07-05 15:28:25 -0400

    Enable inactif schema in advanced search config

--- app/src/com/constellio/app/ui/pages/search/SearchCriteriaPresenterUtils.java
+++ app/src/com/constellio/app/ui/pages/search/SearchCriteriaPresenterUtils.java
@@ -1,9 +1,11 @@
 package com.constellio.app.ui.pages.search;
 
+import com.constellio.app.services.factories.ConstellioFactories;
 import com.constellio.app.ui.pages.base.BasePresenterUtils;
 import com.constellio.app.ui.pages.base.SessionContext;
 import com.constellio.model.entities.Language;
 import com.constellio.model.entities.schemas.MetadataSchema;
+import com.constellio.model.services.migrations.ConstellioEIMConfigs;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,13 +18,19 @@ public class SearchCriteriaPresenterUtils extends BasePresenterUtils{
     }
 
     public final Map<String,String> getMetadataSchemasList(String schemaTypeCode){
+        ConstellioFactories constellioFactories = ConstellioFactories.getInstance();
+        ConstellioEIMConfigs configs = new ConstellioEIMConfigs(
+                constellioFactories.getModelLayerFactory().getSystemConfigurationsManager());
         Map<String,String> metadataSchemasMap = new HashMap<>();
         String collection = sessionContext.getCurrentCollection();
         String language = sessionContext.getCurrentLocale().getLanguage();
         List<MetadataSchema> metadataSchemas = getConstellioFactories().getModelLayerFactory().getMetadataSchemasManager().getSchemaTypes(collection)
                 .getSchemaType(schemaTypeCode).getSchemas();
+
         for (MetadataSchema metadataSchema : metadataSchemas){
-            metadataSchemasMap.put(metadataSchema.getCode(), metadataSchema.getLabel(Language.withCode(language)));
+            if(metadataSchema.isActive() || configs.areInactifSchemasEnabledInSearch()){
+                metadataSchemasMap.put(metadataSchema.getCode(), metadataSchema.getLabel(Language.withCode(language)));
+            }
         }
         return metadataSchemasMap;
     }
