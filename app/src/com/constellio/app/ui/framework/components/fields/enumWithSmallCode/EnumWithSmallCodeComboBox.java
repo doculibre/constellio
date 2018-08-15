b2007e47b85a5f9fdc6f16c066afbb78a813d325
francisbaril <baril.francis@gmail.com>
2018-08-15 09:19:05 -0400

    fix

--- app/src/com/constellio/app/ui/framework/components/fields/enumWithSmallCode/EnumWithSmallCodeComboBox.java
+++ app/src/com/constellio/app/ui/framework/components/fields/enumWithSmallCode/EnumWithSmallCodeComboBox.java
@@ -1,17 +1,12 @@
 package com.constellio.app.ui.framework.components.fields.enumWithSmallCode;
 
-import static com.constellio.app.ui.i18n.i18n.$;
-
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-import com.constellio.app.modules.rm.model.enums.DisposalType;
 import com.constellio.app.ui.application.ConstellioUI;
 import com.constellio.app.ui.framework.components.fields.BaseComboBox;
 import com.constellio.app.ui.pages.base.SessionContext;
 import com.constellio.model.entities.EnumWithSmallCode;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import static com.constellio.app.ui.i18n.i18n.$;
@@ -38,15 +33,19 @@ public class EnumWithSmallCodeComboBox<E extends EnumWithSmallCode> extends Base
 
 	@Override
 	public void setOptions(List<EnumWithSmallCode> enumConstants) {
-		Collections.sort(enumConstants, new Comparator<EnumWithSmallCode>() {
-			@Override
-			public int compare(EnumWithSmallCode o1, EnumWithSmallCode o2) {
-				String caption1 = $(DisposalType.class.getSimpleName() + "." + o1.getCode());
-				String caption2 = $(DisposalType.class.getSimpleName() + "." + o2.getCode());
-				return caption1.compareTo(caption2);
-			}
-		});
-	    for (EnumWithSmallCode enumWithSmallCode : enumConstants) {
+
+		if (!enumConstants.isEmpty()) {
+			final Class enumClass = enumConstants.get(0).getClass();
+			Collections.sort(enumConstants, new Comparator<EnumWithSmallCode>() {
+				@Override
+				public int compare(EnumWithSmallCode o1, EnumWithSmallCode o2) {
+					String caption1 = $(enumClass.getSimpleName() + "." + o1.getCode());
+					String caption2 = $(enumClass.getSimpleName() + "." + o2.getCode());
+					return caption1.compareTo(caption2);
+				}
+			});
+		}
+		for (EnumWithSmallCode enumWithSmallCode : enumConstants) {
 			String enumCode = enumWithSmallCode.getCode();
 			if (!isIgnored(enumCode)) {
 				addItem(enumWithSmallCode);
