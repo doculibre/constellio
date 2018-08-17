b6eaec0cfe26bd8f0f4829647f1724ed19ed80f8
rababMoubine <rabab.moubine@doculibre.com>
2018-08-17 14:21:03 -0400

    Added test

--- sdk/src/com/constellio/app/modules/tasks/caches/UnreadTasksUserCacheInvalidationAcceptanceTest.java
+++ sdk/src/com/constellio/app/modules/tasks/caches/UnreadTasksUserCacheInvalidationAcceptanceTest.java
@@ -1,12 +1,18 @@
 package com.constellio.app.modules.tasks.caches;
 
+import com.constellio.app.modules.tasks.extensions.TaskRecordExtension;
+import com.constellio.app.modules.tasks.model.wrappers.Task;
 import com.constellio.app.modules.tasks.services.TasksSchemasRecordsServices;
 import com.constellio.app.modules.tasks.services.TasksSearchServices;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.model.entities.records.wrappers.User;
+import com.constellio.model.entities.schemas.Schemas;
+import com.constellio.model.services.records.RecordServices;
+import com.constellio.model.services.records.RecordServicesException;
 import com.constellio.sdk.tests.ConstellioTest;
 import com.constellio.sdk.tests.setups.Users;
 import org.assertj.core.api.ListAssert;
+import org.joda.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,13 +31,20 @@ public class UnreadTasksUserCacheInvalidationAcceptanceTest extends ConstellioTe
 	UnreadTasksUserCache instance1Cache;
 	UnreadTasksUserCache instance2Cache;
 
+	Task task;
+	TasksSchemasRecordsServices schemas;
+	RecordServices recordServices;
+	User userAlice;
+
+
 	@Before
 	public void setUp() throws Exception {
 		prepareSystem(withCollection(zeCollection).withTasksModule().withAllTest(users));
 		instance1Cache = getModelLayerFactory().getCachesManager().getUserCache(UnreadTasksUserCache.NAME);
 		instance2Cache = getModelLayerFactory("other-instance").getCachesManager().getUserCache(UnreadTasksUserCache.NAME);
 		linkEventBus(getDataLayerFactory(), getDataLayerFactory("other-instance"));
-
+		schemas = new TasksSchemasRecordsServices(zeCollection, getAppLayerFactory());
+		recordServices = getModelLayerFactory().newRecordServices();
 		loadCache();
 	}
 
@@ -52,6 +65,24 @@ public class UnreadTasksUserCacheInvalidationAcceptanceTest extends ConstellioTe
 		instance1Cache.invalidateUsersInGroup(users.heroes());
 		assertThatInvalidatedUsers().containsOnly(charles, dakota, robin, gandalf);
 
+	}
+
+	@Test
+	public void whenCreatingNewTaskThenInvalidateAssignees1() throws RecordServicesException {
+		//TODO Rabab : Effectuer des changements dans l'application et valider que la cache est bien invalidée.
+		String aliceId = (userAlice = users.aliceIn(zeCollection)).getId();
+		String adminId = (userAlice = users.adminIn(zeCollection)).getId();
+		task = schemas.newTask();
+		task.setTitle("task").setAssignee(aliceId).setAssigner(adminId).setAssignationDate(LocalDate.now()).setAssignedOn(LocalDate.now());
+		task.getWrappedRecord().set(Schemas.LOGICALLY_DELETED_STATUS, true);
+		save(task);
+		recordServices.physicallyDelete(task.getWrappedRecord(),User.GOD);
+		assertThatInvalidatedUsers().containsOnly(alice);
+	}
+
+	private void save(Task task)
+			throws RecordServicesException {
+		recordServices.add(task.getWrappedRecord());
 	}
 
 
