9b33828e5e55205fc04218c9a8fa6d83ded0f012
ericgiguere <eric.giguere@constellio.com>
2018-08-21 15:53:15 -0400

    Fix test preventing gradle build

--- sdk/src/com/constellio/model/entities/LanguageTest.java
+++ sdk/src/com/constellio/model/entities/LanguageTest.java
@@ -1,5 +1,6 @@
 package com.constellio.model.entities;
 
+import com.constellio.sdk.tests.ConstellioTest;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -7,64 +8,65 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class LanguageTest {
-    @Test
-    public void givenLanguageElementsThenSupported() {
-        Language[] values = Language.values();
-        for(Language l:values) {
-            assertThat(Language.isSupported(l.code));
-        }
-    }
+public class LanguageTest extends ConstellioTest {
 
-    @Test
-    public void givenLanguageElementsThenGettersOk() {
-        Language[] values = Language.values();
-        for(Language l:values) {
-            assertThat(l.code).isEqualTo(l.getCode());
-            assertThat(l.locale).isEqualTo(l.getLocale());
-        }
-    }
+	@Test
+	public void givenLanguageElementsThenSupported() {
+		Language[] values = Language.values();
+		for (Language l : values) {
+			assertThat(Language.isSupported(l.code));
+		}
+	}
 
-    @Test
-    public void givenLanguageElementsThenWithLocaleOk() {
-        Language[] values = Language.values();
-        for(Language l:values) {
-            assertThat(Language.withLocale(l.locale)).isEqualTo(l);
-        }
-    }
+	@Test
+	public void givenLanguageElementsThenGettersOk() {
+		Language[] values = Language.values();
+		for (Language l : values) {
+			assertThat(l.code).isEqualTo(l.getCode());
+			assertThat(l.locale).isEqualTo(l.getLocale());
+		}
+	}
 
-    @Test
-    public void givenCodeNullWhenWithCodeThenNull() {
-        assertThat(Language.withCode(null)).isNull();
-    }
+	@Test
+	public void givenLanguageElementsThenWithLocaleOk() {
+		Language[] values = Language.values();
+		for (Language l : values) {
+			assertThat(Language.withLocale(l.locale)).isEqualTo(l);
+		}
+	}
 
-    @Test
-    public void givenCodeUnknownWhenWithCodeThenLanguageFrench() {
-        assertThat(Language.withCode("totallyUnknownCode")).isEqualTo(Language.French);
-    }
+	@Test
+	public void givenCodeNullWhenWithCodeThenNull() {
+		assertThat(Language.withCode(null)).isNull();
+	}
 
-    @Test
-    public void givenLanguageElementsThenWithCodeOk() {
-        Language[] values = Language.values();
-        for(Language l:values) {
-            assertThat(Language.withCode(l.code)).isEqualTo(l);
-        }
-    }
+	@Test
+	public void givenCodeUnknownWhenWithCodeThenLanguageFrench() {
+		assertThat(Language.withCode("totallyUnknownCode")).isEqualTo(Language.French);
+	}
 
-    @Test
-    public void givenLanguageElementsThenWithCodesOk() {
-        Language[] values = Language.values();
+	@Test
+	public void givenLanguageElementsThenWithCodeOk() {
+		Language[] values = Language.values();
+		for (Language l : values) {
+			assertThat(Language.withCode(l.code)).isEqualTo(l);
+		}
+	}
 
-        List<String> codes = new ArrayList<>();
-        for(Language l:values) {
-            codes.add(l.code);
-        }
+	@Test
+	public void givenLanguageElementsThenWithCodesOk() {
+		Language[] values = Language.values();
 
-        assertThat(Language.withCodes(codes)).contains(values);
-    }
+		List<String> codes = new ArrayList<>();
+		for (Language l : values) {
+			codes.add(l.code);
+		}
 
-    @Test
-    public void givenLanguageElementsThenAvalaible() {
-        assertThat(Language.getAvailableLanguages()).contains(Language.values());
-    }
+		assertThat(Language.withCodes(codes)).contains(values);
+	}
+
+	@Test
+	public void givenLanguageElementsThenAvalaible() {
+		assertThat(Language.getAvailableLanguages()).contains(Language.values());
+	}
 }
