dbe8bd5187fe3cb83dc32b70324a6832c9e7b11f
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2018-10-19 16:13:43 -0400

    fixed migration script

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo8_1_1_1.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo8_1_1_1.java
@@ -33,19 +33,19 @@ public class RMMigrationTo8_1_1_1 extends MigrationHelper implements MigrationSc
 
 		@Override
 		protected void migrate(MetadataSchemaTypesBuilder builder) {
-
 			builder.getDefaultSchema(Document.SCHEMA_TYPE).get(Document.COMPANY).setEssential(false);
 			builder.getDefaultSchema(Document.SCHEMA_TYPE).get(Document.SUBJECT).setEssential(false);
-			builder.getSchema(Email.SCHEMA).get(Email.EMAIL_COMPANY).setEssential(false);
-			builder.getSchema(Email.SCHEMA).get(Email.EMAIL_CC_TO).setEssential(false);
-			builder.getSchema(Email.SCHEMA).get(Email.EMAIL_BCC_TO).setEssential(false);
-			builder.getSchema(Email.SCHEMA).get(Email.EMAIL_IN_NAME_OF).setEssential(false);
-			builder.getSchema(Email.SCHEMA).get(Email.EMAIL_FROM).setEssential(false);
-			builder.getSchema(Email.SCHEMA).get(Email.EMAIL_TO).setEssential(false);
-			builder.getSchema(Email.SCHEMA).get(Email.EMAIL_SENT_ON).setEssential(false);
-			builder.getSchema(Email.SCHEMA).get(Email.EMAIL_RECEIVED_ON).setEssential(false);
-			builder.getSchema(Email.SCHEMA).get(Email.SUBJECT_TO_BROADCAST_RULE).setEssential(false);
+			if(builder.getSchemaType(Document.SCHEMA_TYPE).hasSchema(Email.SCHEMA)) {
+				builder.getSchema(Email.SCHEMA).get(Email.EMAIL_COMPANY).setEssential(false);
+				builder.getSchema(Email.SCHEMA).get(Email.EMAIL_CC_TO).setEssential(false);
+				builder.getSchema(Email.SCHEMA).get(Email.EMAIL_BCC_TO).setEssential(false);
+				builder.getSchema(Email.SCHEMA).get(Email.EMAIL_IN_NAME_OF).setEssential(false);
+				builder.getSchema(Email.SCHEMA).get(Email.EMAIL_FROM).setEssential(false);
+				builder.getSchema(Email.SCHEMA).get(Email.EMAIL_TO).setEssential(false);
+				builder.getSchema(Email.SCHEMA).get(Email.EMAIL_SENT_ON).setEssential(false);
+				builder.getSchema(Email.SCHEMA).get(Email.EMAIL_RECEIVED_ON).setEssential(false);
+				builder.getSchema(Email.SCHEMA).get(Email.SUBJECT_TO_BROADCAST_RULE).setEssential(false);
+			}
 		}
-
 	}
 }
