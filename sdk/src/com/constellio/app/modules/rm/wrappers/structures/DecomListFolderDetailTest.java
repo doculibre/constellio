fbb042f829a2c707a7884c3d8a9eb835854d7ab0
rababMoubine <rabab.moubine@doculibre.com>
2018-10-23 14:45:46 -0400

    testing with old structure

--- sdk/src/com/constellio/app/modules/rm/wrappers/structures/DecomListFolderDetailTest.java
+++ sdk/src/com/constellio/app/modules/rm/wrappers/structures/DecomListFolderDetailTest.java
@@ -1,5 +1,6 @@
 package com.constellio.app.modules.rm.wrappers.structures;
 
+import com.constellio.model.entities.schemas.ModifiableStructure;
 import com.constellio.sdk.tests.ConstellioTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,6 +8,7 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DecomListFolderDetailTest extends ConstellioTest {
+	private static final String NULL = "~null~";
 	DecomListFolderDetailFactory factory;
 	DecomListFolderDetail detail;
 
@@ -66,5 +68,76 @@ public class DecomListFolderDetailTest extends ConstellioTest {
 		DecomListFolderDetail deserialized = (DecomListFolderDetail) factory.build(serialized);
 
 		assertThat(deserialized).isEqualTo(detail);
+	}
+
+	@Test
+	public void whenConvertingStructureTheOldWayWithFolderDetailStatusSetToTrueThenConvertedSuccessfully()
+			throws Exception {
+		detail = new DecomListFolderDetail()
+				.setFolderId("01")
+				.setFolderExcluded(false)
+				.setContainerRecordId("containerRecordId")
+				.setReversedSort(true)
+				.setFolderLinearSize(123.4d);
+
+		String serialized = toStringWithOldStructure(detail, true);
+		DecomListFolderDetail deserialized = (DecomListFolderDetail) factory.build(serialized);
+
+		assertThat(deserialized).isEqualTo(detail);
+	}
+
+	@Test
+	public void whenConvertingStructureTheOldWayWithFolderDetailStatusSetToFalseThenConvertedSuccessfully()
+			throws Exception {
+		detail = new DecomListFolderDetail()
+				.setFolderId("01")
+				.setFolderExcluded(true)
+				.setContainerRecordId("containerRecordId")
+				.setReversedSort(true)
+				.setFolderLinearSize(123.4d);
+
+		String serialized = toStringWithOldStructure(detail, false);
+		DecomListFolderDetail deserialized = (DecomListFolderDetail) factory.build(serialized);
+
+		assertThat(deserialized).isEqualTo(detail);
+	}
+
+	private String toStringWithOldStructure(ModifiableStructure structure, boolean included) {
+		DecomListFolderDetail decomListFolderDetail = (DecomListFolderDetail) structure;
+		StringBuilder stringBuilder = new StringBuilder();
+		writeString(stringBuilder, decomListFolderDetail.getFolderId());
+		writeString(stringBuilder, String.valueOf(included));
+		writeString(stringBuilder, null);
+		writeString(stringBuilder, null);
+		writeString(stringBuilder, null);
+		writeString(stringBuilder, decomListFolderDetail.getContainerRecordId());
+		writeString(stringBuilder, String.valueOf(decomListFolderDetail.isReversedSort()));
+		writeDouble(stringBuilder, decomListFolderDetail.getFolderLinearSize());
+		writeString(stringBuilder, "" + decomListFolderDetail.isPlacedInContainer() == null ?
+								   String.valueOf(false) :
+								   String.valueOf(decomListFolderDetail.isPlacedInContainer()));
+		return stringBuilder.toString();
+	}
+
+	private void writeDouble(StringBuilder stringBuilder, Double value) {
+		if (stringBuilder.length() != 0) {
+			stringBuilder.append(":");
+		}
+		if (value == null) {
+			stringBuilder.append(NULL);
+		} else {
+			stringBuilder.append(String.valueOf(value));
+		}
+	}
+
+	private void writeString(StringBuilder stringBuilder, String value) {
+		if (stringBuilder.length() != 0) {
+			stringBuilder.append(":");
+		}
+		if (value == null) {
+			stringBuilder.append(NULL);
+		} else {
+			stringBuilder.append(value.replace(":", "~~~"));
+		}
 	}
 }
