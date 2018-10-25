d22ab702e0447bf892bd3a386dae889d6b0ead19
rababMoubine <rabab.moubine@doculibre.com>
2018-10-25 10:56:34 -0400

    added test

---
+++ sdk/src/com/constellio/app/modules/rm/migrations/RMMigrationTo8_2_AcceptanceTest.java
@@ -0,0 +1,42 @@
+package com.constellio.app.modules.rm.migrations;
+
+import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
+import com.constellio.model.entities.records.Record;
+import com.constellio.model.services.search.SearchServices;
+import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
+import com.constellio.sdk.tests.ConstellioTest;
+import com.constellio.sdk.tests.SDKFoldersLocator;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static com.constellio.model.entities.schemas.Schemas.IDENTIFIER;
+import static com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators.from;
+import static com.constellio.sdk.tests.TestUtils.assertThatRecords;
+import static org.assertj.core.api.Assertions.tuple;
+
+public class RMMigrationTo8_2_AcceptanceTest extends ConstellioTest {
+
+	@Test
+	public void givenSystemIn8_1thenMigrated()
+			throws Exception {
+		givenSystemIn8_1();
+
+		RMSchemasRecordsServices rm = new RMSchemasRecordsServices(zeCollection, getAppLayerFactory());
+		SearchServices searchServices = getModelLayerFactory().newSearchServices();
+
+		List<Record> carts = searchServices
+				.search(new LogicalSearchQuery(from(rm.cart.schemaType()).returnAll()));
+
+		assertThatRecords(carts).extractingMetadatas(IDENTIFIER).containsOnly(tuple("00000047048", "00000047050", "00000047046"));
+	}
+
+	private void givenSystemIn8_1() {
+		givenTransactionLogIsEnabled();
+		File statesFolder = new SDKFoldersLocator().getInitialStatesFolder();
+		File state = new File(statesFolder, "");
+
+		getCurrentTestSession().getFactoriesTestFeatures().givenSystemInState(state);
+	}
+}
