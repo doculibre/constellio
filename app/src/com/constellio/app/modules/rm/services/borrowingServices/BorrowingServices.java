53dec1cbd1ce921a580064afb921219bde9b4412
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2018-10-29 11:38:31 -0400

    fixed merge

--- app/src/com/constellio/app/modules/rm/services/borrowingServices/BorrowingServices.java
+++ app/src/com/constellio/app/modules/rm/services/borrowingServices/BorrowingServices.java
@@ -365,7 +365,7 @@ public class BorrowingServices {
 	}
 
 	public void validateCanReturnFolder(User currentUser, Folder folder) {
-		boolean hasPermissionToReturnOtherUsersFolder = currentUser.has(RMPermissionsTo.RETURN_OTHER_USERS_FOLDER)
+		boolean hasPermissionToReturnOtherUsersFolder = currentUser.has(RMPermissionsTo.RETURN_OTHER_USERS_FOLDERS)
 				.on(folder);
 		if (currentUser.hasReadAccess().on(folder)) {
 			if (folder.getBorrowed() == null || !folder.getBorrowed()) {
