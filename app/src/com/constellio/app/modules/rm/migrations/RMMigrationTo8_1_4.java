34ff2cae99add48217ad42aa5c6bc8fc46508ff9
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2018-12-03 13:28:55 -0500

    fixed task labels

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo8_1_4.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo8_1_4.java
@@ -1,23 +1,13 @@
-package com.constellio.app.modules.tasks.migrations;
+package com.constellio.app.modules.rm.migrations;
 
 import com.constellio.app.entities.modules.MetadataSchemasAlterationHelper;
 import com.constellio.app.entities.modules.MigrationHelper;
 import com.constellio.app.entities.modules.MigrationResourcesProvider;
 import com.constellio.app.entities.modules.MigrationScript;
-import com.constellio.app.modules.tasks.TasksEmailTemplates;
-import com.constellio.app.modules.tasks.model.wrappers.Task;
 import com.constellio.app.services.factories.AppLayerFactory;
-import com.constellio.data.dao.managers.config.ConfigManagerException.OptimisticLockingConfiguration;
-import com.constellio.model.entities.schemas.MetadataValueType;
-import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
-import org.apache.commons.io.IOUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-public class TasksMigrationTo8_1_4 extends MigrationHelper implements MigrationScript {
-
+public class RMMigrationTo8_1_4 extends MigrationHelper implements MigrationScript {
 	@Override
 	public String getVersion() {
 		return "8.1.4";
@@ -33,7 +23,7 @@ public class TasksMigrationTo8_1_4 extends MigrationHelper implements MigrationS
 	class SchemaAlterationFor8_1_4 extends MetadataSchemasAlterationHelper {
 
 		protected SchemaAlterationFor8_1_4(String collection, MigrationResourcesProvider migrationResourcesProvider,
-											 AppLayerFactory appLayerFactory) {
+										   AppLayerFactory appLayerFactory) {
 			super(collection, migrationResourcesProvider, appLayerFactory);
 		}
 
