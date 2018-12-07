a3fb4714f32deb2cb69ce4c6b17ea3f501c35156
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2018-12-06 20:13:26 -0500

    Rapport progression

--- app/src/com/constellio/app/modules/rm/reports/builders/administration/plan/AdministrativeUnitExcelReportParameters.java
+++ app/src/com/constellio/app/modules/rm/reports/builders/administration/plan/AdministrativeUnitExcelReportParameters.java
@@ -1,7 +1,15 @@
 package com.constellio.app.modules.rm.reports.builders.administration.plan;
 
-public class AdministrativeUnitExcelReportParameters  {
-	public AdministrativeUnitExcelReportParameters() {
+import java.util.List;
 
+public class AdministrativeUnitExcelReportParameters  {
+	private List<String> administrativeUnitIdList = null;
+
+	public AdministrativeUnitExcelReportParameters(List<String> administrativeUnitIdList) {
+		this.administrativeUnitIdList = administrativeUnitIdList;
+	}
+
+	public List<String> getAdministrativeUnitIdList() {
+		return administrativeUnitIdList;
 	}
 }
--- app/src/com/constellio/app/modules/rm/reports/model/administration/plan/AdministrativeUnitExcelReportPresenter.java
+++ app/src/com/constellio/app/modules/rm/reports/model/administration/plan/AdministrativeUnitExcelReportPresenter.java
@@ -16,26 +16,35 @@ import com.constellio.model.entities.records.wrappers.Authorization;
 import com.constellio.model.entities.records.wrappers.User;
 import com.constellio.model.entities.schemas.Metadata;
 import com.constellio.model.entities.schemas.MetadataSchema;
+import com.constellio.model.entities.security.Role;
 import com.constellio.model.services.factories.ModelLayerFactory;
 import com.constellio.model.services.security.AuthorizationsServices;
+import com.constellio.model.services.security.roles.RolesManager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+
+import static com.constellio.app.ui.i18n.i18n.$;
 
 public class AdministrativeUnitExcelReportPresenter extends BaseExcelReportPresenter implements NewReportPresenter {
 	private RMSchemasRecordsServices rmSchemasRecordsServices;
 	private AuthorizationsServices authorizationsServices;
+	private List<String> administrativeUnitToIncludes = null;
 	protected transient ModelLayerFactory modelLayerFactory;
 	protected transient AppLayerCollectionExtensions appCollectionExtentions;
 	protected transient AppLayerSystemExtensions appSystemExtentions;
 
+
 	public AdministrativeUnitExcelReportPresenter(String collection, AppLayerFactory appLayerFactory,
-												  Locale locale) {
+												  Locale locale, List<String> administrativeUnitToIncludes) {
 		super(appLayerFactory, locale, collection);
 		this.modelLayerFactory = appLayerFactory.getModelLayerFactory();
 		this.appCollectionExtentions = appLayerFactory.getExtensions().forCollection(collection);
 		this.appSystemExtentions = appLayerFactory.getExtensions().getSystemWideExtensions();
+		this.administrativeUnitToIncludes = administrativeUnitToIncludes;
 	}
 
 	private void init() {
@@ -65,56 +74,132 @@ public class AdministrativeUnitExcelReportPresenter extends BaseExcelReportPrese
 		model.addTitle(i18n.$("Reports.administrativeUnitExcelUserWithPermission"));
 		model.addTitle(i18n.$("Reports.administrativeUnitExcelGroupWithPermission"));
 
-		List<AdministrativeUnit> administrativeUnitList = rmSchemasRecordsServices.getAllAdministrativeUnits();
 
+		if(administrativeUnitToIncludes != null || !administrativeUnitToIncludes.isEmpty()) {
+			for (String administrativeUnitId : administrativeUnitToIncludes) {
+				AdministrativeUnit administrativeUnit1 = rmSchemasRecordsServices.getAdministrativeUnit(administrativeUnitId);
+				List<Object> recordLine = getRecordLine(administrativeUnit1.getWrappedRecord(), metadataListed);
+				getExtraLineData(recordLine, administrativeUnit1);
+				model.addLine(recordLine);
+			}
 
-		for (AdministrativeUnit administrativeUnit : administrativeUnitList) {
-			List<Object> recordLine = getRecordLine(administrativeUnit.getWrappedRecord(), metadataListed);
-			getExtraLineData(recordLine, administrativeUnit);
-			model.addLine(recordLine);
+		} else {
+
 		}
 
-
-		// TODO JOJO
-
 		return model;
+	}
+
+	private List<String> getAccess(Authorization authorization) {
+		List<String> access = new ArrayList<>();
+		for (String roleCode : authorization.getRoles()) {
+			RolesManager rolesManager = modelLayerFactory.getRolesManager();
+			Role role = rolesManager.getRole(authorization.getCollection(), roleCode);
+			if (role.isContentPermissionRole()) {
+				access.add(roleCode);
+			}
+		}
+
+		return access;
+	}
+
+	private String stringListToString(List<String> stringList, String separator) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (String item : stringList) {
+
+			if (stringBuilder.length() != 0) {
+				stringBuilder.append(separator + " ");
+			}
+
+			stringBuilder.append(item);
+		}
+
+		return stringBuilder.toString();
+	}
+
+	private String accessAbreviation(List<String> roles) {
+		List<String> shortened = new ArrayList<>(3);
+
+
+		if (roles.contains(Role.READ)) {
+			shortened.add($("AuthorizationsView.short.READ"));
+		}
+		if (roles.contains(Role.WRITE)) {
+			shortened.add($("AuthorizationsView.short.WRITE"));
+		}
+		if (roles.contains(Role.DELETE)) {
+			shortened.add($("AuthorizationsView.short.DELETE"));
+		}
+
+		if(shortened == null || roles.isEmpty()) {
+			return "";
+		}
+
+		return stringListToString(shortened, "/");
+	}
+
+	private void updateAcces(String user, Map<String, List<String>> accessList, List<String> newAcessList) {
+		List<String> currentAcces = accessList.get(user);
+		if(currentAcces == null) {
+			accessList.put(user, newAcessList);
+		} else {
+			for(String currentNewAccess : newAcessList) {
+				if(!currentAcces.contains(currentNewAccess)) {
+					currentAcces.add(currentNewAccess);
+				}
+			}
+		}
 	}
 
 	private void getExtraLineData(List<Object> recordLine, AdministrativeUnit administrativeUnit) {
 		List<Authorization> authorizationList = authorizationsServices.getRecordAuthorizations(administrativeUnit.getWrappedRecord());
 		List<String> groupList = new ArrayList<>();
 		List<String> userList = new ArrayList<>();
-		StringBuilder userStringBuilder = new StringBuilder();
-		StringBuilder groupStringBuilder = new StringBuilder();
+		Map<String, List<String>> groupHashMap = new HashMap<>();
+		Map<String, List<String>> userAccessHashMap = new HashMap<>();
 
 		for (Authorization authorization : authorizationList) {
+			List<String> accessList = getAccess(authorization);
 			for (String principal : authorization.getPrincipals()) {
 				Record record = rmSchemasRecordsServices.get(principal);
 				if (record.getSchemaCode().split("_")[0].equals(User.SCHEMA_TYPE)) {
+
+					updateAcces(record.getTitle(), userAccessHashMap, accessList);
+
 					if (!userList.contains(record.getTitle())) {
 						userList.add(record.getTitle());
-						if (userList.size() > 1) {
-							userStringBuilder.append(", ");
-						}
-
-						userStringBuilder.append(record.getTitle());
 					}
 				} else {
+					updateAcces(record.getTitle(), groupHashMap, accessList);
 					if (!groupList.contains(record.getTitle())) {
 						groupList.add(record.getTitle());
-
-						if (groupList.size() > 1) {
-							groupStringBuilder.append(", ");
-						}
-
-						groupStringBuilder.append(record.getTitle());
 					}
 				}
 			}
 		}
 
-		recordLine.add(userStringBuilder.toString());
-		recordLine.add(groupStringBuilder.toString());
+		recordLine.add(getUserCellContent(userList, userAccessHashMap));
+		recordLine.add(getUserCellContent(groupList, groupHashMap));
+	}
+
+	private String getUserCellContent(List<String> itemList, Map<String, List<String>> userAccessHashMap) {
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for(String item : itemList) {
+			if(stringBuilder.length() != 0) {
+				stringBuilder.append(", ");
+			}
+
+			String access = accessAbreviation(userAccessHashMap.get(item));
+
+			stringBuilder.append(item);
+
+			if(access != null && access.length() >= 0) {
+				stringBuilder.append(" (" + access + ")");
+			}
+		}
+
+		return stringBuilder.toString();
 	}
 
 	@Override
