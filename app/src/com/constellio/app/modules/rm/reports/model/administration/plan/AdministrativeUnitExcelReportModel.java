668cd5302638803212bef8afa7c86cf71b3ebeb4
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2018-12-09 13:00:12 -0500

    Addition of the possiblity of updating private policy and having user agree to the new version.

--- app/src/com/constellio/app/modules/rm/reports/model/administration/plan/AdministrativeUnitExcelReportModel.java
+++ app/src/com/constellio/app/modules/rm/reports/model/administration/plan/AdministrativeUnitExcelReportModel.java
@@ -1,8 +1,11 @@
 package com.constellio.app.modules.rm.reports.model.administration.plan;
 
+import com.constellio.app.ui.i18n.i18n;
 import org.apache.commons.collections.CollectionUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,11 +16,19 @@ public class AdministrativeUnitExcelReportModel {
 	public static final String SINGLE_SHEET_CAPTION_KEY = "Report.sheetName";
 
 	public List<List<Object>> getResults(String sheet) {
-		return new ArrayList<>(CollectionUtils.unmodifiableCollection(resultBysheet.get(sheet)));
+		if(resultBysheet.keySet().size() > 0) {
+			return new ArrayList<>(CollectionUtils.unmodifiableCollection(resultBysheet.get(sheet)));
+		} else {
+			return Collections.emptyList();
+		}
 	}
 
 	public List<String> getSheetNames() {
-		return new ArrayList<>(resultBysheet.keySet());
+		if(resultBysheet.keySet().size() > 0) {
+			return new ArrayList<>(resultBysheet.keySet());
+		} else {
+			return Arrays.asList(i18n.$(SINGLE_SHEET_CAPTION_KEY));
+		}
 	}
 
 	public List<String> getColumnsTitles() {
