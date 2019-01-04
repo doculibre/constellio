27cb1c02b906a52c1d8e43a8ebc7466acc1bdf35
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2019-01-04 09:48:08 -0500

    added link to guides in views

--- app/src/com/constellio/app/ui/framework/components/BaseMouseOverIcon.java
+++ app/src/com/constellio/app/ui/framework/components/BaseMouseOverIcon.java
@@ -1,5 +1,6 @@
 package com.constellio.app.ui.framework.components;
 
+import com.constellio.app.ui.framework.buttons.BaseButton;
 import com.constellio.app.ui.framework.components.mouseover.NiceTitle;
 import com.vaadin.data.Property;
 import com.vaadin.server.Extension;
@@ -11,16 +12,18 @@ import com.vaadin.ui.themes.ValoTheme;
 
 import static com.constellio.app.ui.i18n.i18n.$;
 
-public class BaseMouseOverIcon extends Button {
-
-	private BaseMouseOverIcon() {
-	}
+public class BaseMouseOverIcon extends BaseButton {
 
 	public BaseMouseOverIcon(Resource icon, String message) {
-		super();
+		super("", icon, true);
 		super.setIcon(icon);
-		setEnabled(false);
+//		setEnabled(false);
 		addStyleName(ValoTheme.BUTTON_BORDERLESS);
 		addExtension(new NiceTitle(this, message, true));
+	}
+
+	@Override
+	protected void buttonClick(ClickEvent event) {
+
 	}
 }
