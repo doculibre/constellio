0350d4c20649342cde77831526a48344ae2b5064
gabrielLefrancoisConstellio <gabriel.lefrancois@constellio.com>
2019-01-04 15:17:43 -0500

    fixed elevation arabic component

---
+++ app/src/com/constellio/app/ui/framework/components/layouts/I18NCssLayout.java
@@ -0,0 +1,75 @@
+package com.constellio.app.ui.framework.components.layouts;
+
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
+
+import static com.constellio.app.ui.i18n.i18n.isRightToLeft;
+import static com.vaadin.ui.AbstractOrderedLayout.ALIGNMENT_DEFAULT;
+
+public class I18NCssLayout extends CssLayout {
+
+	public static final String STYLE_NAME_PREFIX = "i18n-horizontal-layout-";
+
+	public static final String STYLE_NAME_LEFT_TO_RIGHT = STYLE_NAME_PREFIX + "left-to-right";
+
+	public static final String STYLE_NAME_RIGHT_TO_LEFT = STYLE_NAME_PREFIX + "right-to-left";
+
+	public I18NCssLayout() {
+		init();
+	}
+
+	public I18NCssLayout(Component... children) {
+		super();
+		init();
+		addComponents(children);
+	}
+
+	private void init() {
+		if (isRightToLeft()) {
+			addStyleName(STYLE_NAME_RIGHT_TO_LEFT);
+		} else {
+			addStyleName(STYLE_NAME_LEFT_TO_RIGHT);
+		}
+	}
+
+	private int adjustIndex(int index) {
+		//		if (true) return index;
+		int result;
+		if (isRightToLeft()) {
+			int componentCount = getComponentCount();
+			if (componentCount > 0) {
+				result = componentCount - index;
+			} else {
+				result = index;
+			}
+		} else {
+			result = index;
+		}
+		return result;
+	}
+
+	@Override
+	public void addComponent(Component c) {
+		//		super.addComponent(c);
+		int index = getComponentCount();
+		addComponent(c, index);
+	}
+
+	@Override
+	public void addComponentAsFirst(Component c) {
+		//		super.addComponentAsFirst(c);
+		if (equals(c.getParent())) {
+			removeComponent(c);
+		}
+		addComponent(c, 0);
+	}
+
+	@Override
+	public void addComponent(Component c, int index) {
+		index = adjustIndex(index);
+		super.addComponent(c, index);
+	}
+
+}
