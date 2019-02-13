a2d4812f0c281047d558e57f47f6ea5f4e85507d
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2019-02-13 09:59:25 -0500

    Retouche rapport

--- app/src/com/constellio/app/modules/rm/reports/model/administration/plan/UserReportPresenter.java
+++ app/src/com/constellio/app/modules/rm/reports/model/administration/plan/UserReportPresenter.java
@@ -1,26 +1,38 @@
 package com.constellio.app.modules.rm.reports.model.administration.plan;
 
 import com.constellio.app.modules.rm.reports.model.administration.plan.UserReportModel.UserReportModel_AdministrativeUnit;
+import com.constellio.app.modules.rm.reports.model.administration.plan.UserReportModel.UserReportModel_Group;
+import com.constellio.app.modules.rm.reports.model.administration.plan.UserReportModel.UserReportModel_Role;
+import com.constellio.app.modules.rm.reports.model.administration.plan.UserReportModel.UserReportModel_RoleTarget;
 import com.constellio.app.modules.rm.reports.model.administration.plan.UserReportModel.UserReportModel_User;
 import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
 import com.constellio.app.modules.rm.wrappers.AdministrativeUnit;
+import com.constellio.app.ui.util.DateFormatUtils;
 import com.constellio.model.conf.FoldersLocator;
+import com.constellio.model.entities.Language;
 import com.constellio.model.entities.records.Record;
+import com.constellio.model.entities.records.wrappers.Authorization;
 import com.constellio.model.entities.records.wrappers.User;
 import com.constellio.model.entities.schemas.Schemas;
 import com.constellio.model.entities.security.Role;
 import com.constellio.model.services.factories.ModelLayerFactory;
 import com.constellio.model.services.records.RecordServices;
+import com.constellio.model.services.schemas.MetadataSchemasManager;
 import com.constellio.model.services.search.SearchServices;
 import com.constellio.model.services.search.StatusFilter;
 import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
 import com.constellio.model.services.search.query.logical.LogicalSearchQueryOperators;
 import com.constellio.model.services.security.AuthorizationsServices;
+import com.constellio.model.services.security.roles.RolesManager;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+
+import static com.constellio.app.ui.i18n.i18n.$;
 
 public class UserReportPresenter {
 	private String collection;
@@ -31,6 +43,7 @@ public class UserReportPresenter {
 	private AuthorizationsServices authorizationsServices;
 	private List<AdministrativeUnit> administrativeUnits;
 	private Locale locale;
+	private MetadataSchemasManager metadataSchemasManager;
 
 	public UserReportPresenter(String collection, ModelLayerFactory modelLayerFactory, Locale locale) {
 
@@ -57,6 +70,7 @@ public class UserReportPresenter {
 		recordServices = modelLayerFactory.newRecordServices();
 		authorizationsServices = modelLayerFactory.newAuthorizationsServices();
 		administrativeUnits = getAdministrativeUnits();
+		metadataSchemasManager = modelLayerFactory.getMetadataSchemasManager();
 	}
 
 	private List<UserReportModel_User> getModelUsers() {
@@ -77,6 +91,13 @@ public class UserReportPresenter {
 					String userName = StringUtils.defaultString(user.getUsername());
 					modelUser.setUserName(userName);
 
+					modelUser.setCreationDate(DateFormatUtils.format(user.getCreatedOn()));
+					modelUser.setModificationDate(DateFormatUtils.format(user.getModifiedOn()));
+
+					modelUser.setRoles(getSpecificRolesAndTarget(user));
+
+					modelUser.setGroups(getGroupModel(user));
+
 					String userId = StringUtils.defaultString(user.getId());
 					userId = StringUtils.stripStart(userId, "0");
 					modelUser.setUserId(userId);
@@ -91,6 +112,177 @@ public class UserReportPresenter {
 			}
 		}
 		return modelUsers;
+	}
+
+	private boolean isAccessRole(String role) {
+		return role.equals(Role.READ) || role.equals(Role.WRITE) || role.equals(Role.DELETE);
+	}
+
+	protected boolean isRoleAuthorization(Authorization auth) {
+		for (String role : auth.getRoles()) {
+			if (!isAccessRole(role)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	private boolean isOwnAuthorization(Authorization authorization, String userId) {
+		return authorization.getPrincipals().contains(userId);
+	}
+
+	public List<UserReportModel_Group> getGroupModel(User user) {
+		List<UserReportModel_Group> userReportModelGroups = new ArrayList<>();
+		List<String> groups = user.getUserGroups();
+
+		Map<String, List<Authorization>> mapAuthorizationByGroup = new HashMap<>();
+
+		for(String currentGroup : user.getUserGroups()) {
+			mapAuthorizationByGroup.put(currentGroup, new ArrayList<Authorization>());
+		}
+
+		for(Authorization authorization : rmSchemasRecordsServices.getAllAuthorizationsInUnmodifiableState()) {
+			List<String> groupAffected = doesListHaveOneItemInCommon(groups, authorization.getPrincipals());
+			if(groupAffected != null) {
+				for(String currentGroup : groupAffected) {
+					addItemToMapList(mapAuthorizationByGroup, currentGroup, authorization);
+				}
+			}
+		}
+
+		for(String group : mapAuthorizationByGroup.keySet()) {
+			List<Authorization> authorizationByGroupList = mapAuthorizationByGroup.get(group);
+			UserReportModel_Group userReportModel_group = new UserReportModel_Group(group, recordServices.getDocumentById(group).getTitle());
+
+			for(Authorization authorization : authorizationByGroupList) {
+				Record record = recordServices.getDocumentById(authorization.getTarget());
+				String label = metadataSchemasManager.getSchemaTypeOf(record).getLabel(Language.withLocale(locale));
+				userReportModel_group.addTarget(label + " : " + record.getTitle() + " (" + accessAbreviation(getAccess(authorization)) + ")");
+			}
+
+			userReportModelGroups.add(userReportModel_group);
+		}
+
+		return userReportModelGroups;
+	}
+
+	private List<String> getAccess(Authorization authorization) {
+		List<String> access = new ArrayList<>();
+		for (String roleCode : authorization.getRoles()) {
+			RolesManager rolesManager = modelLayerFactory.getRolesManager();
+			Role role = rolesManager.getRole(authorization.getCollection(), roleCode);
+			if (role.isContentPermissionRole()) {
+				access.add(roleCode);
+			}
+		}
+
+		return access;
+	}
+
+	private String stringListToString(List<String> stringList, String separator) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (String item : stringList) {
+
+			if (stringBuilder.length() != 0) {
+				stringBuilder.append(separator + " ");
+			}
+
+			stringBuilder.append(item);
+		}
+
+		return stringBuilder.toString();
+	}
+
+	private void addItemToMapList(Map map, String key, Object item) {
+		List list = (List) map.get(key);
+
+		if(list == null) {
+			list = new ArrayList<>();
+			map.put(key,list);
+		}
+
+		list.add(item);
+	}
+
+	private String accessAbreviation(List<String> roles) {
+		List<String> shortened = new ArrayList<>(3);
+
+
+		if (roles.contains(Role.READ)) {
+			shortened.add($("AuthorizationsView.short.READ"));
+		}
+		if (roles.contains(Role.WRITE)) {
+			shortened.add($("AuthorizationsView.short.WRITE"));
+		}
+		if (roles.contains(Role.DELETE)) {
+			shortened.add($("AuthorizationsView.short.DELETE"));
+		}
+
+		if(shortened == null || roles.isEmpty()) {
+			return "";
+		}
+
+		return stringListToString(shortened, "/");
+	}
+
+	private List<String> doesListHaveOneItemInCommon(List<String> itemList1, List<String> itemList2) {
+		List<String> commonItem = new ArrayList<>();
+
+		for(String currentItem1 : itemList1) {
+			for(String currentItem2 :itemList2) {
+				if(currentItem1.equals(currentItem2)){
+					commonItem.add(currentItem1);
+				}
+			}
+		}
+
+		if(commonItem.size() > 0) {
+			return commonItem;
+		} else {
+			return null;
+		}
+	}
+
+	public Map<UserReportModel_Role, List<UserReportModel_RoleTarget>> getSpecificRolesAndTarget(User user) {
+		Map<UserReportModel_Role, List<UserReportModel_RoleTarget>> specificRoles = new HashMap<>();
+
+		for (String globalRole : user.getUserRoles()) {
+			addToListOfMapUserReportModelRoleTarget(specificRoles, globalRole, null);
+		}
+
+		for (Authorization roleAuth : modelLayerFactory.newAuthorizationsServices().getRecordAuthorizations(user)) {
+			if (isRoleAuthorization(roleAuth) && isOwnAuthorization(roleAuth, user.getId())) {
+				for(String role : roleAuth.getRoles()) {
+					Record record = rmSchemasRecordsServices.get(roleAuth.getTarget());
+					UserReportModel_RoleTarget reportModel_administrativeUnit = new UserReportModel_RoleTarget(roleAuth.getTarget(), record.getTitle(), roleAuth.getTargetSchemaType());
+					addToListOfMapUserReportModelRoleTarget(specificRoles, role, reportModel_administrativeUnit);
+				}
+
+			}
+		}
+		return specificRoles;
+	}
+
+	private RolesManager roleManager() {
+		return modelLayerFactory.getRolesManager();
+	}
+
+	public String getRoleTitle(String roleCode) {
+		return roleManager().getRole(collection, roleCode).getTitle();
+	}
+
+	private void addToListOfMapUserReportModelRoleTarget(Map<UserReportModel_Role, List<UserReportModel_RoleTarget>> specificRoles, String key, UserReportModel_RoleTarget userReportModel_roleTarget) {
+		UserReportModel_Role userReportModel_role = new UserReportModel_Role(key);
+		List<UserReportModel_RoleTarget> userReportModel_roleTargetList = specificRoles.get(userReportModel_role);
+
+		if(userReportModel_roleTargetList == null) {
+			userReportModel_roleTargetList = new ArrayList<>();
+			userReportModel_role.setLabel(getRoleTitle(key));
+			specificRoles.put(userReportModel_role, userReportModel_roleTargetList);
+		}
+
+		userReportModel_roleTargetList.add(userReportModel_roleTarget);
 	}
 
 	private List<User> getUsers() {
--- app/src/com/constellio/app/ui/pages/events/EventPresenter.java
+++ app/src/com/constellio/app/ui/pages/events/EventPresenter.java
@@ -30,6 +30,7 @@ import com.constellio.model.entities.records.wrappers.EventType;
 import com.constellio.model.entities.records.wrappers.User;
 import com.constellio.model.entities.schemas.Metadata;
 import com.constellio.model.entities.schemas.MetadataSchema;
+import com.constellio.model.entities.schemas.MetadataValueType;
 import com.constellio.model.entities.schemas.Schemas;
 import com.constellio.model.services.records.RecordServicesRuntimeException;
 import com.constellio.model.services.schemas.builders.CommonMetadataBuilder;
@@ -178,7 +179,7 @@ public class EventPresenter extends SingleSchemaBasePresenter<EventView> {
 			temporaryFile = ioServices.newTemporaryFile(TEMPORARY_FILE);
 			byteArrayOutputStream = ioServices.newFileOutputStream(temporaryFile, STREAM_NAME);
 			outputStreamWriter = new OutputStreamWriter(byteArrayOutputStream, CharEncoding.ISO_8859_1);
-			csvWriter = new CSVWriter(outputStreamWriter, ',', CSVWriter.NO_QUOTE_CHARACTER);
+			csvWriter = new CSVWriter(outputStreamWriter, ',', CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.NO_ESCAPE_CHARACTER);
 			writeCsvReport(csvWriter);
 			csvWriter.flush();
 			ioServices.closeQuietly(csvWriter);
@@ -217,6 +218,9 @@ public class EventPresenter extends SingleSchemaBasePresenter<EventView> {
 
 				if (metadataValue != null) {
 					valueAsString = metadataValue.toString();
+				}
+				if(metadataVO.getType() == MetadataValueType.STRING) {
+					valueAsString = "\"" + valueAsString + "\"";
 				}
 
 				stringArray[counter++] = valueAsString;
