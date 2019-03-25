ed8cd3b3f24fcf92db8fcf55daae609a1c12a2c0
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2019-03-25 15:14:48 -0400

    Complete task changes

--- app/src/com/constellio/app/modules/tasks/ui/pages/tasks/DisplayTaskPresenter.java
+++ app/src/com/constellio/app/modules/tasks/ui/pages/tasks/DisplayTaskPresenter.java
@@ -1,5 +1,15 @@
 package com.constellio.app.modules.tasks.ui.pages.tasks;
 
+import static com.constellio.app.modules.tasks.model.wrappers.Task.ASSIGNEE;
+import static com.constellio.app.modules.tasks.model.wrappers.Task.DUE_DATE;
+import static com.constellio.app.ui.entities.RecordVO.VIEW_MODE.FORM;
+import static com.constellio.app.ui.i18n.i18n.$;
+import static com.constellio.model.entities.records.wrappers.RecordWrapper.TITLE;
+import static java.util.Arrays.asList;
+
+import java.io.IOException;
+import java.util.List;
+
 import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
 import com.constellio.app.modules.rm.services.events.RMEventsSearchServices;
 import com.constellio.app.modules.tasks.model.wrappers.Task;
@@ -35,16 +45,6 @@ import com.constellio.model.services.records.RecordServicesException;
 import com.constellio.model.services.records.RecordServicesRuntimeException;
 import com.constellio.model.services.records.RecordUtils;
 import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
-
-import java.io.IOException;
-import java.util.List;
-
-import static com.constellio.app.modules.tasks.model.wrappers.Task.ASSIGNEE;
-import static com.constellio.app.modules.tasks.model.wrappers.Task.DUE_DATE;
-import static com.constellio.app.ui.entities.RecordVO.VIEW_MODE.FORM;
-import static com.constellio.app.ui.i18n.i18n.$;
-import static com.constellio.model.entities.records.wrappers.RecordWrapper.TITLE;
-import static java.util.Arrays.asList;
 
 public class DisplayTaskPresenter extends SingleSchemaBasePresenter<DisplayTaskView> implements TaskPresenter {
 	private static final String DISPLAY_TASK_PRESENTER_PREVIOUS_TAB = "DisplayTaskPresenterPreviousTab";
@@ -102,7 +102,7 @@ public class DisplayTaskPresenter extends SingleSchemaBasePresenter<DisplayTaskV
 		ConstellioUI.getCurrentSessionContext().setAttribute(DISPLAY_TASK_PRESENTER_PREVIOUS_TAB, view.getSelectedTab().getId());
 	}
 
-	public RecordVO getTask() {
+	public RecordVO getTaskVO() {
 		return taskVO;
 	}
 
@@ -274,6 +274,10 @@ public class DisplayTaskPresenter extends SingleSchemaBasePresenter<DisplayTaskV
 		return currentRecord;
 	}
 
+	public Task getTask() {
+		return tasksSchemas.wrapTask(getCurrentRecord());
+	}
+
 	public boolean isCompleteCurrentTaskButtonVisible() {
 		return isCompleteTaskButtonVisible(taskVO);
 	}
@@ -433,7 +437,7 @@ public class DisplayTaskPresenter extends SingleSchemaBasePresenter<DisplayTaskV
 	}
 
 	public boolean isLogicallyDeleted() {
-		RecordVO task = getTask();
+		RecordVO task = getTaskVO();
 		return Boolean.TRUE
 				.equals(task.getMetadataValue(task.getMetadata(Schemas.LOGICALLY_DELETED_STATUS.getLocalCode())).getValue());
 	}
