973b8c98e66ac0f05ee12729d8c27c49afa41842
francisbaril <baril.francis@gmail.com>
2019-04-10 11:14:41 -0400

    merges

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo9_0.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo9_0.java
@@ -1,13 +1,21 @@
 package com.constellio.app.modules.rm.migrations;
 
+import com.constellio.app.entities.modules.MetadataSchemasAlterationHelper;
 import com.constellio.app.entities.modules.MigrationResourcesProvider;
 import com.constellio.app.entities.modules.MigrationScript;
 import com.constellio.app.modules.rm.constants.RMPermissionsTo;
 import com.constellio.app.modules.rm.constants.RMRoles;
+import com.constellio.app.modules.rm.model.calculators.folder.FolderActualDepositDateCalculator;
+import com.constellio.app.modules.rm.model.calculators.folder.FolderActualDestructionDateCalculator;
+import com.constellio.app.modules.rm.model.calculators.folder.FolderActualTransferDateCalculator;
+import com.constellio.app.modules.rm.model.calculators.folder.FolderOpeningDateCalculator;
+import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.app.services.migrations.CoreRoles;
 import com.constellio.model.entities.security.Role;
 import com.constellio.model.services.factories.ModelLayerFactory;
+import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
 import com.constellio.model.services.security.roles.RolesManager;
 
 import java.util.ArrayList;
@@ -17,19 +25,6 @@ import static java.util.Arrays.asList;
 
 public class RMMigrationTo9_0 implements MigrationScript {
 	public static final String USE_CART_OLD_PERMISSION = "rm.useCart";
-import com.constellio.app.entities.modules.MetadataSchemasAlterationHelper;
-import com.constellio.app.entities.modules.MigrationResourcesProvider;
-import com.constellio.app.entities.modules.MigrationScript;
-import com.constellio.app.modules.rm.model.calculators.folder.FolderActualDepositDateCalculator;
-import com.constellio.app.modules.rm.model.calculators.folder.FolderActualDestructionDateCalculator;
-import com.constellio.app.modules.rm.model.calculators.folder.FolderActualTransferDateCalculator;
-import com.constellio.app.modules.rm.model.calculators.folder.FolderOpeningDateCalculator;
-import com.constellio.app.modules.rm.wrappers.Folder;
-import com.constellio.app.services.factories.AppLayerFactory;
-import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
-import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
-
-public class RMMigrationTo9_0 implements MigrationScript {
 
 	@Override
 	public String getVersion() {
@@ -41,7 +36,7 @@ public class RMMigrationTo9_0 implements MigrationScript {
 						AppLayerFactory appLayerFactory)
 			throws Exception {
 
-		new SchemaAlterationFor9_0(collection, provider, appLayerFactory).migrate();
+		new SchemaAlterationFor9_0(collection, migrationResourcesProvider, appLayerFactory).migrate();
 
 		ModelLayerFactory modelLayerFactory = appLayerFactory.getModelLayerFactory();
 
