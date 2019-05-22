02a25d38d532f3b4c4a64493b24a42296b1c6ffe
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2019-05-22 19:19:39 -0400

    Change for sub dto in FolderDto

--- app/src/com/constellio/app/modules/restapi/resource/dto/BaseReferenceDto.java
+++ app/src/com/constellio/app/modules/restapi/resource/dto/BaseReferenceDto.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.jetbrains.annotations.NotNull;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
