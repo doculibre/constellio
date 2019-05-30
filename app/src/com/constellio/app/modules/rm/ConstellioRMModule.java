eeb00df1f1ccf1c2502172e68db575bd1e833063
rabab <rabab.moubine@doculibre.com>
2019-05-30 08:46:03 -0400

    added metadata contents to decomissioning list

--- app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
+++ app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
@@ -148,6 +148,7 @@ import com.constellio.app.modules.rm.migrations.RMMigrationTo8_1_4;
 import com.constellio.app.modules.rm.migrations.RMMigrationTo8_2;
 import com.constellio.app.modules.rm.migrations.RMMigrationTo8_2_1_4;
 import com.constellio.app.modules.rm.migrations.RMMigrationTo9_0;
+import com.constellio.app.modules.rm.migrations.RMMigrationTo9_0_1;
 import com.constellio.app.modules.rm.migrations.records.RMContainerRecordMigrationTo7_3;
 import com.constellio.app.modules.rm.migrations.records.RMDocumentMigrationTo7_6_10;
 import com.constellio.app.modules.rm.migrations.records.RMEmailMigrationTo7_7_1;
@@ -319,6 +320,7 @@ public class ConstellioRMModule implements InstallableSystemModule, ModuleWithCo
 		scripts.add(new RMMigrationTo8_1_4());
 		scripts.add(new RMMigrationTo8_2());
 		scripts.add(new RMMigrationTo9_0());
+		scripts.add(new RMMigrationTo9_0_1());
 		scripts.add(new RMMigrationTo8_2_1_4());
 
 		return scripts;
