492c18d285029d399cc6cac61cb984b0973c42bb
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2019-07-03 13:51:05 -0400

    Forgot to commit these file.

---
+++ app/src/com/constellio/app/services/action/SchemaRecordActionsServices.java
@@ -0,0 +1,38 @@
+package com.constellio.app.services.action;
+
+import com.constellio.app.extensions.AppLayerCollectionExtensions;
+import com.constellio.app.modules.rm.ConstellioRMModule;
+import com.constellio.app.modules.rm.extensions.api.RMModuleExtensions;
+import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
+import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.model.entities.records.Record;
+import com.constellio.model.entities.records.wrappers.User;
+import com.constellio.model.extensions.ModelLayerCollectionExtensions;
+import com.constellio.model.services.records.RecordServices;
+
+public class SchemaRecordActionsServices {
+	private RMSchemasRecordsServices rm;
+	private RMModuleExtensions rmModuleExtensions;
+	private transient ModelLayerCollectionExtensions modelLayerCollectionextensions;
+	private transient AppLayerCollectionExtensions appLayerCollectionExtensions;
+	private RecordServices recordServices;
+
+	public SchemaRecordActionsServices(String collection, AppLayerFactory appLayerFactory) {
+		this.rm = new RMSchemasRecordsServices(collection, appLayerFactory);
+		this.rmModuleExtensions = appLayerFactory.getExtensions().forCollection(collection).forModule(ConstellioRMModule.ID);
+		this.modelLayerCollectionextensions = appLayerFactory.getModelLayerFactory().getExtensions().forCollection(collection);
+		this.appLayerCollectionExtensions = appLayerFactory.getExtensions().forCollection(collection);
+		;
+		this.recordServices = appLayerFactory.getModelLayerFactory().newRecordServices();
+	}
+
+	public boolean isEditActionPossible(Record record, User user) {
+		return appLayerCollectionExtensions.isEditActionPossibleOnSchemaRecord(record, user) && !modelLayerCollectionextensions.isModifyBlocked(record, user)
+			   && modelLayerCollectionextensions.isRecordModifiableBy(record, user);
+	}
+
+	public boolean isDeleteActionPossible(Record record, User user) {
+		return appLayerCollectionExtensions.isDeleteActionPossibleOnSchemaRecord(record, user)
+			   && modelLayerCollectionextensions.validateDeleteAuthorized(record, user).isEmpty() && recordServices.validateLogicallyDeletable(record, user).isEmpty();
+	}
+}
