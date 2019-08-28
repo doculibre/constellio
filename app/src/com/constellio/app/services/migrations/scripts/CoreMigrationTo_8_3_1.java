b89477a95e9be966dddc27fe199914cce206818d
Michael <michael.turcotte@doculibre.com>
2019-08-28 16:19:13 -0400

    Fix core migration 8.3.1 for saved search with test

--- app/src/com/constellio/app/services/migrations/scripts/CoreMigrationTo_8_3_1.java
+++ app/src/com/constellio/app/services/migrations/scripts/CoreMigrationTo_8_3_1.java
@@ -4,6 +4,10 @@ import com.constellio.app.entities.modules.MetadataSchemasAlterationHelper;
 import com.constellio.app.entities.modules.MigrationResourcesProvider;
 import com.constellio.app.entities.modules.MigrationScript;
 import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.data.dao.dto.records.RecordsFlushing;
+import com.constellio.data.dao.services.bigVault.solr.BigVaultServer;
+import com.constellio.data.dao.services.bigVault.solr.BigVaultServerTransaction;
+import com.constellio.data.dao.services.bigVault.solr.SolrUtils;
 import com.constellio.model.entities.calculators.SavedSearchRestrictedCalculator2;
 import com.constellio.model.entities.records.ConditionnedActionExecutorInBatchBuilder;
 import com.constellio.model.entities.records.ConditionnedActionExecutorInBatchBuilder.RecordScript;
@@ -16,12 +20,14 @@ import com.constellio.model.entities.records.wrappers.User;
 import com.constellio.model.entities.schemas.Metadata;
 import com.constellio.model.entities.schemas.MetadataSchemaTypes;
 import com.constellio.model.entities.schemas.MetadataValueType;
+import com.constellio.model.services.factories.ModelLayerFactory;
 import com.constellio.model.services.schemas.MetadataSchemasManager;
 import com.constellio.model.services.schemas.builders.MetadataSchemaBuilder;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
-import com.constellio.model.services.search.SearchServices;
 import com.constellio.model.services.search.query.logical.LogicalSearchQuery;
+import org.apache.solr.common.SolrInputDocument;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,31 +44,57 @@ public class CoreMigrationTo_8_3_1 implements MigrationScript {
 		return "8.3.1";
 	}
 
-
 	@Override
 	public void migrate(final String collection, MigrationResourcesProvider provider,
 						AppLayerFactory appLayerFactory)
 			throws Exception {
 
 		if (!Collection.SYSTEM_COLLECTION.equals(collection)) {
-			final MetadataSchemasManager schemasManager = appLayerFactory.getModelLayerFactory().getMetadataSchemasManager();
+			ModelLayerFactory modelLayerFactory = appLayerFactory.getModelLayerFactory();
+			final MetadataSchemasManager schemasManager = modelLayerFactory.getMetadataSchemasManager();
 			MetadataSchemaTypes schemaTypes = schemasManager.getSchemaTypes(collection);
-			SearchServices searchServices = appLayerFactory.getModelLayerFactory().newSearchServices();
+
 			LogicalSearchQuery allSavedSearchQuery = new LogicalSearchQuery()
 					.setCondition(from(schemaTypes.getSchemaType(SavedSearch.SCHEMA_TYPE)).returnAll());
-			List<Record> savedSearches = searchServices.search(allSavedSearchQuery);
+			List<Record> savedSearches = modelLayerFactory.newSearchServices().search(allSavedSearchQuery);
 
+			List<SolrInputDocument> solrInputDocuments = new ArrayList<>();
 			final Map<String, List<String>> sharedGroups = new HashMap<>();
 			final Map<String, List<String>> sharedUsers = new HashMap<>();
 			for (Record savedSearch : savedSearches) {
-				sharedGroups.put(savedSearch.getId(), savedSearch.<String>getValues(schemaTypes.getMetadata(SAVED_SEACH_SHARED_GROUPS_CODE)));
-				sharedUsers.put(savedSearch.getId(), savedSearch.<String>getValues(schemaTypes.getMetadata(SAVED_SEACH_SHARED_USERS_CODE)));
+				SolrInputDocument inputDocument = new SolrInputDocument();
+				Metadata searchGroups = schemaTypes.getMetadata(SAVED_SEACH_SHARED_GROUPS_CODE);
+				Metadata searchUsers = schemaTypes.getMetadata(SAVED_SEACH_SHARED_USERS_CODE);
+				List<String> searchGroupsValues = savedSearch.getValues(searchGroups);
+				List<String> searchUsersValues = savedSearch.getValues(searchUsers);
+
+				inputDocument.setField("id", savedSearch.getId());
+				if (!searchGroupsValues.isEmpty()) {
+					inputDocument.setField("sharedGroupsId_ss", SolrUtils.atomicSet(searchGroupsValues));
+					inputDocument.setField(searchGroups.getDataStoreCode(), SolrUtils.atomicSet(""));
+				}
+				if (!searchGroupsValues.isEmpty()) {
+					inputDocument.setField("sharedUsersId_ss", SolrUtils.atomicSet(searchUsersValues));
+					inputDocument.setField(searchUsers.getDataStoreCode(), SolrUtils.atomicSet(""));
+				}
+
+				sharedGroups.put(savedSearch.getId(), searchGroupsValues);
+				sharedUsers.put(savedSearch.getId(), searchUsersValues);
+
+				if (inputDocument.get("sharedGroupsId_ss") != null || inputDocument.get("sharedUsersId_ss") != null) {
+					solrInputDocuments.add(inputDocument);
+				}
 			}
+
+			BigVaultServerTransaction transaction = new BigVaultServerTransaction(RecordsFlushing.NOW())
+					.setUpdatedDocuments(solrInputDocuments);
+			BigVaultServer vaultServer = modelLayerFactory.getDataLayerFactory().getRecordsVaultServer();
+			vaultServer.addAll(transaction);
 
 			new CoreSchemaAlterationFor8_3_1_delete(collection, provider, appLayerFactory).migrate();
 			new CoreSchemaAlterationFor8_3_1_recreate(collection, provider, appLayerFactory).migrate();
 
-			new ConditionnedActionExecutorInBatchBuilder(appLayerFactory.getModelLayerFactory(), allSavedSearchQuery.getCondition())
+			new ConditionnedActionExecutorInBatchBuilder(modelLayerFactory, allSavedSearchQuery.getCondition())
 					.setOptions(RecordUpdateOptions.validationExceptionSafeOptions())
 					.modifyingRecordsWithImpactHandling(new RecordScript() {
 
@@ -78,7 +110,6 @@ public class CoreMigrationTo_8_3_1 implements MigrationScript {
 					});
 		}
 	}
-
 
 	private class CoreSchemaAlterationFor8_3_1_delete extends MetadataSchemasAlterationHelper {
 		public CoreSchemaAlterationFor8_3_1_delete(String collection,
