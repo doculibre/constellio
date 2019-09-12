f2989a1a0fe0bab49e02110ad24dd548ce0bf934
Vincent Dussault <vincent.dussault@constellio.com>
2019-09-12 10:21:38 -0400

    Viewer responsive adjustments

---
+++ app/WebContent/VAADIN/widgetsets/com.constellio.app.ui.application.widgetset.AppWidgetset/deferredjs/1F8E9EAF86828A41705E4ACDA4F78F9F/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_constellio_app_ui_application_widgetset_AppWidgetset.runAsyncCallback2('Mib(1762,1,Zue);_.yc=function isc(){aac((!V9b&&(V9b=new fac),V9b),this.a.d)};One(_h)(2);\n//# sourceURL=com.constellio.app.ui.application.widgetset.AppWidgetset-2.js\n')
