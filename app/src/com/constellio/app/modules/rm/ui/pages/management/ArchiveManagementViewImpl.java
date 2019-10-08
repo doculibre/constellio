684740df748eb00256437acd71a4f135eae9019b
Jonathan <jonathanplamondon@hotmail.fr>
2019-10-08 14:14:06 -0400

    QAC-272

--- app/src/com/constellio/app/modules/rm/ui/pages/management/ArchiveManagementViewImpl.java
+++ app/src/com/constellio/app/modules/rm/ui/pages/management/ArchiveManagementViewImpl.java
@@ -27,7 +27,7 @@ public class ArchiveManagementViewImpl extends BaseViewImpl implements ArchiveMa
 
 	private final ArchiveManagementPresenter presenter;
 
-	private Button decommissioning, multipleContainers, containers, reports, availableSpace, baginfo;
+	private Button decommissioning, multipleContainers, newContainer, containers, reports, availableSpace, baginfo;
 
 	public ArchiveManagementViewImpl() {
 		presenter = new ArchiveManagementPresenter(this);
@@ -48,6 +48,16 @@ public class ArchiveManagementViewImpl extends BaseViewImpl implements ArchiveMa
 		};
 		decommissioning.addStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
 		decommissioning.addStyleName(DECOMMISSIONING);
+
+		newContainer = new IconButton(NEW_CONTAINER_ICON, $("ArchiveManagementView.newContainer"), false) {
+			@Override
+			protected void buttonClick(ClickEvent event) {
+				presenter.newContainerButtonClicked();
+			}
+		};
+		newContainer.addStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
+		newContainer.addStyleName(NEW_CONTAINER);
+
 
 		multipleContainers = new IconButton(MULTIPLE_CONTAINERS_ICON, $("ArchiveManagementView.newContainers"), false) {
 			@Override
@@ -107,5 +117,10 @@ public class ArchiveManagementViewImpl extends BaseViewImpl implements ArchiveMa
 	@Override
 	public void setPrintReportsButtonVisible(boolean visible) {
 		reports.setVisible(visible);
+	}
+
+	@Override
+	public void setNewContainerButtonVisible(boolean visible) {
+		newContainer.setVisible(visible);
 	}
 }
