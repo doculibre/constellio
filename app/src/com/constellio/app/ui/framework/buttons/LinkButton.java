6fc916138f0ded5a28cba49ac145b9df1798e160
Jonathan <jonathanplamondon@hotmail.fr>
2019-12-17 10:15:53 -0500

    btn

--- app/src/com/constellio/app/ui/framework/buttons/LinkButton.java
+++ app/src/com/constellio/app/ui/framework/buttons/LinkButton.java
@@ -1,8 +1,12 @@
 package com.constellio.app.ui.framework.buttons;
 
+import com.vaadin.server.Resource;
 import com.vaadin.ui.themes.ValoTheme;
 
 public abstract class LinkButton extends BaseButton {
+	public LinkButton(String caption, Resource icon) {
+		super(caption, icon);
+	}
 
 	public LinkButton(String caption) {
 		super(caption);
