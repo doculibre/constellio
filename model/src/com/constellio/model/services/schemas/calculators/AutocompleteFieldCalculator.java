179c5add331f9da573656517b482b7a7007d53e5
Charles Dubois <charles.dubois@doculibre.com>
2020-02-07 14:01:29 -0500

    added method to obtain folders missing adminsitrative units from its children

--- model/src/com/constellio/model/services/schemas/calculators/AutocompleteFieldCalculator.java
+++ model/src/com/constellio/model/services/schemas/calculators/AutocompleteFieldCalculator.java
@@ -50,7 +50,7 @@ public class AutocompleteFieldCalculator extends AbstractMetadataValueCalculator
 															 AutocompleteSplitCriteria autocompleteSplitCriteria) {
 		String regex = autocompleteSplitCriteria.getRegex();
 
-		if (value instanceof List) {
+		if (valueIsStringList(value)) {
 			for (String item : (List<String>) value) {
 				splitInLowerCasedTermsRemovingAccents(words, item, regex);
 			}
@@ -58,6 +58,11 @@ public class AutocompleteFieldCalculator extends AbstractMetadataValueCalculator
 			splitInLowerCasedTermsRemovingAccents(words, (String) value, regex);
 
 		}
+	}
+
+	private static boolean valueIsStringList(Object value) {
+		return (value instanceof List && !((List) value).isEmpty())
+			   && ((List) value).get(0) instanceof String;
 	}
 
 	private static void splitInLowerCasedTermsRemovingAccents(Set<String> words, String value, String regex) {
