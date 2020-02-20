b6f0ca15437a7d94a432ae51e4f8a944fabd10ec
Tristan Lirette <31151912+tristanlirette@users.noreply.github.com>
2020-02-20 15:23:13 -0500

    Added new metadata to the document type display config in the migration script Replaced wrong version number

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo9_1_12.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationTo9_1_12.java
@@ -6,6 +6,8 @@ import com.constellio.app.entities.modules.MigrationScript;
 import com.constellio.app.modules.rm.wrappers.Document;
 import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.app.services.schemasDisplay.SchemaTypesDisplayTransactionBuilder;
+import com.constellio.app.services.schemasDisplay.SchemasDisplayManager;
 import com.constellio.model.entities.schemas.MetadataValueType;
 import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
 
@@ -13,17 +15,33 @@ public class RMMigrationTo9_1_12 implements MigrationScript {
 
 	@Override
 	public String getVersion() {
-		return "9.0.56";
+		return "9.1.12";
 	}
 
 	@Override
 	public void migrate(String collection, MigrationResourcesProvider migrationResourcesProvider,
 						AppLayerFactory appLayerFactory) throws Exception {
-		new SchemaAlterationFor9_0_56(collection, migrationResourcesProvider, appLayerFactory).migrate();
+		new SchemaAlterationFor9_1_12(collection, migrationResourcesProvider, appLayerFactory).migrate();
+
+		setupDisplayConfig(collection, appLayerFactory);
 	}
 
-	private class SchemaAlterationFor9_0_56 extends MetadataSchemasAlterationHelper {
-		SchemaAlterationFor9_0_56(String collection, MigrationResourcesProvider migrationResourcesProvider,
+	private void setupDisplayConfig(String collection, AppLayerFactory appLayerFactory) {
+		SchemasDisplayManager displayManager = appLayerFactory.getMetadataSchemasDisplayManager();
+		SchemaTypesDisplayTransactionBuilder transactionBuilder = displayManager.newTransactionBuilderFor(collection);
+
+		transactionBuilder.in(Document.SCHEMA_TYPE)
+				.addToForm(Document.LINKED_TO)
+				.atTheEnd();
+		transactionBuilder.in(Document.SCHEMA_TYPE)
+				.addToDisplay(Document.LINKED_TO)
+				.atTheEnd();
+
+		displayManager.execute(transactionBuilder.build());
+	}
+
+	private class SchemaAlterationFor9_1_12 extends MetadataSchemasAlterationHelper {
+		SchemaAlterationFor9_1_12(String collection, MigrationResourcesProvider migrationResourcesProvider,
 								  AppLayerFactory appLayerFactory) {
 			super(collection, migrationResourcesProvider, appLayerFactory);
 		}
