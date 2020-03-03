058fda72f8a92a5d2047664724200b69a2c0bdba
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2020-03-03 15:47:30 -0500

    Commit pour phillipe

--- app/src/com/constellio/app/ui/pages/management/authorizations/ShareContentListPresenter.java
+++ app/src/com/constellio/app/ui/pages/management/authorizations/ShareContentListPresenter.java
@@ -1,28 +1,40 @@
 package com.constellio.app.ui.pages.management.authorizations;
 
-import com.constellio.data.utils.LangUtils;
+import com.constellio.app.ui.entities.UserVO;
+import com.constellio.app.ui.pages.base.BasePresenter;
 import com.constellio.model.entities.CorePermissions;
 import com.constellio.model.entities.records.Record;
-import com.constellio.model.entities.records.wrappers.Authorization;
 import com.constellio.model.entities.records.wrappers.User;
-import com.constellio.model.entities.schemas.Schemas;
-import com.constellio.model.entities.security.Role;
+import com.constellio.model.services.security.AuthorizationsServices;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.TabSheet;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+public class ShareContentListPresenter extends BasePresenter<ShareContentListViewImpl> {
 
-import static com.constellio.model.entities.security.global.AuthorizationDeleteRequest.authorizationDeleteRequest;
+	private TabSheet tabSheet;
 
-public class ShareContentListPresenter extends ListAuthorizationsPresenter {
+	private transient AuthorizationsServices authorizationsServices;
 
 	public ShareContentListPresenter(ShareContentListViewImpl view) {
 		super(view);
+
+		tabSheet = new TabSheet();
+
+		tabSheet.addTab(buildFolderTab());
+		tabSheet.addTab(buildDocumentTab());
+		//		tabSheet.addTab()
 	}
 
-	@Override
-	protected List<String> getRestrictedRecordIds(String params) {
-		return Arrays.asList(recordId);
+	private Component buildFolderTab() {
+		return null;
+	}
+
+	private Component buildDocumentTab() {
+		return null;
+	}
+
+	private Component buildRecentItemsTab() {
+		return null;
 	}
 
 	@Override
@@ -35,71 +47,14 @@ public class ShareContentListPresenter extends ListAuthorizationsPresenter {
 		return true;
 	}
 
-	@Override
-	public void backButtonClicked(String schemaCode) {
-		view.navigate().to().adminModule();
+	protected UserVO getUserVO() {
+		return view.getSessionContext().getCurrentUser();
 	}
 
-	@Override
-	public boolean isDetacheable() {
-		return false;
-	}
-
-	public String getSchemaType() {
-		return getRecordVO() == null || getRecordVO().getSchema() == null ? null : getRecordVO().getSchema().getTypeCode();
-	}
-
-	@Override
-	public boolean isAttached() {
-		Record record = recordServices().getDocumentById(recordId);
-		return LangUtils.isFalseOrNull(record.get(Schemas.IS_DETACHED_AUTHORIZATIONS));
-	}
-
-	@Override
-	public List<String> getAllowedAccesses() {
-		List<String> results = new ArrayList<>(3);
-
-		User user = getCurrentUser();
-		Record record = presenterService().getRecord(recordId);
-		if (user.has(CorePermissions.MANAGE_SHARE).globally() || user.has(CorePermissions.MANAGE_GLOBAL_LINKS).globally()) {
-			results.addAll(Arrays.asList(Role.READ, Role.WRITE, Role.DELETE));
-		} else {
-			if (user.hasReadAccess().on(record)) {
-				results.add(Role.READ);
-			}
-			if (user.hasWriteAccess().on(record)) {
-				results.add(Role.WRITE);
-			}
-			if (user.hasDeleteAccess().on(record)) {
-				results.add(Role.DELETE);
-			}
+	protected AuthorizationsServices authorizationsServices() {
+		if (authorizationsServices == null) {
+			authorizationsServices = modelLayerFactory.newAuthorizationsServices();
 		}
-
-		return results;
-	}
-
-	@Override
-	protected boolean isOwnAuthorization(Authorization authorization) {
-		return authorization.getTarget().equals(recordId);
-	}
-
-	@Override
-	protected void removeAuthorization(Authorization authorization) {
-		authorizationsServices().execute(authorizationDeleteRequest(authorization).setExecutedBy(getCurrentUser()));
-	}
-
-	@Override
-	public boolean seeRolesField() {
-		return false;
-	}
-
-	@Override
-	public boolean seeAccessField() {
-		return false;
-	}
-
-	@Override
-	public boolean seeSharedBy() {
-		return true;
+		return authorizationsServices;
 	}
 }
