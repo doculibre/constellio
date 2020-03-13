b1be564a27e80ea8b003f720307dcde2be869e7a
Jonathan Plamondon <jonathan.plamondon@doculibre.com>
2020-03-13 15:19:21 -0400

    progress.

--- app/src/com/constellio/app/ui/framework/components/fields/AdvancedSearchCriteriaFieldPresenter.java
+++ app/src/com/constellio/app/ui/framework/components/fields/AdvancedSearchCriteriaFieldPresenter.java
@@ -1,26 +1,31 @@
 package com.constellio.app.ui.framework.components.fields;
 
 import com.constellio.app.extensions.AppLayerCollectionExtensions;
+import com.constellio.app.modules.es.model.connectors.smb.ConnectorSmbDocument;
+import com.constellio.app.modules.es.model.connectors.smb.ConnectorSmbFolder;
 import com.constellio.app.modules.rm.wrappers.Folder;
 import com.constellio.app.services.factories.AppLayerFactory;
 import com.constellio.app.services.factories.ConstellioFactories;
 import com.constellio.app.ui.application.ConstellioUI;
 import com.constellio.app.ui.entities.MetadataSchemaTypeVO;
 import com.constellio.app.ui.entities.MetadataVO;
-import com.constellio.app.ui.entities.RecordVO.VIEW_MODE;
-import com.constellio.app.ui.framework.builders.MetadataSchemaToVOBuilder;
 import com.constellio.app.ui.framework.builders.MetadataSchemaTypeToVOBuilder;
 import com.constellio.app.ui.framework.builders.MetadataToVOBuilder;
+import com.constellio.app.ui.pages.MetadataSorterUtil;
 import com.constellio.app.ui.pages.base.BasePresenterUtils;
 import com.constellio.app.ui.pages.base.BaseViewImpl;
 import com.constellio.app.ui.pages.base.SessionContext;
 import com.constellio.app.ui.pages.search.AdvancedSearchCriteriaComponent.SearchCriteriaPresenter;
 import com.constellio.app.ui.pages.search.SearchCriteriaPresenterUtils;
 import com.constellio.app.ui.pages.search.criteria.Criterion;
+import com.constellio.model.entities.Taxonomy;
+import com.constellio.model.entities.records.wrappers.User;
 import com.constellio.model.entities.schemas.Metadata;
-import com.constellio.model.entities.schemas.MetadataSchema;
 import com.constellio.model.entities.schemas.MetadataSchemaType;
 import com.constellio.model.entities.schemas.MetadataSchemaTypes;
+import com.constellio.model.entities.schemas.MetadataValueType;
+import com.constellio.model.entities.schemas.Schemas;
+import com.constellio.model.services.schemas.MetadataList;
 import com.constellio.model.services.schemas.MetadataSchemasManager;
 import com.vaadin.navigator.View;
 import com.vaadin.server.Page;
@@ -36,7 +41,6 @@ public class AdvancedSearchCriteriaFieldPresenter implements SearchCriteriaPrese
 	private AppLayerFactory appLayerFactory;
 	private SessionContext sessionContext;
 	private String schemaType;
-	private ConstellioFactories constellioFactories;
 	private BasePresenterUtils presenterUtils;
 
 
@@ -44,7 +48,6 @@ public class AdvancedSearchCriteriaFieldPresenter implements SearchCriteriaPrese
 												ConstellioFactories constellioFactories) {
 		this.field = field;
 		this.appLayerFactory = constellioFactories.getAppLayerFactory();
-		this.constellioFactories = constellioFactories;
 		this.sessionContext = ConstellioUI.getCurrentSessionContext();
 		this.presenterUtils = new BasePresenterUtils(constellioFactories, sessionContext);
 	}
@@ -65,6 +68,30 @@ public class AdvancedSearchCriteriaFieldPresenter implements SearchCriteriaPrese
 		return result;
 	}
 
+	private boolean isMetadataVisibleForUser(Metadata metadata, User currentUser) {
+		if (MetadataValueType.REFERENCE.equals(metadata.getType())) {
+			String referencedSchemaType = metadata.getAllowedReferences().getTypeWithAllowedSchemas();
+			Taxonomy taxonomy = appLayerFactory.getModelLayerFactory().getTaxonomiesManager()
+					.getTaxonomyFor(sessionContext.getCurrentCollection(), referencedSchemaType);
+			if (taxonomy != null) {
+				List<String> taxonomyGroupIds = taxonomy.getGroupIds();
+				List<String> taxonomyUserIds = taxonomy.getUserIds();
+				List<String> userGroups = currentUser.getUserGroups();
+				for (String group : taxonomyGroupIds) {
+					for (String userGroup : userGroups) {
+						if (userGroup.equals(group)) {
+							return true;
+						}
+					}
+				}
+				return (taxonomyGroupIds.isEmpty() && taxonomyUserIds.isEmpty()) || taxonomyUserIds.contains(currentUser.getId());
+			} else {
+				return true;
+			}
+		}
+		return true;
+	}
+
 	public void selectAdvancedSearchSchemaType(String schemaType) {
 		this.schemaType = schemaType;
 	}
@@ -81,19 +108,36 @@ public class AdvancedSearchCriteriaFieldPresenter implements SearchCriteriaPrese
 
 	@Override
 	public List<MetadataVO> getMetadataAllowedInCriteria() {
-		MetadataSchemaType schemaType = types().getSchemaType(Folder.SCHEMA_TYPE);
-		MetadataSchema schema = schemaType.getDefaultSchema();
+		//		String collection = view.getCollection();
+		MetadataSchemaTypes types = types();
+		MetadataSchemaType metadataSchemaType = types.getSchemaType(schemaType);
+		List<String> metadataCodes = new ArrayList<>();
 
-		List<MetadataVO> metadataVOList = new ArrayList<>();
+		List<MetadataVO> result = new ArrayList<>();
+		MetadataList allMetadatas = metadataSchemaType.getAllMetadatas();
 
-		MetadataToVOBuilder metadataToVOBuilder = new MetadataToVOBuilder();
-		MetadataSchemaToVOBuilder metadataSchemaToVOBuilder = new MetadataSchemaToVOBuilder();
-
-		for (Metadata metadata : schemaType.getAllMetadatas()) {
-			metadataVOList.add(metadataToVOBuilder.build(metadata, metadataSchemaToVOBuilder.build(schema, VIEW_MODE.DISPLAY, sessionContext), sessionContext));
+		for (Metadata metadata : metadataSchemaType.getAllMetadatas()) {
+			metadataCodes.add(metadata.getCode());
 		}
 
-		return metadataVOList;
+		MetadataToVOBuilder builder = new MetadataToVOBuilder();
+		for (Metadata metadata : allMetadatas) {
+			if (!metadataSchemaType.hasSecurity() || (metadataCodes.contains(metadata.getCode()))) {
+				boolean isTextOrString =
+						metadata.getType() == MetadataValueType.STRING || metadata.getType() == MetadataValueType.TEXT;
+
+				if (this.presenterUtils.getCurrentUser().hasGlobalAccessToMetadata(metadata)
+					&& isMetadataVisibleForUser(metadata, this.presenterUtils.getCurrentUser()) &&
+					(!isTextOrString || isTextOrString ||
+					 Schemas.PATH.getLocalCode().equals(metadata.getLocalCode()) ||
+					 ConnectorSmbFolder.PARENT_CONNECTOR_URL.equals(metadata.getLocalCode()) ||
+					 ConnectorSmbDocument.PARENT_CONNECTOR_URL.equals(metadata.getLocalCode()))) {
+					result.add(builder.build(metadata, sessionContext));
+				}
+			}
+		}
+		MetadataSorterUtil.sort(result, sessionContext);
+		return result;
 	}
 
 	@Override
