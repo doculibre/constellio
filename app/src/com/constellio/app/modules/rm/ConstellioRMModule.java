3604bb9e9c61c25264c592ccc55a222a7f576b2f
francisbaril <baril.francis@gmail.com>
2020-04-04 15:18:00 -0400

    migration 45 fix

--- app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
+++ app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
@@ -257,6 +257,7 @@ public class ConstellioRMModule implements InstallableSystemModule, ModuleWithCo
 		scripts.add(new RMMigrationTo9_0_0_33());
 		scripts.add(new RMMigrationTo9_0_0_42());
 		scripts.add(new RMMigrationTo9_0_0_45());
+		scripts.add(new RMMigrationTo9_0_0_45_1());
 		scripts.add(new RMMigrationTo9_0_0_47());
 		scripts.add(new RMMigrationTo9_0_0_60());
 		scripts.add(new RMMigrationTo9_0_0_60_1());
