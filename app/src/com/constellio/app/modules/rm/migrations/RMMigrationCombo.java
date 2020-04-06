dd9d78a99889ce31eef0823bcf4ab830749721aa
francisbaril <baril.francis@gmail.com>
2020-04-05 20:50:16 -0400

    merge of wombo combo regenerated for version 9.0.2

--- app/src/com/constellio/app/modules/rm/migrations/RMMigrationCombo.java
+++ app/src/com/constellio/app/modules/rm/migrations/RMMigrationCombo.java
@@ -182,7 +182,6 @@ public class RMMigrationCombo implements ComboMigrationScript {
 		scripts.add(new RMMigrationTo9_0_0_45());
 		scripts.add(new RMMigrationTo9_0_0_45_1());
 		scripts.add(new RMMigrationTo9_0_0_47());
-		scripts.add(new RMMigrationTo9_0_0_60());
 		scripts.add(new RMMigrationTo9_0_0_60_1());
 
 		return scripts;
