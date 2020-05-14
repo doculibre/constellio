a3bfc092e9b9700ad7c55ec79a454e9373df565d
Vincent Dussault <vincent.dussault@constellio.com>
2020-05-14 18:25:35 -0400

    fix(Merge): Merge conflict fix

--- app/src/com/constellio/app/modules/rm/ui/components/breadcrumb/FolderDocumentContainerBreadcrumbTrail.java
+++ app/src/com/constellio/app/modules/rm/ui/components/breadcrumb/FolderDocumentContainerBreadcrumbTrail.java
@@ -65,7 +65,6 @@ public class FolderDocumentContainerBreadcrumbTrail extends TitleBreadcrumbTrail
 		} else if (item instanceof FavoritesBreadcrumbItem) {
 			recordId = null;
 		} else if (item instanceof LastViewedFoldersDocumentsBreadcrumbItem) {
-		} else if (item instanceof FavoritesBreadcrumbItem) {
 			recordId = null;
 		} else if (item instanceof ListContentAccessAndRoleAuthorizationsBreadCrumbItem) {
 			recordId = null;
