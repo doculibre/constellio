7b2d94e327b276ea2099f7d0f8b071b25a2fb4aa
Vincent Dussault <vincent.dussault@constellio.com>
2020-05-21 01:03:58 -0400

    feature(Reports): Add reports for retention rules and categories

--- app/src/com/constellio/app/modules/rm/services/menu/RMRecordsMenuItemServices.java
+++ app/src/com/constellio/app/modules/rm/services/menu/RMRecordsMenuItemServices.java
@@ -1,5 +1,37 @@
 package com.constellio.app.modules.rm.services.menu;
 
+import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
+import com.constellio.app.modules.rm.services.actions.ContainerRecordActionsServices;
+import com.constellio.app.modules.rm.services.actions.DocumentRecordActionsServices;
+import com.constellio.app.modules.rm.services.actions.FolderRecordActionsServices;
+import com.constellio.app.modules.rm.services.menu.behaviors.RMRecordsMenuItemBehaviors;
+import com.constellio.app.modules.rm.wrappers.Category;
+import com.constellio.app.modules.rm.wrappers.ContainerRecord;
+import com.constellio.app.modules.rm.wrappers.Document;
+import com.constellio.app.modules.rm.wrappers.Folder;
+import com.constellio.app.modules.rm.wrappers.RMTask;
+import com.constellio.app.modules.rm.wrappers.RetentionRule;
+import com.constellio.app.modules.tasks.services.actions.TaskRecordActionsServices;
+import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.app.services.menu.ActionDisplayOption;
+import com.constellio.app.services.menu.MenuItemAction;
+import com.constellio.app.services.menu.MenuItemActionState;
+import com.constellio.app.services.menu.behavior.MenuItemActionBehaviorParams;
+import com.constellio.model.entities.records.Record;
+import com.constellio.model.entities.records.wrappers.User;
+import com.constellio.model.services.schemas.MetadataSchemasManager;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.Resource;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
 import static com.constellio.app.modules.rm.services.menu.RMRecordsMenuItemServices.RMRecordsMenuItemActionType.RMRECORDS_ADD_CART;
 import static com.constellio.app.modules.rm.services.menu.RMRecordsMenuItemServices.RMRecordsMenuItemActionType.RMRECORDS_ADD_SELECTION;
 import static com.constellio.app.modules.rm.services.menu.RMRecordsMenuItemServices.RMRecordsMenuItemActionType.RMRECORDS_BATCH_DELETE;
@@ -30,40 +62,6 @@ import static com.constellio.app.ui.i18n.i18n.getLanguage;
 import static com.vaadin.server.FontAwesome.STAR;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.StringUtils;
-
-import com.constellio.app.modules.rm.services.RMSchemasRecordsServices;
-import com.constellio.app.modules.rm.services.actions.ContainerRecordActionsServices;
-import com.constellio.app.modules.rm.services.actions.DocumentRecordActionsServices;
-import com.constellio.app.modules.rm.services.actions.FolderRecordActionsServices;
-import com.constellio.app.modules.rm.services.menu.behaviors.RMRecordsMenuItemBehaviors;
-import com.constellio.app.modules.rm.wrappers.Category;
-import com.constellio.app.modules.rm.wrappers.ContainerRecord;
-import com.constellio.app.modules.rm.wrappers.Document;
-import com.constellio.app.modules.rm.wrappers.Folder;
-import com.constellio.app.modules.rm.wrappers.RMTask;
-import com.constellio.app.modules.rm.wrappers.RetentionRule;
-import com.constellio.app.modules.tasks.services.actions.TaskRecordActionsServices;
-import com.constellio.app.services.factories.AppLayerFactory;
-import com.constellio.app.services.menu.ActionDisplayOption;
-import com.constellio.app.services.menu.MenuItemAction;
-import com.constellio.app.services.menu.MenuItemActionState;
-import com.constellio.app.services.menu.behavior.MenuItemActionBehaviorParams;
-import com.constellio.model.entities.records.Record;
-import com.constellio.model.entities.records.wrappers.User;
-import com.constellio.model.services.schemas.MetadataSchemasManager;
-import com.vaadin.server.FontAwesome;
-import com.vaadin.server.Resource;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 public class RMRecordsMenuItemServices {
 
