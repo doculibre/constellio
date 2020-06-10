325e9575883f18dbe00ba0be232213fb58467891
francisbaril <baril.francis@gmail.com>
2020-06-10 12:41:37 -0400

    feature(Savestates using weekly regenerated base file): Now creating base file with populated fields

--- app/src/com/constellio/app/services/importExport/systemStateExport/SystemStateExporter.java
+++ app/src/com/constellio/app/services/importExport/systemStateExport/SystemStateExporter.java
@@ -406,7 +406,7 @@ public class SystemStateExporter {
 	}
 
 	public void regroupAndMoveInVault() {
-		this.secondTransactionLogManager.regroupAndMoveInVault();
+		this.secondTransactionLogManager.regroupAndMove();
 
 	}
 }
