272b6e54fed311611093b91ef136818aca01393f
francisbaril <baril.francis@gmail.com>
2020-06-11 15:46:32 -0400

    feature(Savestates using weekly regenerated base file): Disabled, since it is causing problems

--- app/src/com/constellio/app/services/background/CreateBaseSaveStateBackgroundAction.java
+++ app/src/com/constellio/app/services/background/CreateBaseSaveStateBackgroundAction.java
@@ -1,7 +1,6 @@
 package com.constellio.app.services.background;
 
 import com.constellio.app.services.factories.AppLayerFactory;
-import com.constellio.app.services.importExport.systemStateExport.SystemStateExporter;
 import com.constellio.data.io.IOServicesFactory;
 
 public class CreateBaseSaveStateBackgroundAction implements Runnable {
@@ -17,7 +16,7 @@ public class CreateBaseSaveStateBackgroundAction implements Runnable {
 
 	@Override
 	public void run() {
-		new SystemStateExporter(appLayerFactory).createSavestateBaseFileInVault(false);
+		//new SystemStateExporter(appLayerFactory).createSavestateBaseFileInVault(false);
 
 	}
 
