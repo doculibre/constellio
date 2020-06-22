0a0fd2923ca357ee86ac7415b368a81c74566a46
Charles Dubois <charles.dubois@doculibre.com>
2020-06-22 14:28:22 -0400

    fix(HttpServletRequestAuthenticator): Reverted HttpServletRequestAuthenticator changes

--- app/src/com/constellio/app/api/HttpServletRequestAuthenticator.java
+++ app/src/com/constellio/app/api/HttpServletRequestAuthenticator.java
@@ -137,10 +137,10 @@ public class HttpServletRequestAuthenticator {
 		try {
 			byte[] tokenOrPassword = userToken.getBytes(Charset.forName("UTF-8"));
 
+			boolean authenticated = modelLayerFactory.newAuthenticationService().authenticate(username, new String(tokenOrPassword));
 			UserCredential userCredential = userServices.getUser(username);
-			boolean authenticated = userCredential.getTokenKeys().contains(new String(tokenOrPassword));
 			if (!authenticated) {
-				authenticated = modelLayerFactory.newAuthenticationService().authenticate(username, new String(tokenOrPassword));
+				authenticated = userCredential.getTokenKeys().contains(new String(tokenOrPassword));
 			}
 
 			if (authenticated) {
