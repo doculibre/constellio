b3f165644b47d3c7a71d916fa1ee537bdc6dd9f9
Maxime <cote.maxime1@gmail.com>
2020-07-14 14:47:55 -0400

    Migration script for DHKey parameters
    Adding the new ephemeralDHKeySize settings in the wrapper config

---
+++ app/src/com/constellio/app/services/migrations/scripts/CoreMigrationTo_9_0_3_22.java
@@ -0,0 +1,48 @@
+package com.constellio.app.services.migrations.scripts;
+
+import java.io.File;
+
+import com.constellio.app.api.admin.services.WrapperConfUpdateUtils;
+import com.constellio.app.entities.modules.MetadataSchemasAlterationHelper;
+import com.constellio.app.entities.modules.MigrationResourcesProvider;
+import com.constellio.app.entities.modules.MigrationScript;
+import com.constellio.app.services.factories.AppLayerFactory;
+import com.constellio.model.conf.FoldersLocator;
+import com.constellio.model.conf.FoldersLocatorMode;
+import com.constellio.model.entities.records.wrappers.Collection;
+import com.constellio.model.services.schemas.builders.MetadataSchemaTypesBuilder;
+
+public class CoreMigrationTo_9_0_3_22 implements MigrationScript {
+	@Override
+	public String getVersion() {
+		return "9.0.3.22";
+	}
+
+	@Override
+	public void migrate(String collection, MigrationResourcesProvider migrationResourcesProvider,
+						AppLayerFactory appLayerFactory) throws Exception {
+		if (collection.equals(Collection.SYSTEM_COLLECTION)) {
+			new CoreSchemaAlterationFor_9_0_3_22(collection, migrationResourcesProvider, appLayerFactory).migrate();
+		} else {
+			FoldersLocator foldersLocator = appLayerFactory.getModelLayerFactory().getFoldersLocator();
+			if (foldersLocator.getFoldersLocatorMode() == FoldersLocatorMode.WRAPPER) {
+					File currentWrapper = foldersLocator.getWrapperConf();
+					WrapperConfUpdateUtils.setSettingAdditionalEphemeralDHKeySize(currentWrapper,
+							appLayerFactory.getModelLayerFactory().getIOServicesFactory().newFileService());
+			}
+		}
+	}
+
+	class CoreSchemaAlterationFor_9_0_3_22 extends MetadataSchemasAlterationHelper {
+
+		protected CoreSchemaAlterationFor_9_0_3_22(String collection,
+												   MigrationResourcesProvider migrationResourcesProvider,
+												   AppLayerFactory appLayerFactory) {
+			super(collection, migrationResourcesProvider, appLayerFactory);
+		}
+
+		@Override
+		protected void migrate(MetadataSchemaTypesBuilder typesBuilder) {
+		}
+	}
+}
