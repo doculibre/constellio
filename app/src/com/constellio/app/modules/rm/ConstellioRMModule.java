69ecde42c98658826421dc05caedaee47e9c4ccb
ericgiguere <eric.giguere@constellio.com>
2020-08-12 12:16:24 -0400

    Fix merge - updated script number

--- app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
+++ app/src/com/constellio/app/modules/rm/ConstellioRMModule.java
@@ -266,8 +266,8 @@ public class ConstellioRMModule implements InstallableSystemModule, ModuleWithCo
 		scripts.add(new RMMigrationTo9_0_3_16());
 		scripts.add(new RMMigrationTo9_0_3_17());
 		scripts.add(new RMMigrationTo9_0_3_18());
+		scripts.add(new RMMigrationTo9_0_3_20());
 		//scripts.add(new RMMigrationTo9_0_666());
-		scripts.add(new RMMigrationTo9_0_3_13());
 
 		return scripts;
 	}
